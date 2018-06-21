00c10e37f9b81d3f72bd876a54cdf45d871c4ff5
Matoga, Adrian <adrian.matoga@intel.com>
2018-06-21 20:13:43 +0200

    Test cuda-to-sycl with lit
    Use "ninja check-clang-cuda-to-sycl" to run only cuda-to-sycl tests.
    "ninja check-all" also includes cuda-to-sycl tests.
    
    Change-Id: I5f184845ab24ca4c81a462ee30f2ca2bffc6a0ba
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/136191
    Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>
    Tested-by: Matoga, Adrian <adrian.matoga@intel.com>

---
+++ clang/test/cuda-to-sycl/iteration-space.cu
@@ -0,0 +1,30 @@
+// RUN: cp %s %t
+// RUN: cuda-to-sycl %t -- -x cuda --cuda-host-only
+// RUN: sed -e 's,//.*$,,' %t | FileCheck %s
+
+__global__
+void foo() {
+  // CHECK: size_t tix = item.get_local(0);
+  // CHECK: size_t tiy = item.get_local(1);
+  // CHECK: size_t tiz = item.get_local(2);
+
+  size_t tix = threadIdx.x;
+  size_t tiy = threadIdx.y;
+  size_t tiz = threadIdx.z;
+
+  // size_t bix = blockIdx.x;
+  // size_t biy = blockIdx.y;
+  // size_t biz = blockIdx.z;
+
+  // CHECK: size_t bdx = item.get_local_range().get(0);
+  // CHECK: size_t bdy = item.get_local_range().get(1);
+  // CHECK: size_t bdz = item.get_local_range().get(2);
+
+  size_t bdx = blockDim.x;
+  size_t bdy = blockDim.y;
+  size_t bdz = blockDim.z;
+
+  // size_t gdx = gridDim.x;
+  // size_t gdy = gridDim.y;
+  // size_t gdz = gridDim.z;
+}
