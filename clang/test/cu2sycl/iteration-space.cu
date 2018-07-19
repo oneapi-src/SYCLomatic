3ac0b18c56927bbd116e40227d37e21c51b97173
Nikolai Bozhenov <nikolai.bozhenov@intel.com>
2018-07-19 18:07:21 +0200

    Add negative checks to iteration-space.cu test
    Change-Id: Icf18280444c6ff16db0f8b4fb4270d753619cdf3
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/140711
    Reviewed-by: German, Leonid <leonid.german@intel.com>
    Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>
    Tested-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>

--- clang/test/cu2sycl/iteration-space.cu
+++ clang/test/cu2sycl/iteration-space.cu
@@ -2,12 +2,13 @@
 // RUN: cu2sycl %t -- -x cuda --cuda-host-only
 // RUN: sed -e 's,//.*$,,' %t | FileCheck %s
 
+// Test that the replacement happens when it should to.
+// CHECK: test_00
 __global__
-void foo() {
+void test_00() {
   // CHECK: size_t tix = item.get_local(0);
   // CHECK: size_t tiy = item.get_local(1);
   // CHECK: size_t tiz = item.get_local(2);
-
   size_t tix = threadIdx.x;
   size_t tiy = threadIdx.y;
   size_t tiz = threadIdx.z;
@@ -19,7 +20,6 @@ void foo() {
   // CHECK: size_t bdx = item.get_local_range().get(0);
   // CHECK: size_t bdy = item.get_local_range().get(1);
   // CHECK: size_t bdz = item.get_local_range().get(2);
-
   size_t bdx = blockDim.x;
   size_t bdy = blockDim.y;
   size_t bdz = blockDim.z;
@@ -27,4 +27,73 @@ void foo() {
   // size_t gdx = gridDim.x;
   // size_t gdy = gridDim.y;
   // size_t gdz = gridDim.z;
+}
+
+// Test that the replacement doesn't happen in host functions.
+// CHECK: test_01
+void test_01() {
+  uint3 threadIdx, blockIdx, blockDim, gridDim;
+
+  // CHECK: size_t tix = threadIdx.x;
+  // CHECK: size_t tiy = threadIdx.y;
+  // CHECK: size_t tiz = threadIdx.z;
+  size_t tix = threadIdx.x;
+  size_t tiy = threadIdx.y;
+  size_t tiz = threadIdx.z;
+
+  // CHECK: size_t bix = blockIdx.x;
+  // CHECK: size_t biy = blockIdx.y;
+  // CHECK: size_t biz = blockIdx.z;
+  size_t bix = blockIdx.x;
+  size_t biy = blockIdx.y;
+  size_t biz = blockIdx.z;
+
+  // CHECK: size_t bdx = blockDim.x;
+  // CHECK: size_t bdy = blockDim.y;
+  // CHECK: size_t bdz = blockDim.z;
+  size_t bdx = blockDim.x;
+  size_t bdy = blockDim.y;
+  size_t bdz = blockDim.z;
+
+  // CHECK: size_t gdx = gridDim.x;
+  // CHECK: size_t gdy = gridDim.y;
+  // CHECK: size_t gdz = gridDim.z;
+  size_t gdx = gridDim.x;
+  size_t gdy = gridDim.y;
+  size_t gdz = gridDim.z;
+}
+
+// Test that the replacement doesn't happen if threadIdx is redefined.
+// CHECK: test_02
+__global__
+void test_02() {
+  uint3 threadIdx, blockIdx, blockDim, gridDim;
+
+  // CHECK: size_t tix = threadIdx.x;
+  // CHECK: size_t tiy = threadIdx.y;
+  // CHECK: size_t tiz = threadIdx.z;
+  size_t tix = threadIdx.x;
+  size_t tiy = threadIdx.y;
+  size_t tiz = threadIdx.z;
+
+  // CHECK: size_t bix = blockIdx.x;
+  // CHECK: size_t biy = blockIdx.y;
+  // CHECK: size_t biz = blockIdx.z;
+  size_t bix = blockIdx.x;
+  size_t biy = blockIdx.y;
+  size_t biz = blockIdx.z;
+
+  // CHECK: size_t bdx = blockDim.x;
+  // CHECK: size_t bdy = blockDim.y;
+  // CHECK: size_t bdz = blockDim.z;
+  size_t bdx = blockDim.x;
+  size_t bdy = blockDim.y;
+  size_t bdz = blockDim.z;
+
+  // CHECK: size_t gdx = gridDim.x;
+  // CHECK: size_t gdy = gridDim.y;
+  // CHECK: size_t gdz = gridDim.z;
+  size_t gdx = gridDim.x;
+  size_t gdy = gridDim.y;
+  size_t gdz = gridDim.z;
 }
