3b9b8f74ec8229285178e1ec69d26164e4d8ada6
Chenwei Sun <chenwei.sun@intel.com>
2018-12-06 19:13:50 +0800

    CTST-250: Added option "-keep-original-code" to embed original code in comments
    1. eg. syclct -keep-original-code <commands> to keep original code in comments.
    
    2. used a general solution, firstly construct replacements
       for all original code to be replaced, then
       merged the comment replacements with current
       code replacements to avoid conflict.
    
    Change-Id: I389f00034e2153bc127a2e0bdad3c71a7bf08be6
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/162977
    Reviewed-by: Chenwei Sun <chenwei.sun@intel.com>
    Tested-by: Chenwei Sun <chenwei.sun@intel.com>

--- clang/lib/SyclCT/SyclCT.cpp
+++ clang/lib/SyclCT/SyclCT.cpp
@@ -58,6 +58,13 @@ static opt<std::string>
             value_desc("/path/to/output/root/"), cat(SyclCTCat),
             llvm::cl::Optional);
 
+bool KeepOriginalCodeFlag = false;
+
+static llvm::cl::opt<bool, true>
+    ShowOrigCode("keep-original-code",
+                 llvm::cl::desc("Keep original code in comments of SYCL file"),
+                 llvm::cl::Hidden, llvm::cl::location(KeepOriginalCodeFlag));
+
 // static opt<bool, true> Verbose("v", desc("Show verbose compiling message"),
 //                                cat(SyclCTCat), location(IsVerbose));
 
@@ -175,6 +182,155 @@ public:
     return ReplSetMerged;
   }
 
+  /// Filter out some style comment to avoid compiler complain on nested
+  /// comments. \returns string proccessed.
+  std::string removeComments(const std::string &Line) {
+
+    std::string CodeLine = Line;
+    size_t Beg = CodeLine.find("/*");
+    size_t End = CodeLine.find("*/");
+
+    // Eg. "comments*/ program\n" => " program\n"
+    if (Beg == std::string::npos && End != std::string::npos) {
+      CodeLine.erase(0, End + 2);
+    }
+
+    while (CodeLine.find("/*") != std::string::npos) {
+      Beg = CodeLine.find("/*");
+      End = CodeLine.find("*/", Beg);
+      if (End != std::string::npos) {
+        // Eg. "program /*comments*/ program\n" => "program  program\n"
+        CodeLine.erase(Beg, (End - Beg) + 2);
+      } else {
+        // Eg. " program /*comments\n" => " program "
+        CodeLine.erase(Beg, (CodeLine.size() - Beg) + 2);
+      }
+    }
+    return CodeLine;
+  }
+
+  /// Merge comments replacement in CommentsReplSet
+  /// with code replacements in CodeRelSet if possible.
+  /// \return code replacements merged.
+  std::vector<ExtReplacement>
+  MergeCommmetsPass(std::vector<ExtReplacement> &CommentsReplSet,
+                    std::vector<ExtReplacement> &CodeRelSet) {
+
+    std::vector<ExtReplacement> ReplSetMerged;
+
+    for (ExtReplacement &CommentsRepl : CommentsReplSet) {
+      bool Merged = false;
+      for (ExtReplacement &CodeRel : CodeRelSet) {
+        if (CommentsRepl.getOffset() == CodeRel.getOffset() &&
+            0 == CodeRel.getLength() &&
+            CommentsRepl.getFilePath() == CodeRel.getFilePath()) {
+          // Coderep is  insert: merge style: (Comment + Code)
+          ExtReplacement ReplMerged(
+              CommentsRepl.getFilePath(), CommentsRepl.getOffset(), 0,
+              StringRef(CommentsRepl.getReplacementText().str() +
+                        CodeRel.getReplacementText().str()),
+              CodeRel.getParentTM());
+          ReplSetMerged.emplace_back(std::move(ReplMerged));
+          Merged = true;
+          CodeRel.setMerged(true);
+          // Note: Can not break here, for there maybe multiple same location
+          // replacements in CodeRelSet
+        } else if (CommentsRepl.getOffset() >= CodeRel.getOffset() &&
+                   CommentsRepl.getOffset() <
+                       CodeRel.getOffset() + CodeRel.getLength() &&
+                   CommentsRepl.getFilePath() == CodeRel.getFilePath()) {
+          // Coderep is replacement: merge style: (Code + Comments)
+          // Coderep is remove: merge style: (Comments)
+          ExtReplacement ReplMerged(
+              CommentsRepl.getFilePath(), CodeRel.getOffset(),
+              CodeRel.getLength(),
+              StringRef(CodeRel.getReplacementText().empty()
+                            ? CodeRel.getReplacementText().str() +
+                                  CommentsRepl.getReplacementText().str()
+                            : CodeRel.getReplacementText().str()),
+              CodeRel.getParentTM());
+          ReplSetMerged.emplace_back(std::move(ReplMerged));
+          Merged = true;
+          CodeRel.setMerged(true);
+          // Note: Can not break here, for there maybe multiple same location
+          // replacements in CodeRelSet
+        }
+      }
+      if (!Merged) {
+        ReplSetMerged.emplace_back(std::move(CommentsRepl));
+      }
+    }
+
+    std::vector<ExtReplacement> ReplSetTotal = ReplSetMerged;
+    for (ExtReplacement &R1 : CodeRelSet) {
+      if (!R1.getMerged()) {
+        ReplSetTotal.emplace_back(std::move(R1));
+      }
+    }
+    return ReplSetTotal;
+  }
+
+  std::vector<ExtReplacement>
+  keepOriginalCode(SourceManager &SM, std::vector<ExtReplacement> &ReplSet) {
+
+    std::vector<ExtReplacement> NewRelInsetSet;
+    std::unordered_set<unsigned int> DuplicateFilter;
+    std::map<StringRef /*FilePath*/, StringRef /*Code*/> CodeCache;
+
+    // Generate comment-replacement for code line which SYCLCT has modified.
+    for (auto I = ReplSet.begin(), E = ReplSet.end(); I != E; ++I) {
+
+      StringRef FilePath = I->getFilePath();
+      StringRef Code;
+      I->setMerged(false); // To initialize the merged flag to avoid dirty value
+                           // in MergeCommmetsPass()
+
+      std::map<StringRef /*FilePath*/, StringRef /*Code*/>::iterator Iter =
+          CodeCache.find(FilePath);
+      if (Iter != CodeCache.end()) {
+        Code = Iter->second;
+      } else {
+        const FileEntry *FileEntry = SM.getFileManager().getFile(FilePath);
+        FileID FID = SM.getOrCreateFileID(FileEntry, SrcMgr::C_User);
+        Code = SM.getBufferData(FID);
+        CodeCache[FilePath] = Code;
+      }
+
+      auto BeginPos = Code.find_last_of('\n', I->getOffset());
+      auto EndPos = Code.find('\n', I->getOffset() + I->getLength());
+      EndPos = (EndPos != StringRef::npos ? EndPos : Code.size());
+      BeginPos += 1;
+      StringRef Line = Code.substr(BeginPos, EndPos - BeginPos);
+
+      // Insert comments in each line
+      if (DuplicateFilter.find(BeginPos) == end(DuplicateFilter)) {
+        DuplicateFilter.insert(BeginPos);
+
+        std::string NewReplacementText;
+        if (Line.endswith("\\")) {
+          // To handle the situation that '\\' appeared in end of row in a macro
+          // statement, lines like:
+          // #define ERROR_CHECK(call) \
+            //    if((call) != 0) { \
+            //        int err = 0; \
+            //       cerr << "Error calling \""#call"\", code is " << err << endl;
+          //        \ my_abort(err); }
+          NewReplacementText = "/*" + removeComments(Line.str()) + "*/ \\\n";
+        } else {
+          NewReplacementText = "/*" + removeComments(Line.str()) + "*/\n";
+        }
+
+        ExtReplacement NewR(FilePath, BeginPos, 0, NewReplacementText,
+                            I->getParentTM() ? I->getParentTM() : nullptr);
+        NewRelInsetSet.emplace_back(std::move(NewR));
+      }
+    }
+
+    std::vector<ExtReplacement> RelpSetTotal =
+        MergeCommmetsPass(NewRelInsetSet, ReplSet);
+    return RelpSetTotal;
+  }
+
   void HandleTranslationUnit(ASTContext &Context) override {
     // The translation process is separated into two stages:
     // 1) Analysis of AST and identification of applicable translation rules
@@ -204,15 +360,31 @@ public:
       }
     }
 
+    // 1. Merge Pass
     std::vector<ExtReplacement> ReplSetMerged = MergeReplacementPass(ReplSet);
-    ReplacementFilter FilteredReplacements(ReplSetMerged);
 
+    // 2. Filter pass
+    ReplacementFilter FilteredReplacements(ReplSetMerged);
+    std::vector<ExtReplacement> ReplSetFiltered;
     for (const ExtReplacement &R : FilteredReplacements) {
+      ReplSetFiltered.emplace_back(R);
+    }
+
+    // 3. May trigger: MergeCommmetsPass
+    if (KeepOriginalCodeFlag) { // To keep original code in comments of SYCL
+                                // files
+      ReplSetFiltered =
+          keepOriginalCode(Context.getSourceManager(), ReplSetFiltered);
+    }
+
+    // Finally Replacement set
+    for (ExtReplacement &R : ReplSetFiltered) {
       if (auto Err = Repl[R.getFilePath()].add(R)) {
         llvm::dbgs() << Err << "\n";
         syclct_unreachable("Adding the replacement: Error occured ");
       }
     }
+
     DebugInfo::printReplacements(FilteredReplacements, Context);
   }
 
@@ -315,7 +487,6 @@ int run(int argc, const char **argv) {
     DebugInfo::ShowStatus(RunResult);
     return RunResult;
   }
-
   // if run was successful
   int status = saveNewFiles(Tool, InRoot, OutRoot);
   DebugInfo::ShowStatus(status);
---
+++ clang/test/syclct/kernel-call-origcode-embedded.cu
@@ -0,0 +1,164 @@
+// RUN: syclct -keep-original-code -out-root %T %s -- -x cuda --cuda-host-only --cuda-path=%cuda-path
+// RUN: FileCheck --input-file %T/kernel-call-origcode-embedded.sycl.cpp --match-full-lines %s
+
+#include <iostream>
+// includes CUDA
+// CHECK:  /*#include <cuda_runtime.h>*/
+#include <cuda_runtime.h>
+
+// CHECK:   /*__global__ void testKernelPtr(const int *L, const int *M, int N) {*/
+// CHECK-NEXT: void testKernelPtr(cl::sycl::nd_item<3> [[ITEMNAME:item_[a-f0-9]+]],
+// CHECK-NEXT:         const int *L, const int *M, int N) {
+__global__ void testKernelPtr(const int *L, const int *M, int N) {
+
+  // CHECK: /*  int gtid = blockIdx.x  * blockDim.x */
+  // CHECK-NEXT:   int gtid = item_{{[a-f0-9]+}}.get_group(0) /*comments*/ * item_{{[a-f0-9]+}}.get_local_range().get(0) /*comments
+  // CHECK-NEXT:  comments*/
+  // CHECK-NEXT: /*  + threadIdx.x;*/
+  // CHECK-NEXT:  + item_{{[a-f0-9]+}}.get_local_id(0);
+  int gtid = blockIdx.x /*comments*/ * blockDim.x /*comments
+  comments*/
+  + threadIdx.x;
+}
+
+// CHECK:     /*__global__ void testKernel(int L, int M, int N) {*/
+// CHECK-NEXT: void testKernel(cl::sycl::nd_item<3> [[ITEMNAME:item_[a-f0-9]+]],
+// CHECK-NEXT:         int L, int M, int N) {
+__global__ void testKernel(int L, int M, int N) {
+  // CHECK:      /*  int gtid = blockIdx.x*/
+  // CHECK-NEXT:  int gtid = item_{{[a-f0-9]+}}.get_group(0)
+  // CHECK-NEXT: /*             * blockDim.x*/
+  // CHECK-NEXT:                * item_{{[a-f0-9]+}}.get_local_range().get(0)
+  // CHECK-NEXT: /*             + threadIdx.x;*/
+  // CHECK-NEXT:                + item_{{[a-f0-9]+}}.get_local_id(0);
+  int gtid = blockIdx.x
+             * blockDim.x
+             + threadIdx.x;
+}
+
+// Error handling macro
+
+// CHECK: #define CUDA_CHECK(call) \
+// CHECK-NEXT:  /*    if((call) != cudaSuccess) { \*/ \
+// CHECK-NEXT:      if((call) != 0) { \
+// CHECK-NEXT:  /*        cudaError_t err = cudaGetLastError(); \*/ \
+// CHECK-NEXT:          int err = 0; \
+// CHECK-NEXT:          std::cout << "CUDA error calling \""#call"\", code is " << err << std::endl; \
+// CHECK-NEXT:          exit(err); }
+#define CUDA_CHECK(call) \
+    if((call) != cudaSuccess) { \
+        cudaError_t err = cudaGetLastError(); \
+        std::cout << "CUDA error calling \""#call"\", code is " << err << std::endl; \
+        exit(err); }
+
+#define checkCudaErrors(val)           check ( (val), #val, __FILE__, __LINE__ )
+template< typename T >
+void check(T result, char const *const func, const char *const file, int const line)
+{}
+
+int main() {
+  // CHECK:  /*  dim3 griddim = 2;*/
+  // CHECK-NEXT:  cl::sycl::range<3> griddim = cl::sycl::range<3>(2, 1, 1);
+  dim3 griddim = 2;
+
+  // CHECK:  /*  dim3 threaddim = 32;*/
+  // CHECK-NEXT:   cl::sycl::range<3> threaddim = cl::sycl::range<3>(32, 1, 1);
+  dim3 threaddim = 32;
+
+  void *karg1 = 0;
+  const int *karg2 = 0;
+  int karg3 = 80;
+  // CHECK:  /*  testKernelPtr<<<griddim, threaddim>>>((const int *)karg1, karg2, karg3);*/
+  // CHECK-NEXT:  {
+  // CHECK-NEXT:    std::pair<syclct::buffer_t, size_t> karg1_buf = syclct::get_buffer_and_offset(karg1);
+  // CHECK-NEXT:    size_t karg1_offset = karg1_buf.second;
+  // CHECK-NEXT:    std::pair<syclct::buffer_t, size_t> karg2_buf = syclct::get_buffer_and_offset(karg2);
+  // CHECK-NEXT:    size_t karg2_offset = karg2_buf.second;
+  // CHECK-NEXT:    syclct::get_default_queue().submit(
+  // CHECK-NEXT:      [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:        auto karg1_acc = karg1_buf.first.get_access<cl::sycl::access::mode::read_write>(cgh);
+  // CHECK-NEXT:        auto karg2_acc = karg2_buf.first.get_access<cl::sycl::access::mode::read_write>(cgh);
+  // CHECK-NEXT:        cgh.parallel_for<SyclKernelName<class testKernelPtr_{{[a-f0-9]+}}>>(
+  // CHECK-NEXT:          cl::sycl::nd_range<3>((griddim * threaddim), threaddim),
+  // CHECK-NEXT:          [=](cl::sycl::nd_item<3> it) {
+  // CHECK-NEXT:            void *karg1 = (void*)(&karg1_acc[0] + karg1_offset);
+  // CHECK-NEXT:            const int *karg2 = (const int*)(&karg2_acc[0] + karg2_offset);
+  // CHECK-NEXT:            testKernelPtr(it, (const int *)karg1, karg2, karg3);
+  // CHECK-NEXT:          });
+  // CHECK-NEXT:      });
+  // CHECK-NEXT:  };
+  testKernelPtr<<<griddim, threaddim>>>((const int *)karg1, karg2, karg3);
+
+  // CHECK: /*  testKernel<<<10, intvar>>>(karg1int, karg2int, karg3int);*/
+  // CHECK-NEXT:  {
+  // CHECK-NEXT:    syclct::get_default_queue().submit(
+  // CHECK-NEXT:      [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:        cgh.parallel_for<SyclKernelName<class testKernel_{{[a-f0-9]+}}>>(
+  // CHECK-NEXT:          cl::sycl::nd_range<3>((cl::sycl::range<3>(10, 1, 1) * cl::sycl::range<3>(intvar, 1, 1)), cl::sycl::range<3>(intvar, 1, 1)),
+  // CHECK-NEXT:          [=](cl::sycl::nd_item<3> it) {
+  // CHECK-NEXT:            testKernel(it, karg1int, karg2int, karg3int);
+  // CHECK-NEXT:          });
+  // CHECK-NEXT:      });
+  // CHECK-NEXT:  };
+  int karg1int = 1;
+  int karg2int = 2;
+  int karg3int = 3;
+  int intvar = 20;
+  testKernel<<<10, intvar>>>(karg1int, karg2int, karg3int);
+
+  // CHECK: /*  testKernel<<<dim3(1), dim3(1, 2)>>>(karg1int, karg2int, karg3int);*/
+  // CHECK-NEXT:  {
+  // CHECK-NEXT:    syclct::get_default_queue().submit(
+  // CHECK-NEXT:      [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:        cgh.parallel_for<SyclKernelName<class testKernel_{{[a-f0-9]+}}>>(
+  // CHECK-NEXT:          cl::sycl::nd_range<3>((cl::sycl::range<3>(1, 1, 1) * cl::sycl::range<3>(1, 2, 1)), cl::sycl::range<3>(1, 2, 1)),
+  // CHECK-NEXT:          [=](cl::sycl::nd_item<3> it) {
+  // CHECK-NEXT:            testKernel(it, karg1int, karg2int, karg3int);
+  // CHECK-NEXT:          });
+  // CHECK-NEXT:      });
+  // CHECK-NEXT:  };
+  testKernel<<<dim3(1), dim3(1, 2)>>>(karg1int, karg2int, karg3int);
+
+  // CHECK: /*  testKernel<<<dim3(1, 2), dim3(1, 2, 3)>>>(karg1int, karg2int, karg3int);*/
+  // CHECK-NEXT:  {
+  // CHECK-NEXT:    syclct::get_default_queue().submit(
+  // CHECK-NEXT:      [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:        cgh.parallel_for<SyclKernelName<class testKernel_{{[a-f0-9]+}}>>(
+  // CHECK-NEXT:          cl::sycl::nd_range<3>((cl::sycl::range<3>(1, 2, 1) * cl::sycl::range<3>(1, 2, 3)), cl::sycl::range<3>(1, 2, 3)),
+  // CHECK-NEXT:          [=](cl::sycl::nd_item<3> it) {
+  // CHECK-NEXT:            testKernel(it, karg1int, karg2int, karg3int);
+  // CHECK-NEXT:          });
+  // CHECK-NEXT:      });
+  // CHECK-NEXT:  };
+  testKernel<<<dim3(1, 2), dim3(1, 2, 3)>>>(karg1int, karg2int, karg3int);
+
+  // CHECK: /*  testKernel<<<griddim.x, griddim.y + 2>>>(karg1int, karg2int, karg3int);*/
+  // CHECK-NEXT:  {
+  // CHECK-NEXT:    syclct::get_default_queue().submit(
+  // CHECK-NEXT:      [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:      cgh.parallel_for<SyclKernelName<class testKernel_{{[a-f0-9]+}}>>(
+  // CHECK-NEXT:        cl::sycl::nd_range<3>((cl::sycl::range<3>(griddim[0], 1, 1) * cl::sycl::range<3>(griddim[1] + 2, 1, 1)), cl::sycl::range<3>(griddim[1] + 2, 1, 1)),
+  // CHECK-NEXT:        [=](cl::sycl::nd_item<3> it) {
+  // CHECK-NEXT:        testKernel(it, karg1int, karg2int, karg3int);
+  // CHECK-NEXT:      });
+  // CHECK-NEXT:    });
+  // CHECK-NEXT:  };
+  testKernel<<<griddim.x, griddim.y + 2>>>(karg1int, karg2int, karg3int);
+
+  float *deviceOutputData = NULL;
+
+  // CHECK: /*  CUDA_CHECK(cudaMalloc((void **)&deviceOutputData, 10 * sizeof(float)));*/
+  // CHECK-NEXT: /*
+  // CHECK-NEXT: SYCLCT1003:0: Translated api does not return error code. (*, 0) is inserted. You may want to rewrite this code
+  // CHECK-NEXT: */
+  CUDA_CHECK(cudaMalloc((void **)&deviceOutputData, 10 * sizeof(float)));
+
+  // copy result from device to host
+  float *h_odata = NULL;
+  float *d_odata = NULL;
+  // CHECK: /*  checkCudaErrors(cudaMemcpy(h_odata, d_odata, sizeof(float) * 4, cudaMemcpyDeviceToHost));*/
+  // CHECK-NEXT:/*
+  // CHECK-NEXT:SYCLCT1003:1: Translated api does not return error code. (*, 0) is inserted. You may want to rewrite this code
+  // CHECK-NEXT:*/
+  checkCudaErrors(cudaMemcpy(h_odata, d_odata, sizeof(float) * 4, cudaMemcpyDeviceToHost));
+}
