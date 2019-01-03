d8822b75f162c7161d8a1465e6ffa3b254db183f
Zack Chen <zack.chen@intel.com>
2019-01-03 10:23:12 +0800

    Add simpleTemplates to pre-checkin test script
    Change-Id: I628e115f579459a51772e221fb43562e5a42f346
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/166356
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Zack Chen <pei-chi.chen@intel.com>

--- clang/tools/syclct/run_syclct_pre-checkin_test_in-ics-env.sh
+++ clang/tools/syclct/run_syclct_pre-checkin_test_in-ics-env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 echo ""
 echo "-------------------------------------------------------------------------------------"
 echo " Please setup ICS env for the test!"
@@ -11,14 +10,30 @@ echo ""
 
 tc_result_folder=`date +%G%m%H%M%S`
 tc_result_folder=tc_$tc_result_folder
-mkdir  $tc_result_folder
-cd  $tc_result_folder
+mkdir -p $tc_result_folder
+cd $tc_result_folder
 
 #old test command
 #tc -r none -x gcc_efi2 -t syclct_sdk_samples/vectorAdd,topologyQuery,cudaOpenMP,cppIntegration,simpleAssert,alignedTypes,simpleMPI,simplePrintf
 
+syclct_sdk_8_samples=(
+  vectorAdd
+  topologyQuery
+  cudaOpenMP
+  cppIntegration
+  simpleAssert
+  alignedTypes
+  simpleMPI
+  simplePrintf
+  template
+  convolutionSeparable
+  FDTD3d
+  simpleTemplates
+)
+IFS=, eval 'syclct_sdk_8_samples_list="syclct_sdk_8_samples/${syclct_sdk_8_samples[*]}"'
+
 #Run test case from syclct_sdk_8_samples
-tc -r none -x gcc_efi2 -t syclct_sdk_8_samples/vectorAdd,topologyQuery,cudaOpenMP,cppIntegration,simpleAssert,alignedTypes,simpleMPI,simplePrintf,template,convolutionSeparable,FDTD3d
+tc -r none -x gcc_efi2 -t $syclct_sdk_8_samples_list
 
 #Run test case from syclct_sdk_92_samples
 #tc -r none -x gcc_efi2 -t syclct_sdk_92_samples/vectorAdd,topologyQuery,cudaOpenMP,cppIntegration,simpleAssert,alignedTypes,simpleMPI,simplePrintf
