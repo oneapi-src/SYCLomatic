5c3174d33a75160e13c9948e46b2ca5a02d05e80
Wang, Zhiming <zhiming.wang@intel.com>
2019-02-18 14:12:49 +0800

    For compatibility layer header files, add proprietary source code notice and an info classification label.
    Change-Id: I0c4a0951964e9acf21ccb5fc5e2c0a5cc4dc54b9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/173389
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/runtime/syclct-rt/include/thrust_memory.hpp
+++ clang/runtime/syclct-rt/include/thrust_memory.hpp
@@ -1,13 +1,22 @@
+/******************************************************************************
+* INTEL CONFIDENTIAL
+*
+* Copyright 2018-2019 Intel Corporation.
+*
+* This software and the related documents are Intel copyrighted materials,
+* and your use of them is governed by the express license under which they
+* were provided to you ("License"). Unless the License provides otherwise,
+* you may not use, modify, copy, publish, distribute, disclose or transmit
+* this software or the related documents without Intel's prior written
+* permission.
+
+* This software and the related documents are provided as is, with no express
+* or implied warranties, other than those that are expressly stated in the
+* License.
+*****************************************************************************/
+
 //===--- thrust_memory.hpp ------------------------------*- C++ -*---===//
-//
-// Copyright (C) 2018 Intel Corporation. All rights reserved.
-//
-// The information and source code contained herein is the exclusive
-// property of Intel Corporation and may not be disclosed, examined
-// or reproduced in whole or in part without explicit written authorization
-// from the company.
-//
-//===-----------------------------------------------------------------===//
+
 #ifndef __THRUST_MEMORY_HPP_
 #define __THRUST_MEMORY_HPP_
 
