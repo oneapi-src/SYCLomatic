7fd0b7041c0dc475aa515987a75245ed86a752ee
zwang36 <zhiming.wang@intel.com>
2019-06-18 13:40:51 +0800

    Update sys_check.sh by adding license and cleaning up comments.
    Change-Id: I52d5f29f44c5d55f200c8494fc9f0f52687a9937
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/193955
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/tools/syclct/sys_check.sh
+++ clang/tools/syclct/sys_check.sh
@@ -1,30 +1,27 @@
+################################################################################
 #
+# Copyright (C) 2019 Intel Corporation. All rights reserved.
+#
+# The information and source code contained herein is the exclusive property of
+# Intel Corporation and may not be disclosed, examined or reproduced in whole or
+# in part without explicit written authorization from the company.
+#
+################################################################################
+
 # Compatibility Tool System Check
 #
 # a. Detect CUDA toolkit.
 # b. profit
-
-
-#this syscheck script is provided as a model for every OneAPI component
-#each syscheck script should do five things:
-#
-# 1 - load the common.sh file   to gain the common API functions
-# 2 - initialize an ERRORSTATE value to 0,
-# 3 - return ERRORSTATE at the end of the shell script
-# 4 - 'echo'   any problems out to the user. (and adjust ERRORSTATE if so)
-# 5 - 'speak'  other messages to the user.
 
 # 'echo' should be used for outputting messages in response to errors. 'echo' is always output.
 # 'speak' outputs only if the -v verbose flag it used. Affirmative messages ( "Everything OK!" ) should
 #    use 'speak', as well as advice, informative messages, or possibly longer explanations of an error. (eg "your cmake installation is not the latest" )
 #
 #  colors for use with 'echo' and 'speak' are defined.  See  common.sh for list and usage example.
-
+#
 # any arguments passed to the root syscheck script are passed on to this one.
 
 # ERRORSTATE: 0 if OK, 1 if not.
-
-
 
 #load common file
 source common.sh $@
@@ -32,14 +29,10 @@ source common.sh $@
 #every syscheck script should set up an ERRORSTATE variable and return it on completion.
 ERRORSTATE=0
 
-
 # CUDA TOOLKIT
-# the compatibility tool requires the cuda toolkit to be installed. There are various ways this could be done.
-# could look for nvcc on the PATH, or search for 'cuda' on the LD_LIBRARY_PATH
-# opting for the latter,
-if [ -z $(grep 'cuda' <<< $LD_LIBRARY_PATH) ]; then
-	echo -e "The OneAPI Compatibility Tool requires the Nvidia CUDA Toolkit to be installed."
-	ERRORSTATE=1
+if [ -z $(which nvcc) ]; then
+    echo -e "The OneAPI Compatibility Tool requires the Nvidia CUDA Toolkit to be installed."
+    ERRORSTATE=1
 fi
 
 if [ -z $(which python) ]; then
@@ -49,7 +42,7 @@ fi
 
 
 if [ $ERRORSTATE -eq 0 ]; then
-	speak "OK"
+    speak "OK"
 fi
 
 #always return ERRORSTATE ( which is 0 if no error )
