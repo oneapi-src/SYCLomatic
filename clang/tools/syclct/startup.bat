eda47015170537170c32bca93c12e448ca272311
zwang36 <zhiming.wang@intel.com>
2019-07-19 14:03:10 +0800

    Add sys_check.bat and startup.bat to support windows.
    startup.bat is only placehold, the conntent has depend on
    DPC++ pakcage. Ref to Linux version.
    sys_check.bat only check SDK.
    
    Change-Id: Ia3073732c58b8db913f5c1b42afd752985a5e060
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/199645
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>

---
+++ clang/tools/syclct/startup.bat
@@ -0,0 +1,11 @@
+::################################################################################
+::#
+::# Copyright (C) 2019 Intel Corporation. All rights reserved.
+::#
+::# The information and source code contained herein is the exclusive property of
+::# Intel Corporation and may not be disclosed, examined or reproduced in whole or
+::# in part without explicit written authorization from the company.
+::#
+::################################################################################
+::
+::
---
+++ clang/tools/syclct/sys_check.bat
@@ -0,0 +1,35 @@
+::################################################################################
+::#
+::# Copyright (C) 2019 Intel Corporation. All rights reserved.
+::#
+::# The information and source code contained herein is the exclusive property of
+::# Intel Corporation and may not be disclosed, examined or reproduced in whole or
+::# in part without explicit written authorization from the company.
+::#
+::################################################################################
+:: this syscheck script is provided as a model for every OneAPI component
+:: each syscheck script should do four things:
+::
+
+@echo off
+
+call common.bat :speak   This Is A Message You Will See In Verbose Mode
+
+:: every syscheck script should set up an ERRORSTATE variable and return it on completion.
+setlocal
+set /A ERRORSTATE=1
+nvcc --version > nul 2>nul
+set ret=%ERRORLEVEL%
+if "%ret%" NEQ "0" (
+    echo  Warning: The oneAPI Compatibility Tool requires the Nvidia CUDA Toolkit to be installed.
+    set ERRORSTATE=1
+) else (
+    set ERRORSTATE=0
+)
+if /I "%ERRORSTATE%" NEQ "0" (
+    echo  Warning: The oneAPI Compatibility Tool component unsafe to operate.
+)
+
+::exit with the %ERRORSTATE%
+:: use /B flag, or the exit will prevent other sys_checks from running.
+exit /B %ERRORSTATE%
