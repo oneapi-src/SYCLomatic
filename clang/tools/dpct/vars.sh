61b2c1fbbc1b1e1faba21aa20e68263ed1378c49
zwang36 <zhiming.wang@intel.com>
2019-08-15 16:35:12 +0800

    Fix the license header in Compatibility tool.
    Change-Id: I5f4080222e2371fec71661a3da84b80143fce65d
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/204531
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/tools/dpct/vars.sh
+++ clang/tools/dpct/vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ################################################################################
 #
-# Copyright (C) 2018 Intel Corporation. All rights reserved.
+# Copyright (C) 2018 - 2019 Intel Corporation. All rights reserved.
 #
 # The information and source code contained herein is the exclusive property of
 # Intel Corporation and may not be disclosed, examined or reproduced in whole or
