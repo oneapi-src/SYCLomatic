12be103f25cdc7718b20ae405bf01551ef306485
Zhenhao Tang <zhenhao.tang@intel.com>
2019-10-09 11:23:14 +0800

    [CTST-926] Add a test case for the logic to insert dpct headers
    Change-Id: I6cd1226bbc94495869247757adf94844830e916e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/213917
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>

---
+++ clang/test/dpct/test-dpct-header.cu
@@ -0,0 +1,52 @@
+// RUN: dpct -out-root %T %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only -DCUDA
+// RUN: FileCheck --match-full-lines --input-file %T/test-dpct-header.dp.cpp %s
+
+// CHECK: #include <CL/sycl.hpp>
+// CHECK-NEXT: #include <dpct/dpct.hpp>
+// CHECK-NEXT: #include "inc/header.h"
+#include "inc/header.h"
+
+// CHECK: #ifdef CUDA
+// CHECK-NEXT: void foo() {
+// CHECK-NEXT: }
+#ifdef CUDA
+__global__ void foo() {
+}
+#elif defined(OPENMP)
+void foo() {
+}
+#else
+void foo() {
+}
+#endif
+
+int main() {
+#ifdef CUDA
+  // CHECK: {
+  // CHECK-NEXT:   dpct::get_default_queue_wait().submit(
+  // CHECK-NEXT:     [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:       cgh.parallel_for<dpct_kernel_name<class foo_{{[a-z0-9]+}}>>(
+  // CHECK-NEXT:         cl::sycl::nd_range<3>((cl::sycl::range<3>(1, 1, 1) * cl::sycl::range<3>(1, 1, 1)), cl::sycl::range<3>(1, 1, 1)),
+  // CHECK-NEXT:         [=](cl::sycl::nd_item<3> item_ct1) {
+  // CHECK-NEXT:           foo();
+  // CHECK-NEXT:         });
+  // CHECK-NEXT:     });
+  // CHECK-NEXT: }
+
+  // CHECK: {
+  // CHECK-NEXT:   dpct::get_default_queue_wait().submit(
+  // CHECK-NEXT:     [&](cl::sycl::handler &cgh) {
+  // CHECK-NEXT:       cgh.parallel_for<dpct_kernel_name<class bar_{{[a-z0-9]+}}>>(
+  // CHECK-NEXT:         cl::sycl::nd_range<3>((cl::sycl::range<3>(1, 1, 1) * cl::sycl::range<3>(1, 1, 1)), cl::sycl::range<3>(1, 1, 1)),
+  // CHECK-NEXT:         [=](cl::sycl::nd_item<3> item_ct1) {
+  // CHECK-NEXT:           bar();
+  // CHECK-NEXT:         });
+  // CHECK-NEXT:     });
+  // CHECK-NEXT: }
+  foo<<<1, 1, 1>>>();
+  bar<<<1, 1, 1>>>();
+#else
+  foo();
+  bar();
+#endif
+}
