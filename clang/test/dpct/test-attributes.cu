113e2b648859c76863cd4fd4b2544e4bea6004e9
Zhiming Wang <zhiming.wang@intel.com>
2019-11-26 01:23:58 +0800

    [NFC] Fix test case which failed in Ubutu 16.04.
    Change-Id: Ic4f090cf19c57c80f95d7304915bee1c45c21e13
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/221814
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/test/dpct/test-attributes.cu
+++ clang/test/dpct/test-attributes.cu
@@ -1,4 +1,4 @@
-// RUN: dpct -out-root %T %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: dpct -out-root %T %s --cuda-include-path="%cuda-path/include" --  -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/test-attributes.dp.cpp --match-full-lines %s
 #include "cuda.h"
 
