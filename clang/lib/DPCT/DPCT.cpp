92759f3abcb5d08b0d7ac34dfcc05acbca2d56d1
Wang, Zhiming <zhiming.wang@intel.com>
2020-05-15 01:41:49 -0700

    Revert "Fix a bug that marcro "__CUDA_ARCH__" is not defined"
    This reverts commit 82c9453a055c2ebfd814e52dad4fa87a30c50f7d.
    
    Change-Id: I187194ed92a6f8f1fe7cc9573398dc2865182fe5
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/248959
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/lib/DPCT/DPCT.cpp
+++ clang/lib/DPCT/DPCT.cpp
@@ -1007,8 +1007,6 @@ int run(int argc, const char **argv) {
 
     Tool.appendArgumentsAdjuster(
         getInsertArgumentAdjuster("-x", ArgumentInsertPosition::BEGIN));
-    Tool.appendArgumentsAdjuster(getInsertArgumentAdjuster(
-        "-D__CUDA_ARCH__=400", ArgumentInsertPosition::END));
   }
   SetSDKIncludePath(CudaPath);
 
