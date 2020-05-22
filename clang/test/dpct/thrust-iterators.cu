99d08715baa02a1b80f1efe70423de47b4e46873
Zhiwei, Jiang <zhiwei.jiang@intel.com>
2020-05-22 16:24:08 +0800

    [CTST-1444] Fix lit on windows
    Change-Id: I45d431b230f5b01cdad40032a16ec7925fb56d92
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/250584
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/test/dpct/thrust-iterators.cu
+++ clang/test/dpct/thrust-iterators.cu
@@ -1,6 +1,6 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0
-// RUN: dpct --format-range=none -out-root %T %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only --std=c++11
+// RUN: dpct --format-range=none -out-root %T %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only --std=c++14
 // RUN: FileCheck --input-file %T/thrust-iterators.dp.cpp --match-full-lines %s
 // CHECK: #include <CL/sycl.hpp>
 // CHECK-NEXT: #include <dpct/dpct.hpp>
