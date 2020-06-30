950804565ec8c836e95bf2a40ca7c2f61ed38a9e
Zhiwei, Jiang <zhiwei.jiang@intel.com>
2020-06-30 14:48:14 +0800

    [NFC] Fix lit tests.
    cublasHgemmBatched is not supported in SDK8.0
    
    Change-Id: I33d6eecbfcc9f68f2273aa63b6efa5f0281e49a3
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/257391
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>

---
+++ clang/test/dpct/cublasHgemmBatched.cu
@@ -0,0 +1,26 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1
+// UNSUPPORTED: v8.0, v9.0, v9.1
+// RUN: dpct --format-range=none -out-root %T %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/cublasHgemmBatched.dp.cpp --match-full-lines %s
+#include <cstdio>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+cublasHandle_t handle;
+int N = 275;
+
+int main() {
+
+  const __half **d_A_H;
+  const __half **d_B_H;
+  __half **d_C_H;
+  __half alpha_H;
+  __half beta_H;
+
+
+  cublasOperation_t trans3 = CUBLAS_OP_N;
+  // CHECK: int64_t m_ct{{[0-9]+}} = N, n_ct{{[0-9]+}} = N, k_ct{{[0-9]+}} = N, lda_ct{{[0-9]+}} = N, ldb_ct{{[0-9]+}} = N, ldc_ct{{[0-9]+}} = N, group_size_ct{{[0-9]+}} = 10;
+  // CHECK-NEXT: sycl::half alpha_ct{{[0-9]+}} = dpct::get_value(&alpha_H, *handle), beta_ct{{[0-9]+}} = dpct::get_value(&beta_H, *handle);
+  // CHECK-NEXT: mkl::blas::gemm_batch(*handle, &trans3, &trans3, &m_ct{{[0-9]+}}, &n_ct{{[0-9]+}}, &k_ct{{[0-9]+}}, &alpha_ct{{[0-9]+}}, d_A_H, &lda_ct{{[0-9]+}}, d_B_H, &ldb_ct{{[0-9]+}}, &beta_ct{{[0-9]+}}, d_C_H, &ldc_ct{{[0-9]+}}, 1, &group_size_ct{{[0-9]+}}, {});
+  cublasHgemmBatched(handle, trans3, trans3, N, N, N, &alpha_H, d_A_H, N, d_B_H, N, &beta_H, d_C_H, N, 10);
+}
