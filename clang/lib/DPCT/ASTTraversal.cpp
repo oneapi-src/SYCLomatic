2653c1a1711c508522853566ac8b978ba8a077ef
zwang36 <zhiming.wang@intel.com>
2020-07-15 10:15:32 +0800

    [CTST-1603] Fix Klocwork critical issues in syclct files 20200713.
    Change-Id: I04055a1f632d5b6d6004c649ad69b6c019999589
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/259964
    Reviewed-by: Zhang Ziran <ziran.zhang@intel.com>
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -9994,7 +9994,7 @@ void TextureRule::replaceResourceDataExpr(const MemberExpr *ME,
   if (!ME)
     return;
   auto TopMember = getParentMemberExpr(ME);
-  if (!ME)
+  if (!TopMember)
     return;
   auto ResName = ME->getMemberNameInfo().getAsString();
   if (ResName == "array") {
