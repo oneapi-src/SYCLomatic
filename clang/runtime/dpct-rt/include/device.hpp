69a7c60e4a0ba940e4f6092c9cf35bee0d2c0187
Zhang Ziran <ziran.zhang@intel.com>
2020-10-10 17:42:04 +0800

    [CTST-1805] Throw std::runtime_error instead of std::string in device.hpp
    std::runtime_error is more friendly for user to catch where error happen.
    
    Change-Id: I7f602af85a9ee512ca3fc934c9192b97b051cf2c
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/276358
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/runtime/dpct-rt/include/device.hpp
+++ clang/runtime/dpct-rt/include/device.hpp
@@ -350,7 +350,7 @@ public:
   device_ext &cpu_device() const {
     std::lock_guard<std::mutex> lock(m_mutex);
     if (_cpu_device == -1) {
-      throw std::string("no valid cpu device");
+      throw std::runtime_error("no valid cpu device");
     } else {
       return *_devs[_cpu_device];
     }
@@ -412,7 +412,7 @@ private:
   }
   void check_id(unsigned int id) const {
     if (id >= _devs.size()) {
-      throw std::string("invalid device id");
+      throw std::runtime_error("invalid device id");
     }
   }
   std::vector<std::shared_ptr<device_ext>> _devs;
