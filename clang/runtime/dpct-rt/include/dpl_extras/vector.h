b125258093c5a0468a6809658720be37fd560480
zwang36 <zhiming.wang@intel.com>
2020-10-14 15:50:46 +0800

    [CTST-1803] Update Copyright and license header for helper functions.
    Change-Id: I8a83045d83214e865c2acade9484f519990d398b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/277438
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/runtime/dpct-rt/include/dpl_extras/vector.h
+++ clang/runtime/dpct-rt/include/dpl_extras/vector.h
@@ -1,18 +1,10 @@
-/******************************************************************************
-*
-* Copyright 2019 - 2020 Intel Corporation.
-*
-* This software and the related documents are Intel copyrighted materials,
-* and your use of them is governed by the express license under which they
-* were provided to you ("License"). Unless the License provides otherwise,
-* you may not use, modify, copy, publish, distribute, disclose or transmit
-* this software or the related documents without Intel's prior written
-* permission.
-
-* This software and the related documents are provided as is, with no express
-* or implied warranties, other than those that are expressly stated in the
-* License.
-*****************************************************************************/
+//==---- vector.h ---------------------------------*- C++ -*----------------==//
+//
+// Copyright (C) 2019 - 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef __DPCT_VECTOR_H__
 #define __DPCT_VECTOR_H__
