3d729fd2c2d827b0e1dd33f8774117482f757307
Chung-Hao(Andy) Huang <andy.huang@intel.com>
2021-06-04 11:09:02 +0800

    [CTST-2161] Update namespace from detail::bit_cast to bit_cast
    Change-Id: Ia2eab40ec3094aad2ce380ab9e4cf111e0a2b4b5
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/330821
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>
    Tested-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/test/dpct/cuda_typecast.cu
+++ clang/test/dpct/cuda_typecast.cu
@@ -8,23 +8,23 @@
 __device__ void foo() {
   long long a = 100000;
   double b = 2.5;
-  // CHECK: double c = sycl::detail::bit_cast<double>(a);
+  // CHECK: double c = sycl::bit_cast<double>(a);
   double c = __longlong_as_double(a);
-  // CHECK: long long d = sycl::detail::bit_cast<long long>(b);
+  // CHECK: long long d = sycl::bit_cast<long long>(b);
   long long d = __double_as_longlong(b);
 
   int e = 23;
   float f = 45.0;
-  // CHECK: float g = sycl::detail::bit_cast<float>(e);
+  // CHECK: float g = sycl::bit_cast<float>(e);
   float g = __int_as_float(e);
-  // CHECK: int h = sycl::detail::bit_cast<int>(f);
+  // CHECK: int h = sycl::bit_cast<int>(f);
   int h = __float_as_int(f);
 
   unsigned i = 23;
   float j = 45.0;
-  // CHECK: float k = sycl::detail::bit_cast<float>(i);
+  // CHECK: float k = sycl::bit_cast<float>(i);
   float k = __uint_as_float(i);
-  // CHECK: unsigned l = sycl::detail::bit_cast<unsigned int>(j);
+  // CHECK: unsigned l = sycl::bit_cast<unsigned int>(j);
   unsigned l = __float_as_uint(j);
 }
 
