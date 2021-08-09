b052069853d7f4f4671d7d94ec068d66d897e1cc
Chung-Hao(Andy) Huang <andy.huang@intel.com>
2021-08-09 10:25:04 +0800

    [CTST-2236][Part 2] Using item.group() instead of group_barrier() in helper function
    Deprecation of item.group() is reverted
    CMPLRLLVM-29946
    
    Change-Id: I5d8c9354ebb86f5bea99fa0ef8aad376514a6036
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/336374
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Wang, Zhiming <zhiming.wang@intel.com>

--- clang/test/dpct/helper_files_ref/include/util.hpp
+++ clang/test/dpct/helper_files_ref/include/util.hpp
@@ -250,7 +250,7 @@ nd_range_barrier(sycl::nd_item<dimensions> item,
   unsigned int num_groups = item.get_group_range(2) * item.get_group_range(1) *
                             item.get_group_range(0);
 
-  cl::sycl::group_barrier(item.get_group());
+  item.barrier();
 
   if (item.get_local_linear_id() == 0) {
     unsigned int inc = 1;
@@ -267,7 +267,7 @@ nd_range_barrier(sycl::nd_item<dimensions> item,
       ;
   }
 
-  cl::sycl::group_barrier(item.get_group());
+  item.barrier();
 }
 
 /// Synchronize work items from all work groups within a DPC++ kernel.
@@ -286,7 +286,7 @@ nd_range_barrier(sycl::nd_item<1> item,
                      cl::sycl::access::address_space::global_space> &counter) {
   unsigned int num_groups = item.get_group_range(0);
 
-  cl::sycl::group_barrier(item.get_group());
+  item.barrier();
 
   if (item.get_local_linear_id() == 0) {
     unsigned int inc = 1;
@@ -302,7 +302,7 @@ nd_range_barrier(sycl::nd_item<1> item,
       ;
   }
 
-  cl::sycl::group_barrier(item.get_group());
+  item.barrier();
 }
 }
 } // namespace dpct
