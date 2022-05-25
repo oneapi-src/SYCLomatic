2051bd09e5ee11979f56a60796c17cb42204308e
Ni, Wenhui <wenhui.ni@intel.com>
2022-05-25 13:52:59 +0800

    [SYCLomatic] Add support for cuda-11.7
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- clang/lib/DPCT/DPCT.cpp
+++ clang/lib/DPCT/DPCT.cpp
@@ -1224,7 +1224,8 @@ int runDPCT(int argc, const char **argv) {
       (SDKVersionMajor == 11 && SDKVersionMinor == 3) ||
       (SDKVersionMajor == 11 && SDKVersionMinor == 4) ||
       (SDKVersionMajor == 11 && SDKVersionMinor == 5) ||
-      (SDKVersionMajor == 11 && SDKVersionMinor == 6)) {
+      (SDKVersionMajor == 11 && SDKVersionMinor == 6) ||
+      (SDKVersionMajor == 11 && SDKVersionMinor == 7)) {
     Tool.appendArgumentsAdjuster(
         getInsertArgumentAdjuster("-fms-compatibility-version=19.21.27702.0",
                                   ArgumentInsertPosition::BEGIN));
