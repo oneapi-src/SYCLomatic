05ceacff8c4f6e27ee935afbca3f21399ffeacbe
Cui, Dele <dele.cui@intel.com>
2022-06-09 08:43:06 +0800

    [SYCLomatic] Fix 4 thrust LIT test fails when using CUDA header file version >=11.2 (#66)
    Signed-off-by: Cui, Dele <dele.cui@intel.com>

--- clang/test/dpct/thrust-for-h2o4gpu.cu
+++ clang/test/dpct/thrust-for-h2o4gpu.cu
@@ -1,6 +1,6 @@
 // UNSUPPORTED: cuda-8.0
 // UNSUPPORTED: v8.0
-// RUN: dpct --format-range=none -out-root %T/thrust-for-h2o4gpu %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only -fno-delayed-template-parsing
+// RUN: dpct --format-range=none -out-root %T/thrust-for-h2o4gpu %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only -fno-delayed-template-parsing -ferror-limit=50
 // RUN: FileCheck --input-file %T/thrust-for-h2o4gpu/thrust-for-h2o4gpu.dp.cpp --match-full-lines %s
 
 
