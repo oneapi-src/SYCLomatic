c1bd593b0fb22b738a95f8d96850b9acb2f66fe3
Ni, Wenhui <wenhui.ni@intel.com>
2022-06-29 01:35:46 +0800

    fix test-api
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- clang/test/dpct/test_api_level/AtomicHelper/api_test2.cu
+++ clang/test/dpct/test_api_level/AtomicHelper/api_test2.cu
@@ -1,7 +1,11 @@
+// UNSUPPORTED: v7.0, v7.5, v8.0, v9.0, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-7.0, cuda-7.5, cuda-8.0, cuda-9.0, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/AtomicHelper/api_test2_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: grep "IsCalled" %T/AtomicHelper/api_test2_out/MainSourceFiles.yaml | wc -l > %T/AtomicHelper/api_test2_out/count.txt
 // RUN: FileCheck --input-file %T/AtomicHelper/api_test2_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/AtomicHelper/api_test2_out
+
+// CHECK: 1
 
 #include <cuda/std/atomic>
 
--- clang/test/dpct/test_api_level/AtomicHelper/api_test3.cu
+++ clang/test/dpct/test_api_level/AtomicHelper/api_test3.cu
@@ -1,7 +1,11 @@
+// UNSUPPORTED: v7.0, v7.5, v8.0, v9.0, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-7.0, cuda-7.5, cuda-8.0, cuda-9.0, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/AtomicHelper/api_test3_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: grep "IsCalled" %T/AtomicHelper/api_test3_out/MainSourceFiles.yaml | wc -l > %T/AtomicHelper/api_test3_out/count.txt
 // RUN: FileCheck --input-file %T/AtomicHelper/api_test3_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/AtomicHelper/api_test3_out
+
+// CHECK: 1
 
 #include <cuda/atomic>
 
@@ -13,11 +17,11 @@
 
 
 int main(){
-    cuda::atomic<int> a(0);
-    int ans = a.fetch_add(1);
-    ans = a.fetch_sub(-1);
-    ans = a.fetch_and(2);
-    ans = a.fetch_or(1);
-    ans = a.fetch_xor(2);
-    return 0;
+  cuda::atomic<int> a(0);
+  int ans = a.fetch_add(1);
+  ans = a.fetch_sub(-1);
+  ans = a.fetch_and(2);
+  ans = a.fetch_or(1);
+  ans = a.fetch_xor(2);
+  return 0;
 }
