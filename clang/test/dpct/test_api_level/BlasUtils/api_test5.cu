161c682d9aaf85748770913aedd189805d653312
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-06-29 11:01:40 +0800

    Fix lit
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/BlasUtils/api_test5.cu
+++ clang/test/dpct/test_api_level/BlasUtils/api_test5.cu
@@ -3,7 +3,7 @@
 // RUN: FileCheck --input-file %T/BlasUtils/api_test5_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/BlasUtils/api_test5_out
 
-// CHECK: 32
+// CHECK: 33
 
 #include "cublas_v2.h"
 
