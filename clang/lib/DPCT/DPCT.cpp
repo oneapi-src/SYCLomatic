a1a5fc720e4cb58ec86e8c68b5a974a47776be63
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-07-29 10:10:33 +0800

    Clean ParentMapContext before matching AST node
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/DPCT.cpp
+++ clang/lib/DPCT/DPCT.cpp
@@ -545,6 +545,7 @@ public:
         Context.getDiagnostics().getClient()->getNumErrors() > 0) {
       return;
     }
+    Context.getParentMapContext().clear();
     // The migration process is separated into two stages:
     // 1) Analysis of AST and identification of applicable migration rules
     // 2) Generation of actual textual Replacements
