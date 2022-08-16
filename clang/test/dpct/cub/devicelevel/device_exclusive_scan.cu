4e5a9acc60484d4ab65505cde8b08aa01ba3a177
Wang, Yihan <yihan.wang@intel.com>
2022-08-16 10:08:16 +0800

    [SYCLomatic] Support Encode, Unique, InclusiveScan and ExclusiveScan
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

---
+++ clang/test/dpct/cub/devicelevel/device_exclusive_scan.cu
@@ -0,0 +1,94 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_exclusive_scan %S/device_exclusive_scan.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/devicelevel/device_exclusive_scan/device_exclusive_scan.dp.cpp --match-full-lines %s
+
+// CHECK:#include <oneapi/dpl/execution>
+// CHECK:#include <oneapi/dpl/algorithm>
+#include <cuda_runtime.h>
+#include <cub/cub.cuh>
+#include <stdio.h>
+
+#define N 10
+
+struct CustomSum {
+    template <typename T>
+    __device__ __forceinline__ T operator()(const T &a, const T &b) const {
+        return a + b;
+    }
+};
+
+// void test_1() {
+// dpct::device_ext &dev_ct1 = dpct::get_current_device();
+// sycl::queue &q_ct1 = dev_ct1.default_queue();
+// int n = 10;
+// int *device_in = nullptr;
+// int *device_out = nullptr;
+// int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// int host_out[10];
+// device_in = sycl::malloc_device<int>(n, q_ct1);
+// device_out = sycl::malloc_device<int>(n, q_ct1);
+// q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
+// DPCT1026:{{.*}}
+// oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + op, device_out, 0, op);
+// q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
+// sycl::free(device_in, q_ct1);
+// sycl::free(device_out, q_ct1);
+// }
+void test_1() {
+  int *device_in = nullptr;
+  int *device_out = nullptr;
+  int *device_tmp = nullptr;
+  size_t n_device_tmp = 0;
+  int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int host_out[10];
+  CustomSum op;
+  cudaMalloc((void **)&device_in, N * sizeof(int));
+  cudaMalloc((void **)&device_out, N * sizeof(int));
+  cudaMemcpy(device_in, (void *)host_in, sizeof(host_in), cudaMemcpyHostToDevice);
+  cub::DeviceScan::ExclusiveScan(device_tmp, n_device_tmp, device_in, device_out, op, 0, N);
+  cudaMalloc((void **)&device_tmp, n_device_tmp);
+  cub::DeviceScan::ExclusiveScan((void *)device_tmp, n_device_tmp, device_in, device_out, op, 0, N);
+  cudaMemcpy((void *)host_out, (void *)device_out, sizeof(host_out), cudaMemcpyDeviceToHost);
+  cudaFree(device_in);
+  cudaFree(device_out);
+  cudaFree(device_tmp);
+}
+
+// void test_2() {
+// dpct::device_ext &dev_ct1 = dpct::get_current_device();
+// sycl::queue &q_ct1 = dev_ct1.default_queue();
+// int n = 10;
+// int *device_in = nullptr;
+// int *device_out = nullptr;
+// int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// int host_out[10];
+// device_in = sycl::malloc_device<int>(n, q_ct1);
+// device_out = sycl::malloc_device<int>(n, q_ct1);
+// q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
+// DPCT1027:{{.*}}
+// 0, 0;
+// oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + op, device_out, 0, op);
+// q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
+// sycl::free(device_in, q_ct1);
+// sycl::free(device_out, q_ct1);
+// }
+void test_2() {
+ int *device_in = nullptr;
+  int *device_out = nullptr;
+  int *device_tmp = nullptr;
+  size_t n_device_tmp = 0;
+  int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int host_out[10];
+  CustomSum op;
+  cudaMalloc((void **)&device_in, N * sizeof(int));
+  cudaMalloc((void **)&device_out, N * sizeof(int));
+  cudaMemcpy(device_in, (void *)host_in, sizeof(host_in), cudaMemcpyHostToDevice);
+  cub::DeviceScan::ExclusiveScan(device_tmp, n_device_tmp, device_in, device_out, op, 0, N), 0;
+  cudaMalloc((void **)&device_tmp, n_device_tmp);
+  cub::DeviceScan::ExclusiveScan((void *)device_tmp, n_device_tmp, device_in, device_out, op, 0, N);
+  cudaMemcpy((void *)host_out, (void *)device_out, sizeof(host_out), cudaMemcpyDeviceToHost);
+  cudaFree(device_in);
+  cudaFree(device_out);
+  cudaFree(device_tmp);
+}
