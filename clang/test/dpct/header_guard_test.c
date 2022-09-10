3320eee629d090d0ca8a8fef93281c13252a09d5
Lu, John <john.lu@intel.com>
2022-09-10 19:26:35 +0000

    Copy UNSUPPORTED directives from thrust-iterators.cu to header_guard_test.c
    Signed-off-by: Lu, John <john.lu@intel.com>

--- clang/test/dpct/header_guard_test.c
+++ clang/test/dpct/header_guard_test.c
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0
 // ------ make test directory
 // RUN: cd %T
 // RUN: rm -rf header_guard_test
