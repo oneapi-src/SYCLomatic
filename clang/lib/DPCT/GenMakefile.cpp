6b7221870bbb7005158d8478fc4d9ee9cd353fc8
LU-JOHN <111294400+LU-JOHN@users.noreply.github.com>
2022-09-12 13:02:00 -0500

    Update clang/lib/DPCT/GenMakefile.cpp
    Co-authored-by: 0x12CC <68250218+0x12CC@users.noreply.github.com>

--- clang/lib/DPCT/GenMakefile.cpp
+++ clang/lib/DPCT/GenMakefile.cpp
@@ -234,8 +234,8 @@ static void getCompileInfo(
         // Keep optimization level same as original compile command.
         NewOptions += Option + " ";
       } else if (Option == "-msse4.1" ||
-		 Option == "-mavx512vl") {
-	// Keep some options from original compile command.
+                 Option == "-mavx512vl") {
+        // Keep some options from original compile command.
         NewOptions += Option + " ";
       }
     }
