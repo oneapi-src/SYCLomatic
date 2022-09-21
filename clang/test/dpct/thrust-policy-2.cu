85b0ee633e545eea7de81480c7ab97e64ad7293f
zhiweij1 <zhiwei.jiang@intel.com>
2022-09-21 10:07:39 +0800

    [SYCLomatic] Fix the un-migrated thrust API due to the replacements conflict of thrust functor migrations (#213)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/thrust-policy-2.cu
+++ clang/test/dpct/thrust-policy-2.cu
@@ -4,10 +4,39 @@
 // RUN: FileCheck %s --match-full-lines --input-file %T/thrust-policy-2/thrust-policy-2.dp.cpp
 
 #include <thrust/execution_policy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/scan.h>
+#include <thrust/iterator/reverse_iterator.h>
+#include <thrust/functional.h>
 
-class MyAlloctor {};
-void foo(cudaStream_t stream) {
+class MyAlloctor {
+public:
+ typedef char value_type;
+ char* allocate(std::ptrdiff_t size) { return nullptr; }
+ void deallocate(char* p, size_t size) {}
+};
+
+template<typename T>
+void foo() {
+  cudaStream_t stream;
   MyAlloctor thrust_allocator;
   // CHECK: auto p = oneapi::dpl::execution::make_device_policy(*stream);
   auto p = thrust::cuda::par(thrust_allocator).on(stream);
+
+  int num = 10;
+  thrust::device_ptr<T> data_a;
+  thrust::device_ptr<T> data_b;
+
+  // CHECK: oneapi::dpl::inclusive_scan_by_segment(p, oneapi::dpl::make_reverse_iterator(data_a + num), oneapi::dpl::make_reverse_iterator(data_a), oneapi::dpl::make_reverse_iterator(data_b + num), oneapi::dpl::make_reverse_iterator(data_b + num), oneapi::dpl::equal_to<T>(), oneapi::dpl::maximum<T>());
+  thrust::inclusive_scan_by_key(
+    p,
+    thrust::make_reverse_iterator(data_a + num),
+    thrust::make_reverse_iterator(data_a),
+    thrust::make_reverse_iterator(data_b + num),
+    thrust::make_reverse_iterator(data_b + num),
+    thrust::equal_to<T>(),
+    thrust::maximum<T>()
+  );
 }
+
+template void foo<int>();
