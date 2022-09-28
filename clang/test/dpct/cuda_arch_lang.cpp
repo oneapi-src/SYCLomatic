e321827df0a6aef6c2e0f27ca70628db69c49c55
Cai, Justin <justin.cai@intel.com>
2022-09-28 13:07:03 -0700

    fix cuda include path 2

--- clang/test/dpct/cuda_arch_lang.cpp
+++ clang/test/dpct/cuda_arch_lang.cpp
@@ -2,7 +2,7 @@
 // RUN: not dpct --format-range=none --out-root=%T/cuda_arch_lang --stop-on-parse-err --cuda-include-path="%cuda-path/include" %s -- -x c++ > %T/cuda_arch_lang/output 2>&1
 // RUN: cat %T/cuda_arch_lang/output
 // RUN: python -c "assert 'no CUDA code detected' in input()" < %T/cuda_arch_lang/output
-// RUN: dpct --format-range=none --out-root=%T/cuda_arch_lang --stop-on-parse-err %s -- -x cuda
+// RUN: dpct --format-range=none --out-root=%T/cuda_arch_lang --stop-on-parse-err --cuda-include-path="%cuda-path/include" %s -- -x cuda
 // RUN: FileCheck --input-file=%T/cuda_arch_lang/cuda_arch_lang.cpp.dp.cpp %s 
 
 // CHECK: #ifdef DPCT_COMPATIBILITY_TEMP
