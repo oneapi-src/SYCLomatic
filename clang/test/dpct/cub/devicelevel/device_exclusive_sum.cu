0f7047491e9dfbf0dd35cee484a080e395468ab5
Wang, Yihan <yihan.wang@intel.com>
2022-09-29 10:47:49 +0800

    [SYCLomatic] Fix missing 'typename' prior to dependent type name 'std::iterator_traits<decltype(d_out)>::value_type' and support zero initializer
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/devicelevel/device_exclusive_sum.cu
+++ clang/test/dpct/cub/devicelevel/device_exclusive_sum.cu
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_exclusive_sum %S/device_exclusive_sum.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/devicelevel/device_exclusive_sum/device_exclusive_sum.dp.cpp --match-full-lines %s
+// RUN: FileCheck --input-file %T/devicelevel/device_exclusive_sum/device_exclusive_sum.dp.cpp %s
 
 // CHECK:#include <oneapi/dpl/execution>
 // CHECK:#include <oneapi/dpl/algorithm>
@@ -9,23 +9,23 @@
 #include <cub/cub.cuh>
 #include <stdio.h>
 
-// void test_1() {
-// dpct::device_ext &dev_ct1 = dpct::get_current_device();
-// sycl::queue &q_ct1 = dev_ct1.default_queue();
-// int n = 10;
-// int *device_in = nullptr;
-// int *device_out = nullptr;
-// int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-// int host_out[10];
-// device_in = sycl::malloc_device<int>(n, q_ct1);
-// device_out = sycl::malloc_device<int>(n, q_ct1);
-// q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
-// DPCT1026:{{.*}}
-// oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + n, device_out, 0);
-// q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
-// sycl::free(device_in, q_ct1);
-// sycl::free(device_out, q_ct1);
-// }
+// CHECK:void test_1() {
+// CHECK:dpct::device_ext &dev_ct1 = dpct::get_current_device();
+// CHECK:sycl::queue &q_ct1 = dev_ct1.default_queue();
+// CHECK:int n = 10;
+// CHECK:int *device_in = nullptr;
+// CHECK:int *device_out = nullptr;
+// CHECK:int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// CHECK:int host_out[10];
+// CHECK:device_in = sycl::malloc_device<int>(n, q_ct1);
+// CHECK:device_out = sycl::malloc_device<int>(n, q_ct1);
+// CHECK:q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
+// CHECK:DPCT1026:{{.*}}
+// CHECK:oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + n, device_out, 0);
+// CHECK:q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
+// CHECK:sycl::free(device_in, q_ct1);
+// CHECK:sycl::free(device_out, q_ct1);
+// CHECK:}
 void test_1() {
   int n = 10;
   int *device_in = nullptr;
@@ -46,24 +46,24 @@ void test_1() {
   cudaFree(device_tmp);
 }
 
-// void test_2() {
-// dpct::device_ext &dev_ct1 = dpct::get_current_device();
-// sycl::queue &q_ct1 = dev_ct1.default_queue();
-// int n = 10;
-// int *device_in = nullptr;
-// int *device_out = nullptr;
-// int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-// int host_out[10];
-// device_in = sycl::malloc_device<int>(n, q_ct1);
-// device_out = sycl::malloc_device<int>(n, q_ct1);
-// q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
-// DPCT1027:{{.*}}
-// 0, 0;
-// oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + n, device_out, 0);
-// q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
-// sycl::free(device_in, q_ct1);
-// sycl::free(device_out, q_ct1);
-// }
+// CHECK:void test_2() {
+// CHECK:dpct::device_ext &dev_ct1 = dpct::get_current_device();
+// CHECK:sycl::queue &q_ct1 = dev_ct1.default_queue();
+// CHECK:int n = 10;
+// CHECK:int *device_in = nullptr;
+// CHECK:int *device_out = nullptr;
+// CHECK:int host_in[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// CHECK:int host_out[10];
+// CHECK:device_in = sycl::malloc_device<int>(n, q_ct1);
+// CHECK:device_out = sycl::malloc_device<int>(n, q_ct1);
+// CHECK:q_ct1.memcpy(device_in, (void *)host_in, sizeof(host_in)).wait();
+// CHECK:DPCT1027:{{.*}}
+// CHECK:0, 0;
+// CHECK:oneapi::dpl::exclusive_scan(oneapi::dpl::execution::device_policy(q_ct1), device_in, device_in + n, device_out, 0);
+// CHECK:q_ct1.memcpy((void *)host_out, (void *)device_out, sizeof(host_out)).wait();
+// CHECK:sycl::free(device_in, q_ct1);
+// CHECK:sycl::free(device_out, q_ct1);
+// CHECK:}
 void test_2() {
   int n = 10;
   int *device_in = nullptr;
