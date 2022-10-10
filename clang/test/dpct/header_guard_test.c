ea9eabb74c173367f628629be7d048e3202547ac
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-10-10 13:28:38 +0800

    [SYCLomatic] Enable LIT header_guard_test.c for cuda 11
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/header_guard_test.c
+++ clang/test/dpct/header_guard_test.c
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.2, cuda-11.7
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.2, v11.7
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.2
 // ------ make test directory
 // RUN: cd %T
 // RUN: rm -rf header_guard_test
@@ -13,22 +13,23 @@
 //
 // ------ create custom header files
 // RUN:  dpct thrust-iterators.cu cublas_curandInMacro.cu nccl.cu --use-custom-helper=api --cuda-include-path="%cuda-path/include"
-// RUN:  grep ifndef dpct_output/include/dpct/*.hpp > header_guards.txt
+// RUN:  grep --no-filename ifndef dpct_output/include/dpct/*.hpp > header_guards.txt
 //
 // ------ ensure header guard names in custom header files match the names in the standard header files
 // RUN: FileCheck --input-file header_guards.txt %s
 //
 // ------ cleanup test directory
 // RUN: cd ..
-// RUN: rm -rf ./gen-build-script
+// RUN: rm -rf ./header_guard_test
 
-// CHECK: dpct_output/include/dpct/blas_utils.hpp:#ifndef __DPCT_BLAS_UTILS_HPP__
-// CHECK: dpct_output/include/dpct/ccl_utils.hpp:#ifndef __DPCT_CCL_UTILS_HPP__
-// CHECK: dpct_output/include/dpct/device.hpp:#ifndef __DPCT_DEVICE_HPP__
-// CHECK: dpct_output/include/dpct/dpct.hpp:#ifndef __DPCT_HPP__
-// CHECK: dpct_output/include/dpct/dpl_utils.hpp:#ifndef __DPCT_DPL_UTILS_HPP__
-// CHECK: dpct_output/include/dpct/memory.hpp:#ifndef __DPCT_MEMORY_HPP__
-// CHECK: dpct_output/include/dpct/util.hpp:#ifndef __DPCT_UTIL_HPP__
+// CHECK: #ifndef __DPCT_BLAS_UTILS_HPP__
+// CHECK: #ifndef __DPCT_CCL_UTILS_HPP__
+// CHECK: #ifndef __DPCT_DEVICE_HPP__
+// CHECK: #ifndef __DPCT_HPP__
+// CHECK: #ifndef __DPCT_DPL_UTILS_HPP__
+// CHECK: #ifndef __DPCT_LIB_COMMON_UTILS_HPP__
+// CHECK: #ifndef __DPCT_MEMORY_HPP__
+// CHECK: #ifndef __DPCT_UTIL_HPP__
 
 void foo() {
 }
