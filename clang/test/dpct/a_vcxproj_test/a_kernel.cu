e75626a46c0effdfc7d694f6aab239db8f9a53b8
Sheng Chen <sheng.s.chen@intel.com>
2022-10-13 14:19:48 +0800

    Update windows

--- clang/test/dpct/a_vcxproj_test/a_kernel.cu
+++ clang/test/dpct/a_vcxproj_test/a_kernel.cu
@@ -1,14 +1,16 @@
 // UNSUPPORTED: -linux-
 // RUN: cat %S/DemoCudaProj.vcxproj > %T/DemoCudaProj.vcxproj
+// RUN: cat %S/a_kernel.cu > %T/a_kernel.cu
+// RUN: cat %S/readme_1.txt > %T/readme_1.txt
 // RUN: cd %T
-// RUN: dpct --format-range=none  --vcxprojfile=%T/DemoCudaProj.vcxproj  -in-root=%S -out-root=%T  %s --cuda-include-path="%cuda-path/include"
+// RUN: dpct --format-range=none  --vcxprojfile=%T/DemoCudaProj.vcxproj  -in-root=%T -out-root=%T/out  %T/a_kernel.cu --cuda-include-path="%cuda-path/include"
 
 // RUN: cat %S/check_compilation_ref.txt  >%T/check_compilation_db.txt
 // RUN: cat %T/compile_commands.json >> %T/check_compilation_db.txt
 
 // RUN: FileCheck --match-full-lines --input-file %T/check_compilation_db.txt %T/check_compilation_db.txt
 
-// RUN: dpct --format-range=none  -p=%S  -in-root=%S -out-root=%T/2 --process-all --cuda-include-path="%cuda-path/include"
+// RUN: dpct --format-range=none  -p=%T  -in-root=%T -out-root=%T/2 --process-all --cuda-include-path="%cuda-path/include"
 
 // RUN: FileCheck --input-file %T/2/a_kernel.dp.cpp --match-full-lines %S/a_kernel.cu
 // RUN: FileCheck --match-full-lines --input-file %T/2/readme_1.txt %T/2/readme_1.txt
