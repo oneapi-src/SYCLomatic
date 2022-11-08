1e6507524698b3dc44652f3fda97ba81cc5664df
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-11-08 13:30:33 +0800

    Fix a lit fail
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/Memory/api_test45.cu
+++ clang/test/dpct/test_api_level/Memory/api_test45.cu
@@ -1,7 +1,7 @@
-// RUN: dpct --format-range=none   --use-custom-helper=api -out-root %T/Memory/api_test44_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
-// RUN: grep "IsCalled" %T/Memory/api_test44_out/MainSourceFiles.yaml | wc -l > %T/Memory/api_test44_out/count.txt
-// RUN: FileCheck --input-file %T/Memory/api_test44_out/count.txt --match-full-lines %s
-// RUN: rm -rf %T/Memory/api_test44_out
+// RUN: dpct --format-range=none   --use-custom-helper=api -out-root %T/Memory/api_test45_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
+// RUN: grep "IsCalled" %T/Memory/api_test45_out/MainSourceFiles.yaml | wc -l > %T/Memory/api_test45_out/count.txt
+// RUN: FileCheck --input-file %T/Memory/api_test45_out/count.txt --match-full-lines %s
+// RUN: rm -rf %T/Memory/api_test45_out
 
 // CHECK: 19
 // TEST_FEATURE: Memory_usm_host_allocator_alias
