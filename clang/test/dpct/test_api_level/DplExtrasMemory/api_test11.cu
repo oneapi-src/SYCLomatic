daba639dd936508b48349faf2e3d8baac3aa52df
Cai, Justin <justin.cai@intel.com>
2022-11-09 09:14:52 -0800

    [SYCLomatic] Fix thrust::device_reference lit test
    Signed-off-by: Cai, Justin <justin.cai@intel.com>

--- clang/test/dpct/test_api_level/DplExtrasMemory/api_test11.cu
+++ clang/test/dpct/test_api_level/DplExtrasMemory/api_test11.cu
@@ -1,11 +1,11 @@
 // UNSUPPORTED: cuda-8.0
 // UNSUPPORTED: v8.0
-// RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/DplExtrasMemory/api_test11_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17
+// RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/DplExtrasMemory/api_test11_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17 -fsized-deallocation
 // RUN: grep "IsCalled" %T/DplExtrasMemory/api_test11_out/MainSourceFiles.yaml | wc -l > %T/DplExtrasMemory/api_test11_out/count.txt
 // RUN: FileCheck --input-file %T/DplExtrasMemory/api_test11_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/DplExtrasMemory/api_test11_out
 
-// CHECK: 37
+// CHECK: 38
 // TEST_FEATURE: DplExtrasMemory_device_reference
 
 
