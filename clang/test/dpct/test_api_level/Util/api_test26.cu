ef7901764e196788187a551bbe725a7afca10ca6
Cai, Justin <justin.cai@intel.com>
2022-11-14 14:29:45 -0800

    Add cuda include path

--- clang/test/dpct/test_api_level/Util/api_test26.cu
+++ clang/test/dpct/test_api_level/Util/api_test26.cu
@@ -1,5 +1,7 @@
 // RUN: rm -rf %T/Util/api_test26_out
-// RUN: dpct --use-custom-helper=api --out-root=%T/Util/api_test26_out %s -- -x cuda -ptx
+// RUN: dpct --use-custom-helper=api --out-root=%T/Util/api_test26_out \
+// RUN:      --cuda-include-path="%cuda-path/include" \
+// RUN:      %s -- -x cuda -ptx
 // RUN: [ 1 -eq $(grep "get_nth_parameter:" %T/Util/api_test26_out/MainSourceFiles.yaml | wc -l) ]
 // RUN: [ 1 -eq $(grep "get_args_ptr:" %T/Util/api_test26_out/MainSourceFiles.yaml | wc -l) ]
 
