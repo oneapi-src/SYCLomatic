a0758d60e06ba003b0838fe74bfe9b935b201f7b
zhiweij1 <zhiwei.jiang@intel.com>
2022-11-16 10:20:14 +0800

    [SYCLomatic] Migrate cublasHandle_t to dpct::queue_ptr (#354)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/BlasUtils/api_test4.cu
+++ clang/test/dpct/test_api_level/BlasUtils/api_test4.cu
@@ -3,7 +3,7 @@
 // RUN: FileCheck --input-file %T/BlasUtils/api_test4_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/BlasUtils/api_test4_out
 
-// CHECK: 39
+// CHECK: 40
 
 #include "cublas_v2.h"
 
