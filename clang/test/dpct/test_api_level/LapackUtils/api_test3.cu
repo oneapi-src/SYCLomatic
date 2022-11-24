94102d64d34ee0afc02b231f4b6f8f3a0b42f01f
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-11-24 16:51:14 +0800

    Fix lit
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/LapackUtils/api_test3.cu
+++ clang/test/dpct/test_api_level/LapackUtils/api_test3.cu
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0
+// UNSUPPORTED: v8.0
 // RUN: dpct --format-range=none --use-custom-helper=api -out-root %T/LapackUtils/api_test3_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: grep "IsCalled" %T/LapackUtils/api_test3_out/MainSourceFiles.yaml | wc -l > %T/LapackUtils/api_test3_out/count.txt
 // RUN: FileCheck --input-file %T/LapackUtils/api_test3_out/count.txt --match-full-lines %s
