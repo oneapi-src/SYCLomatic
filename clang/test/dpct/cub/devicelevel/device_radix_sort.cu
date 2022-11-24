b71498136136149713ae23efb031422b35b04a7a
Wang, Yihan <yihan.wang@intel.com>
2022-11-24 20:05:39 +0800

    [SYCLomatic] fix tests
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/devicelevel/device_radix_sort.cu
+++ clang/test/dpct/cub/devicelevel/device_radix_sort.cu
@@ -1,5 +1,5 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4
 // RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_radix_sort %S/device_radix_sort.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/devicelevel/device_radix_sort/device_radix_sort.dp.cpp %s
 
--- clang/test/dpct/test_api_level/DplExtrasAlgorithm/api_test13.cu
+++ clang/test/dpct/test_api_level/DplExtrasAlgorithm/api_test13.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0
-// UNSUPPORTED: v8.0
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4s
 // RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/DplExtrasAlgorithm/api_test13 %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17
 // RUN: grep "IsCalled" %T/DplExtrasAlgorithm/api_test13/MainSourceFiles.yaml | wc -l > %T/DplExtrasAlgorithm/api_test13/count.txt
 // RUN: FileCheck --input-file %T/DplExtrasAlgorithm/api_test13/count.txt --match-full-lines %s
