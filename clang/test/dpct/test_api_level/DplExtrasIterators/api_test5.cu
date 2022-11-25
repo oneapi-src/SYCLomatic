7f4e7d10d3c7a62a22cf273249ec4d7123a4dc47
Wang, Yihan <yihan.wang@intel.com>
2022-11-25 08:47:36 +0800

    [SYCLomatic] UPdate tests
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/test_api_level/DplExtrasIterators/api_test5.cu
+++ clang/test/dpct/test_api_level/DplExtrasIterators/api_test5.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0
-// UNSUPPORTED: v8.0
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/DplExtrasIterators/api_test5_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17
 // RUN: grep "IsCalled" %T/DplExtrasIterators/api_test5_out/MainSourceFiles.yaml | wc -l > %T/DplExtrasIterators/api_test5_out/count.txt
 // RUN: FileCheck --input-file %T/DplExtrasIterators/api_test5_out/count.txt --match-full-lines %s
