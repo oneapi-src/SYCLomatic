15741a53ee94cede25bb8b36857f57fec92d71dc
Wang, Yihan <yihan.wang@intel.com>
2022-11-30 09:42:03 +0800

    Fix test
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/user_define_rule_header_order.cu
+++ clang/test/dpct/user_define_rule_header_order.cu
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4
 // RUN: cat %s > %T/user_define_rule_header_order.cu
 // RUN: cat %S/user_define_rule_header_order.yaml > %T/user_define_rule_header_order.yaml
 // RUN: cd %T
