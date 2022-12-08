a8610248d486449d59a48161d356509eff9fd844
Wang, Yihan <yihan.wang@intel.com>
2022-12-08 15:05:13 +0800

    [SYCLomatic] Split CallExprRewriter.cpp
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

---
+++ clang/lib/DPCT/CallExprRewriterComplex.cpp
@@ -0,0 +1,24 @@
+//===--------------- CallExprRewriterComplex.cpp --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "CallExprRewriter.h"
+#include "CallExprRewriterCommon.h"
+
+namespace clang {
+namespace dpct {
+
+void CallExprRewriterFactoryBase::initRewriterMapComplex() {
+  RewriterMap->merge(
+      std::unordered_map<std::string,
+                         std::shared_ptr<CallExprRewriterFactoryBase>>({
+#include "APINamesComplex.inc"
+      }));
+}
+
+} // namespace dpct
+} // namespace clang
---
+++ clang/lib/DPCT/CallExprRewriterDriver.cpp
@@ -0,0 +1,24 @@
+//===--------------- CallExprRewriterDriver.cpp ---------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "CallExprRewriter.h"
+#include "CallExprRewriterCommon.h"
+
+namespace clang {
+namespace dpct {
+
+void CallExprRewriterFactoryBase::initRewriterMapDriver() {
+  RewriterMap->merge(
+      std::unordered_map<std::string,
+                         std::shared_ptr<CallExprRewriterFactoryBase>>({
+#include "APINamesDriver.inc"
+      }));
+}
+
+} // namespace dpct
+} // namespace clang
