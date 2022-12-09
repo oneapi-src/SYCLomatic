6fa96a9177af623c96094bfac7793f1d8f1de79d
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-12-09 10:49:34 +0800

    WIP
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/Utility.cpp
+++ clang/lib/DPCT/Utility.cpp
@@ -3478,6 +3478,71 @@ bool analyzeMemcpyOrder(
   return true;
 }
 
+static std::optional<bool> isPassedByReference(const clang::DeclRefExpr *DRE) {
+}
+
+/// Except cudaMalloc
+static std::optional<bool> isPassedByAddress(const clang::DeclRefExpr *DRE) {
+
+}
+
+/// DRE1 is the 1st memcpy's 1st argument (pointer), DRE2 is the 2nd's memcpy's
+/// 2nd argument (pointer).
+/// (1) Variable should be local declared and in the same function where
+///     DRE is used (avoid alias)
+/// (2) Variable itself, it's address and it's reference are not be passed to
+///     other variables or functions (avoid alias)
+/// (3) Not the same Decl
+static bool hasPossibleRelationship(const clang::DeclRefExpr *DRE1,
+                                    const clang::DeclRefExpr *DRE2,
+                                    const clang::CompoundStmt *CS) {
+  if (!DRE1 || !DRE2)
+    return true;
+
+  const clang::VarDecl *Decl1 =
+      dyn_cast_or_null<clang::VarDecl>(DRE1->getDecl());
+  const clang::VarDecl *Decl2 =
+      dyn_cast_or_null<clang::VarDecl>(DRE2->getDecl());
+  if (!Decl1 || !Decl2)
+    return true;
+
+  if (!Decl1->isLocalVarDecl() || Decl1->isStaticLocal())
+    return true;
+  if (!Decl2->isLocalVarDecl() || Decl2->isStaticLocal())
+    return true;
+
+  if (!dpct::DpctGlobalInfo::isAncestor(CS, Decl1))
+    return true;
+  if (!dpct::DpctGlobalInfo::isAncestor(CS, Decl2))
+    return true;
+
+  auto VarReferenceMatcher = clang::ast_matchers::findAll(
+      clang::ast_matchers::declRefExpr().bind("VarReference"));
+  auto MatchedResults = clang::ast_matchers::match(
+      VarReferenceMatcher, *CS, clang::dpct::DpctGlobalInfo::getContext());
+  std::vector<const DeclRefExpr *> Refs;
+  for (auto &Result : MatchedResults) {
+    const DeclRefExpr *MatchedDRE =
+        Result.getNodeAs<DeclRefExpr>("VarReference");
+    if (!MatchedDRE)
+      continue;
+    if (MatchedDRE->getDecl() == Decl1 ||
+        MatchedDRE->getDecl() == Decl2) {
+      auto OptRef = isPassedByReference(MatchedDRE);
+      auto OptAddr = isPassedByAddress(MatchedDRE);
+      if (!OptRef.has_value() || !OptAddr.has_value())
+        return true;
+      if (!OptRef.has_value() || !OptAddr.has_value())
+        return true;
+    }
+  }
+
+  if (DRE1 == DRE2)
+    return true;
+
+  return false;
+}
+
 /// This function is used to check if the ".wait()" can be omitted in the
 /// migrated code of cudaMemcpy/cudaMemcpyFromSymbol/cudaMemcpyToSymbol API.
 /// Rule:
@@ -3485,11 +3550,15 @@ bool analyzeMemcpyOrder(
 /// of WhileStmt/ForStmt/DoStmt/IfStmt/FuncDecl). The analysis result of the
 /// outer scope will not affect the inner scope.
 ///
-/// Traverse all CallExpr nodes between current cudaMemcpy CallExpr and
-/// the next cudaMemcpy CallExpr with pre-order.
-/// If (1) all these CallExpr nodes are in the SpecialFuncNameSet(e.g.,
-/// printf()), and (2) current and next cudaMemcpy are not in flow control
-/// Stmt, then the ".wait()" for current cudaMemcpy can be omitted.
+/// Traverse all CallExpr nodes between current cudaMemcpy CallExpr and the
+/// next cudaMemcpy CallExpr with pre-order.
+/// If all 3 conditions are met, the ".wait()" for current cudaMemcpy can be
+/// omitted:
+/// (1) all these CallExpr nodes are in the SpecialFuncNameSet (E.g., printf())
+/// (2) current and next cudaMemcpy are not in flow control Stmt
+/// (3) The pointer type DeclRefExpr nodes in the 2nd memcpy API's 2nd argument
+///     are not related to any pointer type DeclRefExpr nodes in the 1st memcpy
+///     API's 1st argument
 ///
 /// E.g.,
 /// \code
