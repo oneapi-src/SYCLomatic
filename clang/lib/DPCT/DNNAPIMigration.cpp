ab783eaaafd1e7ab10064a8923e5d0ce8d7cfe8f
intwanghao <hao3.wang@intel.com>
2023-01-15 19:36:59 -0800

    [SYCLomatic] Enable RNN layer APIs and types migration (#436)
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- clang/lib/DPCT/DNNAPIMigration.cpp
+++ clang/lib/DPCT/DNNAPIMigration.cpp
@@ -40,7 +40,10 @@ void CuDNNTypeRule::registerMatcher(MatchFinder &MF) {
               "cudnnBatchNormOps_t", "cudnnBatchNormMode_t", "cudnnNormMode_t",
               "cudnnNormOps_t", "cudnnConvolutionDescriptor_t",
               "cudnnConvolutionFwdAlgo_t", "cudnnConvolutionBwdDataAlgo_t",
-              "cudnnConvolutionBwdFilterAlgo_t", "cudnnFilterDescriptor_t"))))))
+              "cudnnConvolutionBwdFilterAlgo_t", "cudnnFilterDescriptor_t",
+              "cudnnRNNMode_t", "cudnnRNNBiasMode_t", "cudnnDirectionMode_t",
+              "cudnnRNNDescriptor_t", "cudnnForwardMode_t", "cudnnRNNDataDescriptor_t",
+              "cudnnRNNDataLayout_t"))))))
           .bind("CuDNNType"),
       this);
   MF.addMatcher(declRefExpr(to(enumConstantDecl(matchesName("CUDNN_.*"))))
@@ -107,6 +110,12 @@ void CuDNNTypeRule::runRule(const MatchFinder::MatchResult &Result) {
   }
 }
 
+auto AssignedStmt = []() {
+  return anyOf(hasParent(compoundStmt()), hasParent(forStmt()),
+               hasParent(whileStmt()), hasParent(doStmt()),
+               hasParent(ifStmt()));
+};
+
 void CuDNNAPIRule::registerMatcher(ast_matchers::MatchFinder &MF) {
   auto CuDNNAPI = [&]() {
     return hasAnyName(
@@ -163,15 +172,130 @@ void CuDNNAPIRule::registerMatcher(ast_matchers::MatchFinder &MF) {
         "cudnnConvolutionBackwardBias", "cudnnConvolutionBackwardFilter",
         "cudnnGetConvolutionForwardWorkspaceSize", "cudnnGetConvolutionBackwardDataWorkspaceSize",
         "cudnnGetConvolutionBackwardFilterWorkspaceSize",
-        "cudnnGetNormalizationBackwardWorkspaceSize");
+        "cudnnGetNormalizationBackwardWorkspaceSize",
+        "cudnnCreateRNNDescriptor", "cudnnCreateRNNDataDescriptor", "cudnnDestroyRNNDescriptor",
+        "cudnnDestroyRNNDataDescriptor", "cudnnSetRNNDataDescriptor", "cudnnGetRNNDataDescriptor",
+        "cudnnSetRNNDescriptor_v8", "cudnnGetRNNDescriptor_v8", "cudnnGetRNNWeightSpaceSize",
+        "cudnnGetRNNTempSpaceSizes", "cudnnRNNForward", "cudnnRNNBackwardData_v8",
+        "cudnnRNNBackwardWeights_v8");
   };
 
-  MF.addMatcher(callExpr(callee(functionDecl(CuDNNAPI()))).bind("call"), this);
+  MF.addMatcher(
+      callExpr(allOf(callee(functionDecl(CuDNNAPI())), AssignedStmt()))
+          .bind("Call"),
+      this);
+  MF.addMatcher(
+      callExpr(allOf(callee(functionDecl(CuDNNAPI())), unless(AssignedStmt())))
+          .bind("AssignedCall"),
+      this);
 }
 
 void CuDNNAPIRule::runRule(
     const ast_matchers::MatchFinder::MatchResult &Result) {
-  if (const CallExpr *CE = getNodeAsType<CallExpr>(Result, "call")) {
+  bool IsAssigned = false;
+  const CallExpr *CE = getNodeAsType<CallExpr>(Result, "Call");
+  if (!CE) {
+    if (!(CE = getNodeAsType<CallExpr>(Result, "AssignedCall")))
+      return;
+    IsAssigned = true;
+  }
+  llvm::StringRef FuncName;
+  if (auto DC = CE->getDirectCallee()) {
+    FuncName = DC->getName();
+  }
+  if (FuncName == "cudnnRNNBackwardData_v8" ||
+      FuncName == "cudnnRNNBackwardWeights_v8") {
+    RnnBackwardFuncInfo FuncInfo;
+    auto &Global = DpctGlobalInfo::getInstance();
+    // 1.RnnDescIndex, 2.HXDataIndex, 3.YDataIndex
+    unsigned RnnInputArgIndex[3] = {1, 9, 4};
+    auto CERange = getDefinitionRange(CE->getBeginLoc(), CE->getEndLoc());
+    auto CELocInfo = Global.getLocInfo(CERange.getBegin());
+    auto FileInfo = Global.insertFile(CELocInfo.first);
+    auto &BackwardFuncInfo = FileInfo->getRnnBackwardFuncInfo();
+    auto &RnnInputMap = DpctGlobalInfo::getRnnInputMap();
+    unsigned BeginOffset = CELocInfo.second;
+    unsigned EndOffset = Global.getLocInfo(CERange.getEnd()).second;
+    FuncInfo.isAssigned = IsAssigned;
+    FuncInfo.Length = EndOffset - BeginOffset + 1;
+    FuncInfo.FilePath = CELocInfo.first;
+    FuncInfo.Offset = CELocInfo.second;
+    FuncInfo.isDataGradient = true;
+    unsigned int ArgsNum = CE->getNumArgs();
+    auto Condition = [&](const clang::DynTypedNode &Node) -> bool {
+      return Node.get<IfStmt>() || Node.get<WhileStmt>() ||
+             Node.get<ForStmt>() || Node.get<DoStmt>() ||
+             Node.get<CaseStmt>() || Node.get<SwitchStmt>() ||
+             Node.get<CompoundStmt>();
+    };
+
+    if (auto CS = DpctGlobalInfo::findAncestor<CompoundStmt>(CE, Condition)) {
+      auto LocInfo = Global.getLocInfo(CS->getBeginLoc());
+      FuncInfo.CompoundLoc = LocInfo.first + std::to_string(LocInfo.second);
+    } else {
+      report(CE->getBeginLoc(), Diagnostics::API_NOT_MIGRATED, false, FuncName);
+      return;
+    }
+    if (FuncName == "cudnnRNNBackwardWeights_v8") {
+      RnnInputArgIndex[1] = 7;
+      RnnInputArgIndex[2] = 9;
+      FuncInfo.isDataGradient = false;
+      if (!IsAssigned) {
+        auto Tok =
+            Lexer::findNextToken(CERange.getEnd(), Global.getSourceManager(),
+                                 LangOptions())
+                .value();
+        if (Tok.is(tok::TokenKind::semi)) {
+          FuncInfo.Length +=
+              Global.getLocInfo(Tok.getLocation()).second - EndOffset;
+        }
+      }
+    }
+    for(int i = 0; i < 3; i++) {
+      if (auto RnnInputDRE = dyn_cast<DeclRefExpr>(
+              CE->getArg(RnnInputArgIndex[i])->IgnoreImplicitAsWritten())) {
+        if (auto RnnInputDecl = RnnInputDRE->getDecl()) {
+          auto ArgName = RnnInputDecl->getName();
+          auto DeclLocInfo = Global.getLocInfo(RnnInputDecl->getBeginLoc());
+          std::string MapKey =
+              DeclLocInfo.first + std::to_string(DeclLocInfo.second) + ArgName.str();
+          auto &SubMap = RnnInputMap[MapKey];
+          if (SubMap.empty()) {
+            std::vector<const clang::DeclRefExpr *> MatchResults;
+            findAllVarRef(RnnInputDRE, MatchResults, true);
+            for (auto Result : MatchResults) {
+              auto DRELocInfo = Global.getLocInfo(Result->getBeginLoc());
+              SubMap[DRELocInfo.first].push_back(DRELocInfo.second);
+            }
+          }
+          FuncInfo.RnnInputDeclLoc.push_back(std::move(MapKey));
+        }
+      }
+    }
+    if (FuncName == "cudnnRNNBackwardData_v8") {
+      FuncInfo.FuncArgs.reserve(ArgsNum);
+      for (unsigned int ArgIndex = 0; ArgIndex < ArgsNum; ArgIndex++) {
+        auto Arg = CE->getArg(ArgIndex)->IgnoreImplicitAsWritten();
+        ExprAnalysis ArgEA(Arg);
+        ArgEA.analyze();
+        std::string ArgString = ArgEA.getReplacedString();
+        FuncInfo.FuncArgs.push_back(ArgString);
+      }
+    } else {
+      FuncInfo.FuncArgs.reserve(2);
+      ExprAnalysis ArgEA;
+      ArgEA.analyze(CE->getArg(5));
+      FuncInfo.FuncArgs.push_back(ArgEA.getReplacedString());
+      ArgEA.analyze(CE->getArg(11));
+      FuncInfo.FuncArgs.push_back(ArgEA.getReplacedString());
+    }
+    if (FuncInfo.FuncArgs.empty() || FuncInfo.RnnInputDeclLoc.empty() ||
+        (FuncInfo.RnnInputDeclLoc.size() != 3)) {
+      report(CE->getBeginLoc(), Diagnostics::API_NOT_MIGRATED, false, FuncName);
+      return;
+    }
+    BackwardFuncInfo.push_back(std::move(FuncInfo));
+  } else {
     ExprAnalysis EA(CE);
     emplaceTransformation(EA.getReplacement());
     EA.applyAllSubExprRepl();
---
+++ clang/test/dpct/dnn/rnn.cu
@@ -0,0 +1,355 @@
+// RUN: dpct -in-root %S -out-root %T/rnn %S/rnn.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/rnn/rnn.dp.cpp --match-full-lines %s
+
+// CHECK: #include <dpct/dnnl_utils.hpp>
+// CHECK: #include <sycl/sycl.hpp>
+// CHECK: #include <dpct/dpct.hpp>
+// CHECK: #include <iostream>
+// CHECK: #include <vector>
+#include <cuda_runtime.h>
+#include <cudnn.h>
+#include <iostream>
+#include <vector>
+
+int main() {
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+
+    int hidenSize = 2;
+    int layerSize = 3;
+    int inputSize = 2;
+    int projectSize = 2;
+    int batchSize = 3;
+    int maxSeqLength = 4;
+    int dir = 2;
+
+    int hDim[3] = {dir * layerSize, batchSize, projectSize};
+    int hStride[3] = {hDim[1] * hDim[2], hDim[2], 1};
+
+    int cDim[3] = {layerSize * dir, batchSize, hidenSize};
+    int cStride[3] = {cDim[2] * cDim[1], cDim[2], 1};
+    int xDim[3] = {maxSeqLength, batchSize, inputSize};
+    int yDim[3] = {maxSeqLength, batchSize, dir * projectSize};
+
+    int h_size = hDim[0] * hDim[1] * hDim[2];
+    int c_size = cDim[0] * cDim[1] * cDim[2];
+    int x_size = xDim[0] * xDim[1] * xDim[2];
+    int y_size = yDim[0] * yDim[1] * yDim[2];
+
+    cudnnHandle_t handle;
+
+    cudnnCreate(&handle);
+// CHECK:    dpct::dnnl::rnn_desc rnnDesc;
+// CHECK:    dpct::dnnl::memory_desc_ext xDesc;
+// CHECK:    dpct::dnnl::memory_desc_ext yDesc;
+// CHECK:    dpct::dnnl::memory_desc_ext hDesc;
+// CHECK:    dpct::dnnl::memory_desc_ext cDesc;
+    cudnnRNNDescriptor_t rnnDesc;
+    cudnnRNNDataDescriptor_t xDesc;
+    cudnnRNNDataDescriptor_t yDesc;
+    cudnnTensorDescriptor_t hDesc;
+    cudnnTensorDescriptor_t cDesc;
+    cudnnCreateRNNDescriptor(&rnnDesc);
+    cudnnCreateRNNDataDescriptor(&xDesc);
+    cudnnCreateRNNDataDescriptor(&yDesc);
+    cudnnCreateTensorDescriptor(&hDesc);
+    cudnnCreateTensorDescriptor(&cDesc);
+
+    size_t spacesize, statesize;
+    void* reservespace, *state;
+
+    cudnnSetTensorNdDescriptor(hDesc, CUDNN_DATA_FLOAT, 3, hDim, hStride);
+    cudnnSetTensorNdDescriptor(cDesc, CUDNN_DATA_FLOAT, 3, cDim, cStride);
+// CHECK: auto p = (rnnDesc.set(dpct::dnnl::rnn_mode::vanilla_relu, dpct::dnnl::rnn_bias_mode::single, dir == 1 ? dpct::dnnl::rnn_direction::unidirectional : dpct::dnnl::rnn_direction::bidirectional, dpct::library_data_t::real_float, inputSize, hidenSize, projectSize, layerSize), 0);
+    auto p = cudnnSetRNNDescriptor_v8(rnnDesc,
+        CUDNN_RNN_ALGO_STANDARD,
+        CUDNN_RNN_RELU,
+        CUDNN_RNN_SINGLE_INP_BIAS,
+        dir == 1 ? CUDNN_UNIDIRECTIONAL : CUDNN_BIDIRECTIONAL,
+        CUDNN_LINEAR_INPUT,
+        CUDNN_DATA_FLOAT,
+        CUDNN_DATA_FLOAT,
+        CUDNN_DEFAULT_MATH,
+        inputSize,  // inputSize
+        hidenSize,  // hiddenSize
+        projectSize,  // projSize
+        layerSize,  // numLayers
+        NULL,
+        CUDNN_RNN_PADDED_IO_ENABLED
+    );
+
+    int seqLenArray[3];
+    seqLenArray[0] = maxSeqLength;
+    seqLenArray[1] = maxSeqLength;
+    seqLenArray[2] = maxSeqLength;
+// CHECK: p = (xDesc.set(dpct::dnnl::rnn_memory_format_tag::tnc, dpct::library_data_t::real_float, xDim[0], xDim[1], xDim[2]), 0);
+    p = cudnnSetRNNDataDescriptor(xDesc, 
+        CUDNN_DATA_FLOAT, 
+        CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED,
+        xDim[0], // maxSeqLength
+        xDim[1],  // batchSize
+        xDim[2],  // vectorSize
+        seqLenArray, // seqLengthArray
+        NULL
+    );
+// CHECK: p = (yDesc.set(dpct::dnnl::rnn_memory_format_tag::tnc, dpct::library_data_t::real_float, yDim[0], yDim[1], yDim[2]), 0);
+    p = cudnnSetRNNDataDescriptor(yDesc,
+        CUDNN_DATA_FLOAT,
+        CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED,
+        yDim[0], // maxSeqLength
+        yDim[1],  // batchSize
+        yDim[2],  // vectorSize
+        seqLenArray, // seqLengthArray
+        NULL
+    );
+
+    size_t weightsSpaceSize, workSpaceSize, reserveSpaceSize;
+    // CHECK: handle.rnn_get_weight_space_size(rnnDesc, &weightsSpaceSize);
+    cudnnGetRNNWeightSpaceSize(handle, rnnDesc, &weightsSpaceSize);
+    // CHECK: handle.rnn_get_scratchpad_workspace_size(rnnDesc, dnnl::prop_kind::forward_training, xDesc, &workSpaceSize, &reserveSpaceSize);
+    cudnnGetRNNTempSpaceSizes(handle, 
+        rnnDesc, 
+        CUDNN_FWD_MODE_TRAINING,
+        xDesc, 
+        &workSpaceSize,
+        &reserveSpaceSize
+    );
+
+    float *xData, *yData, *hxData, *hyData, *cxData, *cyData, *weightsData, *workSpaceData, *reserveSpaceData;
+    float *dxData, *dyData, *dhxData, *dhyData, *dcxData, *dcyData, *dweightsData;
+    cudnnWgradMode_t WGMode = CUDNN_WGRAD_MODE_ADD;
+    int *seqlenarray;
+    // CHECK: auto e = (handle.async_rnn_forward(rnnDesc, dnnl::prop_kind::forward_training, xDesc, xData, yDesc, yData, hDesc, hxData, hyData, cDesc, cxData, cyData, weightsSpaceSize, weightsData, workSpaceSize, workSpaceData, reserveSpaceSize, reserveSpaceData), 0);
+    auto e = cudnnRNNForward(
+        handle, 
+        rnnDesc, 
+        CUDNN_FWD_MODE_TRAINING, 
+        seqlenarray, 
+        xDesc, 
+        xData, 
+        yDesc, 
+        yData, 
+        hDesc, 
+        hxData, 
+        hyData, 
+        cDesc, 
+        cxData,
+        cyData,
+        weightsSpaceSize, 
+        weightsData, 
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+// CHECK: /*
+// CHECK: DPCT1003:{{[0-9]+}}: Migrated API does not return error code. (*, 0) is inserted. You may need to rewrite this code.
+// CHECK: */
+// CHECK: e = (handle.async_rnn_backward(rnnDesc, yDesc, yData, dyData, xDesc, xData, dxData, hDesc, hxData, dhyData, dhxData, cDesc, cxData, dcyData, dcxData, weightsSpaceSize, weightsData, dweightsData, workSpaceSize, workSpaceData, reserveSpaceSize, reserveSpaceData), 0);
+    e = cudnnRNNBackwardData_v8(
+        handle,
+        rnnDesc,
+        seqlenarray,
+        yDesc,
+        yData,
+        dyData,
+        xDesc,
+        dxData,
+        hDesc,
+        hxData,
+        dhyData,
+        dhxData,
+        cDesc,
+        cxData,
+        dcyData,
+        dcxData,
+        weightsSpaceSize,
+        weightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+// CHECK: /*
+// CHECK: DPCT1027:{{[0-9]+}}: The call to cudnnRNNBackwardWeights_v8 was replaced with 0 because this call and cudnnRNNBackwardData_v8 are migrated to a single function call async_rnn_backward
+// CHECK: */
+// CHECK: e = 0;
+    e = cudnnRNNBackwardWeights_v8(
+        handle,
+        rnnDesc,
+        WGMode,
+        seqlenarray,
+        xDesc,
+        xData,
+        hDesc,
+        hxData,
+        yDesc,
+        yData,
+        weightsSpaceSize,
+        dweightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+   float *y1Data;
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardData_v8 is not supported.
+// CHECK:     */
+    cudnnRNNBackwardData_v8(
+        handle,
+        rnnDesc,
+        seqlenarray,
+        yDesc,
+        y1Data,
+        dyData,
+        xDesc,
+        dxData,
+        hDesc,
+        hxData,
+        dhyData,
+        dhxData,
+        cDesc,
+        cxData,
+        dcyData,
+        dcxData,
+        weightsSpaceSize,
+        weightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+    
+    cudaMalloc(&y1Data, 100);
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardWeights_v8 is not supported.
+// CHECK:     */
+    cudnnRNNBackwardWeights_v8(
+        handle,
+        rnnDesc,
+        WGMode,
+        seqlenarray,
+        xDesc,
+        xData,
+        hDesc,
+        hxData,
+        yDesc,
+        y1Data,
+        weightsSpaceSize,
+        dweightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+    float *y2Data;
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardData_v8 is not supported.
+// CHECK:     */
+    cudnnRNNBackwardData_v8(
+        handle,
+        rnnDesc,
+        seqlenarray,
+        yDesc,
+        y2Data,
+        dyData,
+        xDesc,
+        dxData,
+        hDesc,
+        hxData,
+        dhyData,
+        dhxData,
+        cDesc,
+        cxData,
+        dcyData,
+        dcxData,
+        weightsSpaceSize,
+        weightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+    float *y3Data;
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardWeights_v8 is not supported.
+// CHECK:     */
+    e = cudnnRNNBackwardWeights_v8(
+        handle,
+        rnnDesc,
+        WGMode,
+        seqlenarray,
+        xDesc,
+        xData,
+        hDesc,
+        hxData,
+        yDesc,
+        y3Data,
+        weightsSpaceSize,
+        dweightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+    float *y4Data;
+    bool flag;
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardData_v8 is not supported.
+// CHECK:     */
+    if(flag)    
+      cudnnRNNBackwardData_v8(
+        handle,
+        rnnDesc,
+        seqlenarray,
+        yDesc,
+        y4Data,
+        dyData,
+        xDesc,
+        dxData,
+        hDesc,
+        hxData,
+        dhyData,
+        dhxData,
+        cDesc,
+        cxData,
+        dcyData,
+        dcxData,
+        weightsSpaceSize,
+        weightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+// CHECK:     /*
+// CHECK:     DPCT1007:{{[0-9]+}}: Migration of cudnnRNNBackwardWeights_v8 is not supported.
+// CHECK:     */
+    e = cudnnRNNBackwardWeights_v8(
+        handle,
+        rnnDesc,
+        WGMode,
+        seqlenarray,
+        xDesc,
+        xData,
+        hDesc,
+        hxData,
+        yDesc,
+        y4Data,
+        weightsSpaceSize,
+        dweightsData,
+        workSpaceSize, 
+        workSpaceData, 
+        reserveSpaceSize, 
+        reserveSpaceData
+    );
+
+
+    return 0;
+}
