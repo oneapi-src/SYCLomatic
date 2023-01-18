7e6f4413381a3c6fd178dac866e0bc093a113fe0
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-01-18 16:32:00 +0800

    Refine logic
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/CallExprRewriterCUFFT.cpp
+++ clang/lib/DPCT/CallExprRewriterCUFFT.cpp
@@ -19,25 +19,25 @@ public:
   const Expr *E = nullptr;
 
   template <class StreamT> void print(StreamT &Stream) const {
+    requestFeature(HelperFeatureEnum::FftUtils_fft_engine, E);
     Expr::EvalResult ER;
-    bool Evaluated = false;
     int64_t Value = 0;
     if (!E->isValueDependent() &&
         E->EvaluateAsInt(ER, DpctGlobalInfo::getContext())) {
-      Evaluated = true;
       Value = ER.Val.getInt().getExtValue();
+      if (Evaluated && (Value == -1)) {
+        Stream << MapNames::getDpctNamespace() << "fft::fft_direction::forward";
+        return;
+      } else if (Evaluated && (Value == 1)) {
+        Stream << MapNames::getDpctNamespace()
+               << "fft::fft_direction::backward";
+        return;
+      }
     }
-    if (Evaluated && (Value == -1)) {
-      Stream << MapNames::getDpctNamespace() << "fft::fft_direction::forward";
-    } else if (Evaluated && (Value == 1)) {
-      Stream << MapNames::getDpctNamespace() << "fft::fft_direction::backward";
-    } else {
-      clang::dpct::print(Stream, E);
-      Stream << " == 1 ? " << MapNames::getDpctNamespace()
-             << "fft::fft_direction::backward : "
-             << MapNames::getDpctNamespace() << "fft::fft_direction::forward";
-    }
-    requestFeature(HelperFeatureEnum::FftUtils_fft_engine, E);
+    clang::dpct::print(Stream, E);
+    Stream << " == 1 ? " << MapNames::getDpctNamespace()
+           << "fft::fft_direction::backward : " << MapNames::getDpctNamespace()
+           << "fft::fft_direction::forward";
   }
 
   static FFTDirExpr create(const Expr *E);
