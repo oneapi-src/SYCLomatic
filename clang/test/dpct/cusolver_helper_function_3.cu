a9837b2d1a9372a70697eae060fc85085fffbdda
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-01-30 15:31:51 +0800

    Update test

--- clang/test/dpct/cusolver_helper_function_3.cu
+++ clang/test/dpct/cusolver_helper_function_3.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0
 // RUN: dpct --format-range=none -out-root %T/cusolver_helper_function_3 %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/cusolver_helper_function_3/cusolver_helper_function_3.dp.cpp --match-full-lines %s
 
