a1617266c974aeefc09fbfed7117d409909c626f
Wang, Yihan <yihan.wang@intel.com>
2023-02-02 10:08:53 +0800

    [SYCLomatic] Fix lit
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/devicelevel/device_unique_by_key.cu
+++ clang/test/dpct/cub/devicelevel/device_unique_by_key.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.4, cuda-11.8
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.4, v11.8
 // RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_unique_by_key %S/device_unique_by_key.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/devicelevel/device_unique_by_key/device_unique_by_key.dp.cpp %s
 
