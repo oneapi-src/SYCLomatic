96699006ce592464a5314fc46f2b3fc7193a991a
Wang, Yihan <yihan.wang@intel.com>
2023-02-06 15:26:38 +0800

    [SYCLomatic] Support the migration of cub::DeviceSelect::If
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/devicelevel/device_select_if.cu
+++ clang/test/dpct/cub/devicelevel/device_select_if.cu
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
-// RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_select_flagged %S/device_select_flagged.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/devicelevel/device_select_flagged/device_select_flagged.dp.cpp --match-full-lines %s
+// RUN: dpct --format-range=none -in-root %S -out-root %T/devicelevel/device_select_if %S/device_select_if.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/devicelevel/device_select_if/device_select_if.dp.cpp --match-full-lines %s
 
 // CHECK: #include <oneapi/dpl/execution>
 // CHECK: #include <oneapi/dpl/algorithm>
