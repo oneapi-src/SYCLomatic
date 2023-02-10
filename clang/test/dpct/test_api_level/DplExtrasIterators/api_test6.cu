da10371517c38014f77e769c22739072092166ff
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-02-10 08:47:03 +0800

    Fix test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/DplExtrasIterators/api_test6.cu
+++ clang/test/dpct/test_api_level/DplExtrasIterators/api_test6.cu
@@ -1,4 +1,4 @@
-// RUN: dpct --format-range=none --use-custom-helper=api -out-root %T/DplExtrasIterators/api_test6_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17
+// RUN: dpct --format-range=none --use-custom-helper=api -out-root %T/DplExtrasIterators/api_test6_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17 -fno-delayed-template-parsing
 // RUN: grep "IsCalled" %T/DplExtrasIterators/api_test6_out/MainSourceFiles.yaml | wc -l > %T/DplExtrasIterators/api_test6_out/count.txt
 // RUN: FileCheck --input-file %T/DplExtrasIterators/api_test6_out/count.txt --match-full-lines %s
 // RUN: rm -rf %T/DplExtrasIterators/api_test6_out
