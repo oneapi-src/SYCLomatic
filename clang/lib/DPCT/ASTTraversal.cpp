b0b04ee9d26dd2721cc472b8eb363ce7f1cdff88
chenwei.sun <chenwei.sun@intel.com>
2023-02-20 15:01:12 +0800

    Address review comments
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -11428,7 +11428,8 @@ void MemoryMigrationRule::miscMigration(const MatchFinder::MatchResult &Result,
       }
       emplaceTransformation(new ReplaceStmt(C, OS.str()));
       requestFeature(HelperFeatureEnum::Device_device_ext_get_memory_info, C);
-      report(C->getBeginLoc(), Diagnostics::EXTENSION_DEVICE_INFO, false, Name);
+      report(C->getBeginLoc(), Diagnostics::EXTENSION_DEVICE_INFO, false,
+             Name == "cuMemGetInfo_v2" ? "cuMemGetInfo" : Name);
     } else {
       auto &SM = DpctGlobalInfo::getSourceManager();
       std::ostringstream OS;
