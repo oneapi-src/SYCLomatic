c26ee396e4e6fc3f784be529f1127bb9493ebc79
zhiweij1 <zhiwei.jiang@intel.com>
2023-02-24 13:39:50 +0800

    [SYCLomatic] Update DPCT1101/1106 message and replace reserved DPCT1105 with DPCT1104 in APINames.inc (#663)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/macro_test.cu
+++ clang/test/dpct/macro_test.cu
@@ -1107,8 +1107,8 @@ template<class T1, class T2, int N> __global__ void foo31();
 //CHECK:   q_ct1.submit([&](sycl::handler &cgh) {
 //CHECK-NEXT:     /*
 //CHECK-NEXT:     DPCT1101:{{[0-9]+}}: 'BLOCK_PAIR / SIMD_SIZE' expression was replaced with a
-//CHECK-NEXT:     value. Modify the code to use original expression, provided in comments,
-//CHECK-NEXT:     if it is correct.
+//CHECK-NEXT:     value. Modify the code to use the original expression, provided in
+//CHECK-NEXT:     comments, if it is correct.
 //CHECK-NEXT:     */
 //CHECK-NEXT:     sycl::local_accessor<double, 2> red_acc_acc_ct1(
 //CHECK-NEXT:         sycl::range<2>(8, 8 /*BLOCK_PAIR / SIMD_SIZE*/), cgh);
