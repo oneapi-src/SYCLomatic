c10c48b9b19e797426d3b2da452ba520e686bead
0x12CC <68250218+0x12CC@users.noreply.github.com>
2023-02-28 19:07:31 -0500

    [SYCLomatic] Add migration for cusolverEigRange_t (#651)
    * [SYCLomatic] Add migration for cusolverEigRange_t
    
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>
    
    * [SYCLomatic] Update mapping to use MKL enum
    
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>
    
    ---------
    
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- clang/lib/DPCT/MapNames.cpp
+++ clang/lib/DPCT/MapNames.cpp
@@ -426,6 +426,7 @@ void MapNames::setExplicitNamespaceMap() {
       {"cuda::std::tuple", std::make_shared<TypeNameRule>("std::tuple")},
       {"cuda::std::complex", std::make_shared<TypeNameRule>("std::complex")},
       {"cuda::std::array", std::make_shared<TypeNameRule>("std::array")},
+      {"cusolverEigRange_t", std::make_shared<TypeNameRule>("oneapi::mkl::rangev")},
       // ...
   };
 
@@ -1193,6 +1194,9 @@ void MapNames::setExplicitNamespaceMap() {
       {"ncclFloat64", std::make_shared<EnumNameRule>("oneapi::ccl::datatype::float64")},
       {"ncclDouble", std::make_shared<EnumNameRule>("oneapi::ccl::datatype::float64")},
       {"ncclBfloat16", std::make_shared<EnumNameRule>("oneapi::ccl::datatype::bfloat16")},
+      {"CUSOLVER_EIG_RANGE_ALL", std::make_shared<EnumNameRule>("oneapi::mkl::rangev::all")},
+      {"CUSOLVER_EIG_RANGE_V", std::make_shared<EnumNameRule>("oneapi::mkl::rangev::values")},
+      {"CUSOLVER_EIG_RANGE_I", std::make_shared<EnumNameRule>("oneapi::mkl::rangev::indices")},
       // ...
   };
 
