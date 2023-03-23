f63464132511373bf0f0357552c2daf3ab7d86d3
Chen, Sheng S <sheng.s.chen@intel.com>
2023-03-23 16:24:50 +0800

    Update test case.

--- clang/test/dpct/recursive_function.cu
+++ clang/test/dpct/recursive_function.cu
@@ -1,3 +1,5 @@
+// RUN: dpct --format-range=none -out-root %T/recursive_function %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck %s --match-full-lines --input-file %T/recursive_function/recursive_function.dp.cpp
 #include <cuda.h>
 // CHECK: /*
 // CHECK-NEXT: DPCT1110:{{[0-9]+}}: Recursive functions cannot be called in a SYCL kernel or by functions called by the kernel. You may need to adjust the code.
