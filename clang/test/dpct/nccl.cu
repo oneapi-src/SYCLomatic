6d15fdf4ad1be0c5d60c5a705d972a6aa45b5656
Ni, Wenhui <wenhui.ni@intel.com>
2023-03-24 09:43:37 +0800

    fix conflict
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- clang/test/dpct/nccl.cu
+++ clang/test/dpct/nccl.cu
@@ -128,7 +128,7 @@ int main() {
 
     
     // CHECK:     /*
-    // CHECK-NEXT: DPCT1107:{{[0-9]+}}: OneCCL currently only supports in-plane Broadcast operations. You may need to modify the code.
+    // CHECK-NEXT: DPCT1109:{{[0-9]+}}: OneCCL currently only supports in-plane Broadcast operations. You may need to modify the code.
     // CHECK-NEXT: */
     // CHECK: comm->broadcast(recvbuff, count, dpct::library_data_t::real_int8, rank, stream);
     ncclBroadcast(buff, recvbuff, count, ncclChar, rank, comm, stream);
