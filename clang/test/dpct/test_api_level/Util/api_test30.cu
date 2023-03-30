9a54165aa64f2a000f8a0aad96ff7ce09336b398
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-03-30 11:14:59 +0800

    Fix bug in lit
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/Util/api_test30.cu
+++ clang/test/dpct/test_api_level/Util/api_test30.cu
@@ -1,7 +1,7 @@
-// RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/Util/api_test29_out %s --cuda-include-path="%cuda-path/include"
-// RUN: grep "IsCalled" %T/Util/api_test29_out/MainSourceFiles.yaml | wc -l > %T/Util/api_test29_out/count.txt
-// RUN: FileCheck --input-file %T/Util/api_test29_out/count.txt --match-full-lines %s
-// RUN: rm -rf %T/Util/api_test29_out
+// RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/Util/api_test30_out %s --cuda-include-path="%cuda-path/include"
+// RUN: grep "IsCalled" %T/Util/api_test30_out/MainSourceFiles.yaml | wc -l > %T/Util/api_test30_out/count.txt
+// RUN: FileCheck --input-file %T/Util/api_test30_out/count.txt --match-full-lines %s
+// RUN: rm -rf %T/Util/api_test30_out
 
 // CHECK: 2
 // TEST_FEATURE: Math_min_max
