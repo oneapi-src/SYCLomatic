51569905c70df894c415544f8b739f442e87528c
Kristal Dale <kristal.dale@intel.com>
2023-04-06 19:35:33 -0700

    [SYCLomatic][DOC] Add README for docs (#804)
    Add README with info re set up to build docs locally.
    Update conf.py to remove un-used extension.
    
    Signed-off-by: Kristal Dale <kristal.dale@intel.com>

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Intel'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx2dita']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
