b45b4533d496331afd3dab9d163baffaaf46ac1e
Hao Wang <hao3.wang@intel.com>
2023-04-10 17:36:25 +0800

    Emit warning message
    Signed-off-by: Hao Wang <hao3.wang@intel.com>

---
+++ clang/test/dpct/register_pressure.cu
@@ -0,0 +1,15 @@
+// RUN: dpct --format-range=none -out-root %T/register_pressure %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck %s --match-full-lines --input-file %T/register_pressure/register_pressure.dp.cpp
+#include<cuda_runtime.h>
+
+// CHECK: /*
+// CHECK: DPCT1110:{{[0-9]+}}: The total local variable size in device function exceeds 128 bytes and may cause high register pressure. Consider replacing sub-group size with smaller value to reduce register pressure for better performance.
+// CHECK: */
+__global__ void kernel(){
+  int a[100];
+}
+
+int main(void) {
+    kernel<<<1, 1>>>();
+    return 0;
+}
