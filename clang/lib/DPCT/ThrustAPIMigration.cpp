6c51bc2981de69a7512054833ade28770238a442
tomflinda <chenwei.sun@intel.com>
2023-04-21 13:40:03 +0800

    [SYCLomatic] Support migration of thrust::system_error and thrust::cuda_category (#825)
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/lib/DPCT/ThrustAPIMigration.cpp
+++ clang/lib/DPCT/ThrustAPIMigration.cpp
@@ -34,6 +34,10 @@ void ThrustAPIRule::registerMatcher(ast_matchers::MatchFinder &MF) {
   MF.addMatcher(typedefDecl(isExpandedFromMacro("THRUST_STATIC_ASSERT"))
                     .bind("THRUST_STATIC_ASSERT"),
                 this);
+  MF.addMatcher(cxxTemporaryObjectExpr(
+                    hasType(namedDecl(hasName("thrust::system::system_error"))))
+                    .bind("THRUST_SYSTEM_ERROR"),
+                this);
 }
 
 void ThrustAPIRule::runRule(
@@ -49,6 +53,14 @@ void ThrustAPIRule::runRule(
     const SourceLocation BeginLoc = SM.getExpansionLoc(D->getBeginLoc());
     emplaceTransformation(new ReplaceText(
         BeginLoc, std::string("THRUST_STATIC_ASSERT").size(), "static_assert"));
+  } else if (const CXXTemporaryObjectExpr *CXXTempObj =
+                 getNodeAsType<CXXTemporaryObjectExpr>(Result,
+                                                       "THRUST_SYSTEM_ERROR")) {
+    dpct::ExprAnalysis EA;
+    EA.analyze(CXXTempObj);
+    emplaceTransformation(EA.getReplacement());
+    EA.applyAllSubExprRepl();
+    return;
   }
 }
 
