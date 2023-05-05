b9e76fb98f07fbb902f8d1c1597bee81b308461f
chenwei.sun <chenwei.sun@intel.com>
2023-05-06 06:49:36 +0800

    [SYCLomatic] Fix crash issue when intercept-build tool processes the option "-arch=all"
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/tools/scan-build-py/lib/libscanbuild/compilation.py
+++ clang/tools/scan-build-py/lib/libscanbuild/compilation.py
@@ -277,6 +277,8 @@ def parse_args(args):
                 new_flag += arg_next
             elif arg == '--gpu-architecture' or arg == '-arch':
                 arg_next = next(args)
+                if arg_next == "all": # Skip option "-arch all" and "--gpu-architecture all"
+                    continue
                 arg_next = gpu_virtual_arch_to_arch(arg_next)
                 # clang cuda parser does not support CUDA gpu architecture: sm_13
                 if(arg_next == "sm_13"):
@@ -420,6 +422,10 @@ def parse_args(args):
         elif re.match(r'^--gpu-architecture=', arg) or re.match(r'^-arch=', arg):
             #split by '=' and strip whitespace
             result = [x.strip() for x in arg.split('=')]
+
+            if result[1] == 'all': # Skip option "-arch=all" and "--gpu-architecture=all"
+                continue
+
             new_opt = MAP_FLAGS[result[0]] + gpu_virtual_arch_to_arch(result[1])
             flags.append(new_opt)
             pass
