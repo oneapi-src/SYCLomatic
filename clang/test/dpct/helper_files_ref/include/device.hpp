115884ee5f4d8074ae6c87692a5e691ade0080e9
Huang, Andy <andy.huang@intel.com>
2023-05-20 00:16:08 +0800

    Fix lit
    Signed-off-by: Huang, Andy <andy.huang@intel.com>

--- clang/test/dpct/helper_files_ref/include/device.hpp
+++ clang/test/dpct/helper_files_ref/include/device.hpp
@@ -574,7 +574,7 @@ public:
     return id;
   }
 
-  template <DeviceSelector>
+  template <class DeviceSelector>
   void select_device(const DeviceSelector &selector = sycl::gpu_selector_v) {
     sycl::device selected_device = sycl::device(selector);
     unsigned int selected_device_id = get_device_id(selected_device);
