832337f56240fa4bb3b20ad7a80021ac25b26549
Huang, Andy <andy.huang@intel.com>
2023-05-23 16:09:31 +0800

    Fix lit fail
    Signed-off-by: Huang, Andy <andy.huang@intel.com>

--- clang/test/dpct/occupancy_expr.cu
+++ clang/test/dpct/occupancy_expr.cu
@@ -8,14 +8,14 @@ int main() {
   int block_size = 128;
   size_t dynamic_shared_memory_size = 0;
   // CHECK: /*
-  // CHECK: DPCT1111:{{[0-9]+}}: Please verify the input arguments of dpct::experimental::calculate_max_active_wg_per_xecore base on the target function "k".
+  // CHECK: DPCT1111:{{[0-9]+}}: Please verify the input arguments of "dpct::experimental::calculate_max_active_wg_per_xecore" base on the target function "k".
   // CHECK: */
   // CHECK: dpct::experimental::calculate_max_active_wg_per_xecore(&num_blocks, block_size, dynamic_shared_memory_size + dpct_placeholder /* total share local memory size */);
   cudaOccupancyMaxActiveBlocksPerMultiprocessor(&num_blocks, k, block_size, dynamic_shared_memory_size);
 
   CUfunction func;
   // CHECK: /*
-  // CHECK: DPCT1111:{{[0-9]+}}: Please verify the input arguments of dpct::experimental::calculate_max_active_wg_per_xecore base on the target function "func".
+  // CHECK: DPCT1111:{{[0-9]+}}: Please verify the input arguments of "dpct::experimental::calculate_max_active_wg_per_xecore" base on the target function "func".
   // CHECK: */
   // CHECK: dpct::experimental::calculate_max_active_wg_per_xecore(&num_blocks, block_size, dynamic_shared_memory_size + dpct_placeholder /* total share local memory size */);
   cuOccupancyMaxActiveBlocksPerMultiprocessor(&num_blocks, func, block_size, dynamic_shared_memory_size);
