e61400b0c958c4741fa69cd3964483a716e0ebc4
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-05-24 09:54:57 +0800

    Fix test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/test_api_level/DplExtrasMemory/api_test12.cu
+++ clang/test/dpct/test_api_level/DplExtrasMemory/api_test12.cu
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0
+// UNSUPPORTED: v8.0
 // RUN: dpct --format-range=none  --usm-level=none  --use-custom-helper=api -out-root %T/DplExtrasMemory/api_test12_out %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only -std=c++17 -fsized-deallocation
 // RUN: grep "IsCalled" %T/DplExtrasMemory/api_test12_out/MainSourceFiles.yaml | wc -l > %T/DplExtrasMemory/api_test12_out/count.txt
 // RUN: FileCheck --input-file %T/DplExtrasMemory/api_test12_out/count.txt --match-full-lines %s
@@ -9,7 +11,8 @@
 #include <thrust/device_vector.h>
 #include <thrust/memory.h>
 
-void main() {
+int main() {
   thrust::device_vector<int> d_vec;
   int &ref1 = thrust::raw_reference_cast(d_vec[0]);
+  return 0;
 }
