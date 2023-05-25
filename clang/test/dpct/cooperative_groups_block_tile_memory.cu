36adf43f5cc362fc89eb91b4d844e650ac440772
Chen, Sheng S <sheng.s.chen@intel.com>
2023-05-25 13:57:40 +0800

    Update the case

--- clang/test/dpct/cooperative_groups_block_tile_memory.cu
+++ clang/test/dpct/cooperative_groups_block_tile_memory.cu
@@ -1,5 +1,5 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0 
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1, v11.2, v11.3, v11.4, v11.5, v11.6, v11.7, v11.8
 // RUN: dpct --format-range=none -out-root %T/cooperative_groups_block_tile_memory %s --cuda-include-path="%cuda-path/include" -use-experimental-features=free-function-queries,logical-group -- -x cuda --cuda-host-only -std=c++14
 // RUN: FileCheck %s --match-full-lines --input-file %T/cooperative_groups_block_tile_memory/cooperative_groups_block_tile_memory.dp.cpp
 
