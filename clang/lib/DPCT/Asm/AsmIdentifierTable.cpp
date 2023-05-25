9eb0042da7810df78ef383258ea01b02efade122
Yihan Wang <yihan.wang@intel.com>
2023-05-25 17:03:23 +0800

    [SYCLomatic] Refine the migration rule for device ASM code by introducing an inline device ASM code parser. (#796)
    Support migration of mov and setp instruction, variable declaration and parameterized name variable. 
    Refactor lop3 migration.
    
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

---
+++ clang/lib/DPCT/Asm/AsmIdentifierTable.cpp
@@ -0,0 +1,31 @@
+//===------------------ AsmIdentifierTable.cpp ------------------*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "AsmIdentifierTable.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang::dpct;
+
+InlineAsmIdentifierInfoLookup::~InlineAsmIdentifierInfoLookup() = default;
+
+InlineAsmIdentifierTable::InlineAsmIdentifierTable(
+    InlineAsmIdentifierInfoLookup *ExternalLookup)
+    : HashTable(128), // Start with space for 8K identifiers.
+      ExternalLookup(ExternalLookup) {
+  // Populate the identifier table with info about keywords for the current
+  // language.
+  AddKeywords();
+}
+
+void InlineAsmIdentifierTable::AddKeywords() {
+#define KEYWORD(X, Y) get(#X, asmtok::kw_##X);
+#define BUILTIN_ID(X, Y, Z) get(Y, asmtok::bi_##X).setBuiltinID();
+#define BUILTIN_TYPE(X, Y) get(#X, asmtok::kw_##X).setBuiltinType();
+#define INSTRUCTION(X) get(#X, asmtok::op_##X).setInstruction();
+#include "AsmTokenKinds.def"
+}
