8a1a2d56ba02c9bdeb42cb1f56538560fd4f8189
Wang, Yihan <yihan.wang@intel.com>
2023-05-26 16:25:32 +0800

    [SYCLomatic] Fix the migration of the builtin identifier %laneid in asm
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/lib/DPCT/AsmMigration.cpp
+++ clang/lib/DPCT/AsmMigration.cpp
@@ -346,7 +346,7 @@ bool SYCLGenBase::emitDeclRefExpression(const InlineAsmDeclRefExpr *E) {
   if (E->getDecl().getDeclName()->isBuiltinID()) {
     switch (E->getDecl().getDeclName()->getTokenID()) {
     case asmtok::bi_laneid:
-      OS() << getItemName() << ".get_local_id()";
+      OS() << getItemName() << ".get_local_linear_id()";
       break;
     default:
       return SYCLGenError();
