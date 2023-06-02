37797a47204a7f1e5341010a8aa72d45c743ccac
Yihan Wang <yihan.wang@intel.com>
2023-06-02 17:01:02 +0800

    [SYCLomatic] Add migration support of ASM used in macro and fix bug in inline ASM migration and  (#948)
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/lib/DPCT/Asm/AsmIdentifierTable.cpp
+++ clang/lib/DPCT/Asm/AsmIdentifierTable.cpp
@@ -23,9 +23,9 @@ InlineAsmIdentifierTable::InlineAsmIdentifierTable(
 }
 
 void InlineAsmIdentifierTable::AddKeywords() {
-#define KEYWORD(X, Y) get(#X, asmtok::kw_##X);
+#define KEYWORD(X, Y) get(Y, asmtok::kw_##X);
 #define BUILTIN_ID(X, Y, Z) get(Y, asmtok::bi_##X).setBuiltinID();
-#define BUILTIN_TYPE(X, Y) get(#X, asmtok::kw_##X).setBuiltinType();
+#define BUILTIN_TYPE(X, Y) get(Y, asmtok::kw_##X).setBuiltinType();
 #define INSTRUCTION(X) get(#X, asmtok::op_##X).setInstruction();
 #include "AsmTokenKinds.def"
 }
