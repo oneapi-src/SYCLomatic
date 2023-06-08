51432a9a43198ea853b6146372e581ee1c9f527c
Wang, Yihan <yihan.wang@intel.com>
2023-06-08 10:01:14 +0800

    [SYCLomatic] Fix lit
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/intrinsic/warpid.cu
+++ clang/test/dpct/cub/intrinsic/warpid.cu
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none -in-root %S -out-root %T/intrinsic/warpid %S/warpid.cu --cuda-include-path="%cuda-path/include" -- -std=c++17 -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/intrinsic/warpid/warpid.dp.cpp --match-full-lines %s
+// RUN: FileCheck --input-file %T/intrinsic/warpid/warpid.dp.cpp %s
 
 // CHECK:#include <sycl/sycl.hpp>
 // CHECK:#include <dpct/dpct.hpp>
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 // CHECK: void warpid(int *id, int *ws, const sycl::nd_item<3> &item_ct1) {
-// CHECK-NEXT:   int tid = item_ct1.get_local_id(2) + item_ct1.get_group(2)* item_ct1.get_local_range(2);
+// CHECK-NEXT:   int tid = item_ct1.get_local_id(2) + item_ct1.get_group(2) * item_ct1.get_local_range(2);
 // CHECK-NEXT:   id[tid] = item_ct1.get_sub_group().get_group_linear_id();
 // CHECK-NEXT:   *ws = item_ct1.get_sub_group().get_local_range().get(0);
 // CHECK-NEXT: }
