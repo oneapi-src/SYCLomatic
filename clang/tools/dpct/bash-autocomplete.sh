2cef0d0acfc8b26bffb0f570ba0968225ba91313
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-06-13 11:16:04 +0800

    Update
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/tools/dpct/bash-autocomplete.sh
+++ clang/tools/dpct/bash-autocomplete.sh
@@ -1,10 +1,10 @@
-# ===----- bash-autocomplete.sh -------------------------------------------===#
+# ===----- bash-autocomplete.sh -------------------------------------------=== #
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# ===----------------------------------------------------------------------===#
+# ===----------------------------------------------------------------------=== #
 
 # Please add "source /path/to/bash-autocomplete.sh" to your .bashrc to use this.
 
