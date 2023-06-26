636b2d9e4b5607463fb734b0f180c2a38953e760
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-06-26 16:05:28 +0800

    Remove inc files
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/GenHelperFunction.cpp
+++ clang/lib/DPCT/GenHelperFunction.cpp
@@ -19,76 +19,76 @@ namespace clang {
 namespace dpct {
 
 const std::string DpctAllContentStr =
-#include "clang/DPCT/dpct.all.inc"
+#include "clang/DPCT/dpct.hpp.inc"
     ;
 const std::string AtomicAllContentStr =
-#include "clang/DPCT/atomic.all.inc"
+#include "clang/DPCT/atomic.hpp.inc"
     ;
 const std::string BlasUtilsAllContentStr =
-#include "clang/DPCT/blas_utils.all.inc"
+#include "clang/DPCT/blas_utils.hpp.inc"
     ;
 const std::string DnnlUtilsAllContentStr =
-#include "clang/DPCT/dnnl_utils.all.inc"
+#include "clang/DPCT/dnnl_utils.hpp.inc"
     ;
 const std::string DeviceAllContentStr =
-#include "clang/DPCT/device.all.inc"
+#include "clang/DPCT/device.hpp.inc"
     ;
 const std::string DplUtilsAllContentStr =
-#include "clang/DPCT/dpl_utils.all.inc"
+#include "clang/DPCT/dpl_utils.hpp.inc"
     ;
 const std::string ImageAllContentStr =
-#include "clang/DPCT/image.all.inc"
+#include "clang/DPCT/image.hpp.inc"
     ;
 const std::string KernelAllContentStr =
-#include "clang/DPCT/kernel.all.inc"
+#include "clang/DPCT/kernel.hpp.inc"
     ;
 const std::string MathAllContentStr =
-#include "clang/DPCT/math.all.inc"
+#include "clang/DPCT/math.hpp.inc"
     ;
 const std::string MemoryAllContentStr =
-#include "clang/DPCT/memory.all.inc"
+#include "clang/DPCT/memory.hpp.inc"
     ;
 const std::string UtilAllContentStr =
-#include "clang/DPCT/util.all.inc"
+#include "clang/DPCT/util.hpp.inc"
     ;
 const std::string RngUtilsAllContentStr =
-#include "clang/DPCT/rng_utils.all.inc"
+#include "clang/DPCT/rng_utils.hpp.inc"
     ;
 const std::string LibCommonUtilsAllContentStr =
-#include "clang/DPCT/lib_common_utils.all.inc"
+#include "clang/DPCT/lib_common_utils.hpp.inc"
     ;
 const std::string CclUtilsAllContentStr =
-#include "clang/DPCT/ccl_utils.all.inc"
+#include "clang/DPCT/ccl_utils.hpp.inc"
     ;
 const std::string SparseUtilsAllContentStr =
-#include "clang/DPCT/sparse_utils.all.inc"
+#include "clang/DPCT/sparse_utils.hpp.inc"
     ;
 const std::string FftUtilsAllContentStr =
-#include "clang/DPCT/fft_utils.all.inc"
+#include "clang/DPCT/fft_utils.hpp.inc"
     ;
 const std::string LapackUtilsAllContentStr =
-#include "clang/DPCT/lapack_utils.all.inc"
+#include "clang/DPCT/lapack_utils.hpp.inc"
     ;
 const std::string DplExtrasAlgorithmAllContentStr =
-#include "clang/DPCT/dpl_extras/algorithm.all.inc"
+#include "clang/DPCT/dpl_extras/algorithm.h.inc"
     ;
 const std::string DplExtrasFunctionalAllContentStr =
-#include "clang/DPCT/dpl_extras/functional.all.inc"
+#include "clang/DPCT/dpl_extras/functional.h.inc"
     ;
 const std::string DplExtrasIteratorsAllContentStr =
-#include "clang/DPCT/dpl_extras/iterators.all.inc"
+#include "clang/DPCT/dpl_extras/iterators.h.inc"
     ;
 const std::string DplExtrasMemoryAllContentStr =
-#include "clang/DPCT/dpl_extras/memory.all.inc"
+#include "clang/DPCT/dpl_extras/memory.h.inc"
     ;
 const std::string DplExtrasNumericAllContentStr =
-#include "clang/DPCT/dpl_extras/numeric.all.inc"
+#include "clang/DPCT/dpl_extras/numeric.h.inc"
     ;
 const std::string DplExtrasVectorAllContentStr =
-#include "clang/DPCT/dpl_extras/vector.all.inc"
+#include "clang/DPCT/dpl_extras/vector.h.inc"
     ;
 const std::string DplExtrasDpcppExtensionsAllContentStr =
-#include "clang/DPCT/dpl_extras/dpcpp_extensions.all.inc"
+#include "clang/DPCT/dpl_extras/dpcpp_extensions.h.inc"
     ;
 
 void replaceAllOccurredStrsInStr(std::string &StrNeedProcess,
--- clang/runtime/dpct-rt/processFiles.py
+++ clang/runtime/dpct-rt/processFiles.py
@@ -15,40 +15,24 @@ import io
 import argparse
 
 cur_file_dir = os.path.dirname(os.path.realpath(__file__))
-content_files_list = ["atomic", "blas_utils", "device",
-                      "dpct", "dpl_utils", "image", "kernel", "math", "memory", "util", "rng_utils", "lib_common_utils",
-                      "dnnl_utils", "ccl_utils", "fft_utils", "lapack_utils", "sparse_utils"]
-dpl_extras_content_files_list = [
-    "algorithm", "functional", "iterators", "memory", "numeric", "vector", "dpcpp_extensions"]
 
-input_files_dir = os.path.join(cur_file_dir, "include")
+runtime_header_src_files_dir = os.path.join(cur_file_dir, "include")
 
 def exit_script():
     sys.exit()
 
-def get_file_paths(cont_file, inc_files_dir, runtime_files_dir, is_dpl_extras):
+def get_file_paths(runtime_src_file, inc_files_dir, is_dpl_extras):
     if (is_dpl_extras):
-        cont_file_result = os.path.join(
-            input_files_dir, "dpl_extras/", cont_file + ".h")
-        runtime_files_dir_result = os.path.join(
-            runtime_files_dir, "dpl_extras/", cont_file + ".h")
-        inc_files_all_dir_result = os.path.join(
-            inc_files_dir, "dpl_extras/", cont_file + ".all.inc")
-        return [cont_file_result, runtime_files_dir_result, inc_files_all_dir_result]
+        cont_file_result = os.path.join(runtime_header_src_files_dir, "dpl_extras/", runtime_src_file)
+        inc_files_all_dir_result = os.path.join(inc_files_dir, "dpl_extras/", runtime_src_file + ".inc")
+        return [cont_file_result, inc_files_all_dir_result]
     else:
-        cont_file_result = os.path.join(input_files_dir, cont_file + ".hpp")
-        runtime_files_dir_result = os.path.join(
-            runtime_files_dir, cont_file + ".hpp")
-        inc_files_all_dir_result = os.path.join(
-            inc_files_dir, cont_file + ".all.inc")
-        return [cont_file_result, runtime_files_dir_result, inc_files_all_dir_result]
+        cont_file_result = os.path.join(runtime_header_src_files_dir, runtime_src_file)
+        inc_files_all_dir_result = os.path.join(inc_files_dir, runtime_src_file + ".inc")
+        return [cont_file_result, inc_files_all_dir_result]
 
 
-def create_dir(inc_files_dir, runtime_files_dir):
-    if (not os.path.exists(os.path.join(runtime_files_dir))):
-        os.makedirs(os.path.join(runtime_files_dir))
-    if (not os.path.exists(os.path.join(runtime_files_dir, "dpl_extras/"))):
-        os.makedirs(os.path.join(runtime_files_dir, "dpl_extras/"))
+def create_dir(inc_files_dir):
     if (not os.path.exists(os.path.join(inc_files_dir))):
         os.makedirs(os.path.join(inc_files_dir))
     if (not os.path.exists(os.path.join(inc_files_dir, "dpl_extras/"))):
@@ -57,23 +41,15 @@ def create_dir(inc_files_dir, runtime_files_dir):
 def convert_to_cxx_code(line_code):
     return bytes("R\"Delimiter(", 'utf-8') + line_code + bytes(")Delimiter\"\n", 'utf-8')
 
-def process_a_file(cont_file, inc_files_dir, runtime_files_dir, is_dpl_extras):
-    file_names = get_file_paths(
-        cont_file, inc_files_dir, runtime_files_dir, is_dpl_extras)
+def process_a_file(runtime_src_file, inc_files_dir, is_dpl_extras):
+    file_names = get_file_paths(runtime_src_file, inc_files_dir, is_dpl_extras)
     cont_file_handle = io.open(file_names[0], "rb")
-    runtime_file_handle = io.open(file_names[1], "w+b")
-    inc_all_file_handle = io.open(file_names[2], "w+b")
+    inc_all_file_handle = io.open(file_names[1], "w+b")
 
     inc_all_file_lines = []
-    runtime_file_lines = []
 
     for line in cont_file_handle:
-        runtime_file_lines.append(line)
         inc_all_file_lines.append(convert_to_cxx_code(line))
-
-    runtime_file_str = bytes("", 'utf-8')
-    for line in runtime_file_lines:
-        runtime_file_str = runtime_file_str + line
 
     # cl.exe will emit error if a literal string length >= 65535
     # so we need convert the generated code from style
@@ -104,12 +80,18 @@ def process_a_file(cont_file, inc_files_dir, runtime_files_dir, is_dpl_extras):
             string_literal_length_counter = len(line)
     inc_all_file_str = inc_all_file_str + bytes(")", 'utf-8')
 
-    runtime_file_handle.write(runtime_file_str)
     inc_all_file_handle.write(inc_all_file_str)
 
     cont_file_handle.close()
-    runtime_file_handle.close()
     inc_all_file_handle.close()
+
+def get_files(path):
+    items = os.listdir(path)
+    files = []
+    for item in items:
+        if (os.path.isfile(os.path.join(path, item))):
+            files.append(item)
+    return files
 
 def main():
     if (sys.version_info.major < 3):
@@ -117,46 +99,29 @@ def main():
         exit_script()
 
     parser = argparse.ArgumentParser(description="Processing files.")
-    parser.add_argument('--build-dir', action='store', default='',
-                        help='The build directory.')
-    parser.add_argument('--inc-output-dir', action='store', default='',
-                        help='The path of the output inc files. These inc files will be included by src code. Ignored when --build-dir specified.')
-    parser.add_argument('--helper-header-output-dir', action='store', default='',
-                        help='The path of the output runtime files. These files are the final full set helper header files. Ignored when --build-dir specified.')
-    parser.add_argument('--input-inc-dir', action='store', default='',
-                        help='The path of the input *.inc files. Default value is dir where this script at.')
+    parser.add_argument('--build-dir', action='store', default='', help='The build directory.')
     args = parser.parse_args()
 
     inc_files_dir = str()
-    runtime_files_dir = str()
 
     if (args.build_dir):
         inc_files_dir = os.path.join(
             args.build_dir, "tools/clang/include/clang/DPCT")
-        runtime_files_dir = os.path.join(
-            args.build_dir, "tools/clang/runtime/dpct-rt/include")
     else:
-        if (args.inc_output_dir and args.helper_header_output_dir):
-            inc_files_dir = args.inc_output_dir
-            runtime_files_dir = args.helper_header_output_dir
-        else:
-            print("Error: inc-output-dir or helper-header-output-dir is empty.")
-            exit_script()
+        print("Error: build-dir is empty.")
+        exit_script()
 
-    if (args.input_inc_dir):
-        input_files_dir = args.input_inc_dir
+    create_dir(inc_files_dir)
 
-    create_dir(inc_files_dir, runtime_files_dir)
+    helper_files_list = get_files(runtime_header_src_files_dir)
+    dpl_extras_helper_files_list = get_files(os.path.join(runtime_header_src_files_dir, "dpl_extras"))
 
-    for cont_file in content_files_list:
-        process_a_file(cont_file, inc_files_dir,
-                       runtime_files_dir, False)
-    for cont_file in dpl_extras_content_files_list:
-        process_a_file(cont_file, inc_files_dir, runtime_files_dir,
-                       True)
+    for runtime_src_file in helper_files_list:
+        process_a_file(runtime_src_file, inc_files_dir, False)
+    for runtime_src_file in dpl_extras_helper_files_list:
+        process_a_file(runtime_src_file, inc_files_dir, True)
 
     print("[Note] DPCT *.inc files are generated successfully!")
-
 
 if __name__ == "__main__":
     main()
