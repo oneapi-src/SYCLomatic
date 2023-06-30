0de6c503ca369cbfe52781e6422813ab42b54c4e
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-06-30 11:16:57 +0800

    WIP
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/MapNames.cpp
+++ clang/lib/DPCT/MapNames.cpp
@@ -1853,7 +1853,15 @@ void MapNames::setExplicitNamespaceMap() {
                            "cusolverDnSyevd",
                            "cusolverDnSyevd_bufferSize",
                            "cusolverDnXtrtri",
-                           "cusolverDnXtrtri_bufferSize"};
+                           "cusolverDnXtrtri_bufferSize",
+                           "cusolverDnSsyevd_bufferSize",
+                           "cusolverDnDsyevd_bufferSize",
+                           "cusolverDnCheevd_bufferSize",
+                           "cusolverDnZheevd_bufferSize",
+                           "cusolverDnSsyevd",
+                           "cusolverDnDsyevd",
+                           "cusolverDnCheevd",
+                           "cusolverDnZheevd"};
 
   SPARSEAPIWithRewriter = {"cusparseCreateMatDescr",
                            "cusparseDestroyMatDescr",
