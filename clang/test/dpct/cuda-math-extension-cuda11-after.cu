28f8c5a0e1023f0b44704cc49a08741579a0bcf0
Tang Jiajun <jiajun.tang@intel.com>
2023-07-05 16:12:36 +0800

    [SYCLomatic] Disable some lit cases on cuda-11.0~11.5 (#1056)
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- clang/test/dpct/cuda-math-extension-cuda11-after.cu
+++ clang/test/dpct/cuda-math-extension-cuda11-after.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.1, cuda-11.2, cuda-11.3, cuda-11.4, cuda-11.5
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1, v11.2, v11.3, v11.4, v11.5
 // RUN: dpct --format-range=none --use-dpcpp-extensions=intel_device_math -out-root %T/cuda-math-extension-cuda11-after %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only --std=c++14
 // RUN: FileCheck --input-file %T/cuda-math-extension-cuda11-after/cuda-math-extension-cuda11-after.dp.cpp --match-full-lines %s
 
