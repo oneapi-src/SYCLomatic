fc2414528be8ade1fd3615bd8d462c974eaded6f
Wang, Hao3 <hao3.wang@intel.com>
2023-07-10 09:44:17 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- clang/test/dpct/optimized_math_function.cu
+++ clang/test/dpct/optimized_math_function.cu
@@ -1,9 +1,8 @@
-// RUN: dpct --format-range=none --optimize-migration --usm-level=none -out-root %T/optimized_math_function %s --cuda-include-path="%cuda-path/include" --sycl-named-lambda -- -x cuda --cuda-host-only -fno-delayed-template-parsing
+// RUN: dpct --format-range=none --optimize-migration -out-root %T/optimized_math_function %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/optimized_math_function/optimized_math_function.dp.cpp --match-full-lines %s
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cmath>
-#include <math_functions.h>
 
 __device__ void sincos_1(float x, float* sptr, float* cptr) {
     // CHECK:  return [&](){ (*sptr = sycl::sin(x), *cptr = sycl::cos(x)); }();
@@ -11,7 +10,7 @@ __device__ void sincos_1(float x, float* sptr, float* cptr) {
 }
 
 __device__ void sincos_2(double x, double* sptr, double* cptr) {
-    // CHECK:  [&](){ (*sptr = sycl::sin(x), *cptr = sycl::cos(x)); }();
+    // CHECK:  return [&](){ (*sptr = sycl::sin(x), *cptr = sycl::cos(x)); }();
     return sincos(x, sptr, cptr);
 }
 
