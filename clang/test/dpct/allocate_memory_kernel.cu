6713571b76f528bf6761c12572e991319bba5d96
Shengchen <sheng.s.chen@intel.com>
2023-07-26 17:13:46 +0800

    [SYCLomatic] Emit un-support message for the dynamic memory management API using in kernel (#1099)
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/allocate_memory_kernel.cu
+++ clang/test/dpct/allocate_memory_kernel.cu
@@ -1,5 +1,7 @@
+// UNSUPPORTED: system-windows
 // RUN: dpct --format-range=none -out-root %T/allocate_memory_kernel %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck %s --match-full-lines --input-file %T/allocate_memory_kernel/allocate_memory_kernel.dp.cpp
+#include <cstdlib>
 #include <cuda.h>
 #include <stdio.h>
 
@@ -7,45 +9,74 @@
 template <typename T>
 class TestVirtualFunc {
 public:
-
-    __device__ TestVirtualFunc() {}
-    __device__ virtual ~TestVirtualFunc() {}
-    __device__ virtual void push(const T &&e)= 0;
+  __device__ TestVirtualFunc() {}
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}:  Virtual functions cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  __device__ virtual ~TestVirtualFunc() {}
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}:  Virtual functions cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  __device__ virtual void push(const T &&e) = 0;
 };
 template <typename T>
 class TestSeqContainer : public TestVirtualFunc<T> {
 public:
-// CHECK: /*
-// CHECK: DPCT1109:{{[0-9]+}}: Memory storage allocation cannot be called in SYCL device code. You need to adjust the code.
-// CHECK: */
-    __device__ TestSeqContainer(int size) : index_top(-1) { m_data = new T[size]; }
-
-    __device__ ~TestSeqContainer() {
-        if (m_data) delete []m_data;
-    }
-    __device__ virtual void push(const T &&e) {
-        if (m_data) {
-           int idx = atomicAdd(&this->index_top, 1);
-           m_data[idx] = e;
-        }
-    }
-private:
-    T *m_data;
-    int index_top;
-
-};
-__global__ void func(){
   // CHECK: /*
-// CHECK: DPCT1109:{{[0-9]+}}: Memory storage allocation cannot be called in SYCL device code. You need to adjust the code.
-// CHECK: */
-    auto seq = new TestSeqContainer<int>(10);
-    seq->push(10);
-    delete seq;
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  __device__ TestSeqContainer(int size) : index_top(-1) { m_data = new T[size]; }
+
+  __device__ ~TestSeqContainer() {
+    if (m_data)
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+      delete[] m_data;
+  }
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}:  Virtual functions cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  __device__ virtual void push(const T &&e) {
+    if (m_data) {
+      int idx = atomicAdd(&this->index_top, 1);
+      m_data[idx] = e;
+    }
+  }
+
+private:
+  T *m_data;
+  int index_top;
+};
+__global__ void func() {
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  auto seq = new TestSeqContainer<int>(10);
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}:  Virtual functions cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  seq->push(10);
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  int *test = (int *)malloc(10 * sizeof(10));
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  free(test);
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  delete seq;
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1109:{{[0-9]+}}: The usage of dynamic memory allocation and deallocation APIs cannot be called in SYCL device code. You need to adjust the code.
+  // CHECK-NEXT: */
+  int4 *ptr = (int4 *)alloca(10 * sizeof(int4));
 }
 
 int main() {
-func<<<1,1>>>();
-cudaDeviceSynchronize();
-return 0;
-
+  func<<<1, 1>>>();
+  cudaDeviceSynchronize();
+  return 0;
 }
