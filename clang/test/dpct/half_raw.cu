2c1ee358ec57ffe948d5f9c5e19655ba180f2ce4
Ni, Wenhui <wenhui.ni@intel.com>
2023-08-01 09:44:39 +0800

    add more lit
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- clang/test/dpct/half_raw.cu
+++ clang/test/dpct/half_raw.cu
@@ -3,8 +3,8 @@
 // RUN: dpct --format-range=none -out-root %T/half_raw %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/half_raw/half_raw.dp.cpp --match-full-lines %s
 
-#include<cuda_runtime.h>
-#include<cuda_fp16.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
 int main(){
   // CHECK: uint16_t one_h{0x3C00};
   __half_raw one_h{0x3C00};
@@ -12,10 +12,24 @@ int main(){
   __half_raw zero_h{0};
   // CHECK: uint16_t *ptr = new uint16_t{0};
   __half_raw *ptr = new __half_raw{0};
+// clang-format off
   // CHECK: *ptr = 0x3C00;
   ptr->x = 0x3C00;
+  // CHECK: *ptr = 0x3C00;
+  ptr ->x = 0x3C00;
+  // CHECK: *ptr = 0x3C00;
+  ptr-> x = 0x3C00;
+  // CHECK: *ptr = 0x3C00;
+  ptr -> x = 0x3C00;
   // CHECK: zero_h = 0x3C00;
   zero_h.x = 0x3C00;
+  // CHECK: zero_h = 0x3C00;
+  zero_h .x = 0x3C00;
+  // CHECK: zero_h = 0x3C00;
+  zero_h. x = 0x3C00;
+  // CHECK: zero_h = 0x3C00;
+  zero_h . x = 0x3C00;
+// clang-format on
   // CHECK: sycl::half alpha = sycl::bit_cast<sycl::half>(one_h);
   half alpha = one_h;
   // CHECK: alpha = sycl::bit_cast<sycl::half>(one_h);
