a4acb06ee85d34f05c9dae911d85f7d40dbc4c22
Ahmed, Daiyaan <daiyaan.ahmed@intel.com>
2023-08-01 13:11:00 +0800

    Apply clang-format and rebase
    Signed-off-by: Ahmed, Daiyaan <daiyaan.ahmed@intel.com>

--- clang/test/dpct/clock.cu
+++ clang/test/dpct/clock.cu
@@ -4,51 +4,49 @@
 // CHECK: #include <stdio.h>
 // CHECK-NEXT: #include <stdint.h>
 // CHECK-NEXT: #include <time.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <time.h>
 
-__global__ static void timedReduction(const float *input, float *output, clock_t *timer)
-{
-    // CHECK: /*
-    // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
-    // CHECK-NEXT: */
-    *timer = clock();
+__global__ static void timedReduction(const float *input, float *output, clock_t *timer) {
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
+  // CHECK-NEXT: */
+  *timer = clock();
 
-    // CHECK: /*
-    // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
-    // CHECK-NEXT: */
-    clock();
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
+  // CHECK-NEXT: */
+  clock();
 
-    // CHECK: /*
-    // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock64 function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
-    // CHECK-NEXT: */
-    *timer = clock64();
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock64 function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
+  // CHECK-NEXT: */
+  *timer = clock64();
 
-    // CHECK: /*
-    // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock64 function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
-    // CHECK-NEXT: */
-    clock64();
+  // CHECK: /*
+  // CHECK-NEXT: DPCT1008:{{[0-9]+}}: clock64 function is not defined in SYCL. This is a hardware-specific feature. Consult with your hardware vendor to find a replacement.
+  // CHECK-NEXT: */
+  clock64();
 }
 
 // CHECK: void timedAddition(const float *input, float *output, clock_t *timer){
 // CHECK-NEXT:    *timer = clock();
 // CHECK-NEXT:    clock();
 // CHECK-NEXT:}
-void timedAddition(const float *input, float *output, clock_t *timer){
-    *timer = clock();
-    clock();
+void timedAddition(const float *input, float *output, clock_t *timer) {
+  *timer = clock();
+  clock();
 }
 
-int main(int argc, char **argv)
-{
-    float *dinput = NULL;
-    float *hinput = NULL;
-    float *doutput = NULL;
-    float *houtput = NULL;
-    clock_t *dtimer = NULL;
-    clock_t *htimer = NULL;
-    timedReduction<<<64, 256, sizeof(float) * 2 * 256>>>(dinput, doutput, dtimer);
-    timedAddition(hinput, houtput, htimer);
-    return 0;
+int main(int argc, char **argv) {
+  float *dinput = NULL;
+  float *hinput = NULL;
+  float *doutput = NULL;
+  float *houtput = NULL;
+  clock_t *dtimer = NULL;
+  clock_t *htimer = NULL;
+  timedReduction<<<64, 256, sizeof(float) * 2 * 256>>>(dinput, doutput, dtimer);
+  timedAddition(hinput, houtput, htimer);
+  return 0;
 }
