e9de987122912332a3bc9a9d18a84fc1d8954ad1
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-08-02 13:41:51 +0800

    Fix test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/cublas_64.cu
+++ clang/test/dpct/cublas_64.cu
@@ -1,6 +1,6 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.1, cuda-11.2, cuda-11.3, cuda-11.4, cuda-11.5, cuda-11.6, cuda-11.7, cuda-11.8
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1, v11.2, v11.3, v11.4, v11.5, v11.6, v11.7, v11.8
-// RUN: dpct --format-range=none --out-root %T/cublas_64 %s --cuda-include-path="%cuda-path/include"
+// RUN: dpct --format-range=none --usm-level=none --out-root %T/cublas_64 %s --cuda-include-path="%cuda-path/include"
 // RUN: FileCheck --input-file %T/cublas_64/cublas_64.dp.cpp --match-full-lines %s
 
 #include "cublas_v2.h"
