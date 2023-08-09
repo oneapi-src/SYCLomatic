2a3c86ef98805837d7f2a02884a8142564d60521
Wang, Hao3 <hao3.wang@intel.com>
2023-08-09 14:54:50 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- clang/test/dpct/memcpy_optimization.cu
+++ clang/test/dpct/memcpy_optimization.cu
@@ -6,7 +6,7 @@ int main(){
 
     float *a, *b;
 //CHECK:  /*
-//CHECK:  DPCT1114:{{[0-9]+}}: There is no synchronization operation append after memcpy. This is based on the assumption that the source host memory is pageable. If this assumption doesn't hold, please append .wait() after memcpy for correct synchronization behavior.
+//CHECK:  DPCT1114:{{[0-9]+}}: cudaMemcpy is migrated to asynchronization memcpy, assuming in original code the source host memory is pageable memory. If this assumption doesn't hold, please append '.wait()' after memcpy to synchronize.
 //CHECK:  */
 //CHECK:  q_ct1.memcpy(a, b, 10);
 //CHECK:  q_ct1.memcpy(a, b, 10);
