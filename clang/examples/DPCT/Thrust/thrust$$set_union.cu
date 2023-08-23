f63690c5233fbdb2bfa92dc846b9c7aefd94b3ec
chenwei.sun <chenwei.sun@intel.com>
2023-08-23 16:48:46 +0800

    Add thrust::set_union
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

---
+++ clang/examples/DPCT/Thrust/thrust$$set_union.cu
@@ -0,0 +1,88 @@
+#include <thrust/device_malloc.h>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/functional.h>
+#include <thrust/host_vector.h>
+#include <thrust/logical.h>
+#include <thrust/partition.h>
+#include <thrust/set_operations.h>
+#include <thrust/sort.h>
+
+void test() {
+
+  int A1[7] = {0, 2, 4, 6, 8, 10, 12};
+  int A2[5] = {1, 3, 5, 7, 9};
+  int result[12];
+  int *result_end;
+  thrust::device_vector<int> d_A1(A1, A1 + 7);
+  thrust::device_vector<int> d_A2(A2, A2 + 5);
+  thrust::device_vector<int> d_result(11);
+  thrust::device_vector<int>::iterator d_result_iter_end;
+  thrust::host_vector<int> h_A1(A1, A1 + 7);
+  thrust::host_vector<int> h_A2(A2, A2 + 5);
+  thrust::host_vector<int> h_result(12);
+  thrust::host_vector<int>::iterator h_result_iter_end;
+
+  // Start
+  thrust::set_union(thrust::host /*const thrust::detail::execution_policy_base<
+                                    DerivedPolicy > &*/,
+                    A1 /*InputIterator1*/, A1 + 7 /*InputIterator1*/,
+                    A2 /*InputIterator2 */, A2 + 5 /*InputIterator2 */,
+                    result /*OutputIterator */);
+  thrust::set_union(A1 /*InputIterator1*/, A1 + 7 /*InputIterator1*/,
+                    A2 /*InputIterator2 */, A2 + 5 /*InputIterator2 */,
+                    result /*OutputIterator */);
+  thrust::set_union(thrust::host /*const thrust::detail::execution_policy_base<
+                                    DerivedPolicy > &*/,
+                    A1 /*InputIterator1*/, A1 + 7 /*InputIterator1*/,
+                    A2 /*InputIterator2 */, A2 + 5 /*InputIterator2 */,
+                    result /*OutputIterator */, thrust::greater<int>());
+  thrust::set_union(A1 /*InputIterator1*/, A1 + 7 /*InputIterator1*/,
+                    A2 /*InputIterator2 */, A2 + 5 /*InputIterator2 */,
+                    result /*OutputIterator */,
+                    thrust::greater<int>() /*StrictWeakCompare */);
+  thrust::set_union(
+      thrust::device /*const thrust::detail::execution_policy_base<
+                        DerivedPolicy > &*/,
+      d_A1.begin() /*InputIterator1*/, d_A1.end() /*InputIterator1*/,
+      d_A2.begin() /*InputIterator2 */, d_A2.end() /*InputIterator2 */,
+      d_result.begin() /*OutputIterator */);
+  thrust::set_union(
+      d_A1.begin() /*InputIterator1*/, d_A1.end() /*InputIterator1*/,
+      d_A2.begin() /*InputIterator2 */, d_A2.end() /*InputIterator2 */,
+      d_result.begin() /*OutputIterator */);
+  thrust::set_union(
+      thrust::device /*const thrust::detail::execution_policy_base<
+                        DerivedPolicy > &*/,
+      d_A1.begin() /*InputIterator1*/, d_A1.end() /*InputIterator1*/,
+      d_A2.begin() /*InputIterator2 */, d_A2.end() /*InputIterator2 */,
+      d_result.begin() /*OutputIterator */, thrust::greater<int>());
+  thrust::set_union(
+      d_A1.begin() /*InputIterator1*/, d_A1.end() /*InputIterator1*/,
+      d_A2.begin() /*InputIterator2 */, d_A2.end() /*InputIterator2 */,
+      d_result.begin() /*OutputIterator */,
+      thrust::greater<int>() /*StrictWeakCompare */);
+  thrust::set_union(
+      thrust::host /*const thrust::detail::execution_policy_base<
+                      DerivedPolicy > &*/,
+      h_A1.begin() /*InputIterator1*/, h_A1.end() /*InputIterator1*/,
+      h_A2.begin() /*InputIterator2 */, h_A2.end() /*InputIterator2 */,
+      h_result.begin() /*OutputIterator */);
+  thrust::set_union(
+      h_A1.begin() /*InputIterator1*/, h_A1.end() /*InputIterator1*/,
+      h_A2.begin() /*InputIterator2 */, h_A2.end() /*InputIterator2 */,
+      h_result.begin() /*OutputIterator */);
+  thrust::set_union(
+      thrust::host /*const thrust::detail::execution_policy_base<
+                      DerivedPolicy > &*/,
+      h_A1.begin() /*InputIterator1*/, h_A1.end() /*InputIterator1*/,
+      h_A2.begin() /*InputIterator2 */, h_A2.end() /*InputIterator2 */,
+      h_result.begin() /*OutputIterator */,
+      thrust::greater<int>() /*StrictWeakCompare */);
+  thrust::set_union(
+      h_A1.begin() /*InputIterator1*/, h_A1.end() /*InputIterator1*/,
+      h_A2.begin() /*InputIterator2 */, h_A2.end() /*InputIterator2 */,
+      h_result.begin() /*OutputIterator */,
+      thrust::greater<int>() /*StrictWeakCompare */);
+  // End
+}
--- clang/test/dpct/query_api_mapping/Thrust/thrust_api_test.cu
+++ clang/test/dpct/query_api_mapping/Thrust/thrust_api_test.cu
@@ -13,3 +13,18 @@
 // set_union_by_key-NEXT:  dpct::set_union(oneapi::dpl::execution::seq, h_A_keys.begin(), h_A_keys.end(), h_B_keys.begin(), h_B_keys.end(), h_A_vals.begin(), h_B_vals.begin(), h_keys_result.begin(), h_vals_result.begin(), Compare());
 // set_union_by_key-NEXT:  dpct::set_union(oneapi::dpl::execution::seq, h_A_keys.begin(), h_A_keys.end(), h_B_keys.begin(), h_B_keys.end(), h_A_vals.begin(), h_B_vals.begin(), h_keys_result.begin(), h_vals_result.begin(), Compare());
 // set_union_by_key-EMPTY:
+
+// RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::set_union | FileCheck %s -check-prefix=set_union
+// set_union:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, A1, A1 + 7, A2, A2 + 5, result);
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, A1, A1 + 7, A2, A2 + 5, result);
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, A1, A1 + 7, A2, A2 + 5, result, std::greater<int>());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, A1, A1 + 7, A2, A2 + 5, result, std::greater<int>());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::make_device_policy(q_ct1), d_A1.begin(), d_A1.end(), d_A2.begin(), d_A2.end(), d_result.begin());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::make_device_policy(q_ct1), d_A1.begin(), d_A1.end(), d_A2.begin(), d_A2.end(), d_result.begin());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::make_device_policy(q_ct1), d_A1.begin(), d_A1.end(), d_A2.begin(), d_A2.end(), d_result.begin(), std::greater<int>());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::make_device_policy(q_ct1), d_A1.begin(), d_A1.end(), d_A2.begin(), d_A2.end(), d_result.begin(), std::greater<int>());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, h_A1.begin(), h_A1.end(), h_A2.begin(), h_A2.end(), h_result.begin());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, h_A1.begin(), h_A1.end(), h_A2.begin(), h_A2.end(), h_result.begin());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, h_A1.begin(), h_A1.end(), h_A2.begin(), h_A2.end(), h_result.begin(), std::greater<int>());
+// set_union-NEXT:  oneapi::dpl::set_union(oneapi::dpl::execution::seq, h_A1.begin(), h_A1.end(), h_A2.begin(), h_A2.end(), h_result.begin(), std::greater<int>());
+// set_union-EMPTY:
