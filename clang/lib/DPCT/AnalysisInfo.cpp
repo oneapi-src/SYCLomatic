a89f771891dd4973deae60c649673e6b1f0557c1
tomflinda <chenwei.sun@intel.com>
2023-08-25 09:30:26 +0800

    [SYCLomatic] Emit a warning msg if sycl::ext::oneapi::group_local_memory function is not used at the kernel functor scope of a work-group data parallel kernel (#1188)
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/lib/DPCT/AnalysisInfo.cpp
+++ clang/lib/DPCT/AnalysisInfo.cpp
@@ -3607,6 +3607,12 @@ std::string MemVarInfo::getDeclarationReplacement(const VarDecl *VD) {
   switch (Scope) {
   case clang::dpct::MemVarInfo::Local:
     if (DpctGlobalInfo::useGroupLocalMemory() && VD) {
+
+      auto FD = dyn_cast<FunctionDecl>(VD->getDeclContext());
+      if (FD && FD->hasAttr<CUDADeviceAttr>())
+        DiagnosticsUtils::report(getFilePath(), getOffset(),
+                                 Diagnostics::GROUP_LOCAL_MEMORY, true, false);
+
       std::string Ret;
       llvm::raw_string_ostream OS(Ret);
       OS << "auto &" << getName() << " = "
