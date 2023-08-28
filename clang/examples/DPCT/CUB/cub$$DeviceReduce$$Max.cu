a4cb621c497df29fdcdfc969b4a0051e75b05db3
Wang, Yihan <yihan.wang@intel.com>
2023-08-28 17:47:02 +0800

    [SYCLomatic] Add mapping query for 20 CUB APIs
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

---
+++ clang/examples/DPCT/CUB/cub$$DeviceReduce$$Max.cu
@@ -0,0 +1,10 @@
+// clang-format off
+#include <cstddef>
+#include <cub/cub.cuh>
+
+void test(void *temp_storage, size_t &temp_storage_bytes, int *d_in, int *d_out,
+          int num_items, cudaStream_t stream) {
+  // Start
+  cub::DeviceReduce::Max(temp_storage/*void **/, temp_storage_bytes/*size_t &*/, d_in/*InputIteratorT*/, d_out/*OutputIteratorT*/, num_items/*int*/, stream/*cudaStream_t*/);
+  // End
+}
---
+++ clang/examples/DPCT/CUB/cub$$DeviceSelect$$Flagged.cu
@@ -0,0 +1,10 @@
+// clang-format off
+#include <cstddef>
+#include <cub/cub.cuh>
+
+void test(void *temp_storage, size_t &temp_storage_bytes, int *d_in, int *d_flags, int *d_out,int *d_num_selected_out,
+          int num_items, cudaStream_t stream) {
+  // Start
+  cub::DeviceSelect::Flagged(temp_storage/*void **/, temp_storage_bytes/*size_t &*/, d_in/*InputIteratorT*/, d_flags/*FlagIterator*/, d_out/*OutputIteratorT*/, d_num_selected_out/*NumSelectedIteratorT*/, num_items/*int*/, stream/*cudaStream_t*/);
+  // End
+}
