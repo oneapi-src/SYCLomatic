98aad91c1ea4b450e388f6f4e36e5db9f3d22abb
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-08-28 13:52:08 +0800

    Remove year number to avoid updating it every year
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/tools/dpct/bash-autocomplete.sh
+++ clang/tools/dpct/bash-autocomplete.sh
@@ -1,6 +1,6 @@
 # ===----- bash-autocomplete.sh -------------------------------------------=== #
 #
-# Copyright (C) 2023 Intel Corporation
+# Copyright (C) Intel Corporation
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
