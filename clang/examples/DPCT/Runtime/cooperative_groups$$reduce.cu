3b9c45a5d90e83f714ed4491b33dbf2219ae19f9
Chen, Sheng S <sheng.s.chen@intel.com>
2023-08-28 23:00:53 +0800

    [SYCLomatic] Add 4 cooperative_groups API for query the mapping.
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/examples/DPCT/Runtime/cooperative_groups$$reduce.cu
+++ clang/examples/DPCT/Runtime/cooperative_groups$$reduce.cu
@@ -1,13 +1,15 @@
+// Option: --use-experimental-features=free-function-queries
 #include "cooperative_groups.h"
 #include <cooperative_groups/reduce.h>
 
 
-__global__ void test(const cooperative_groups::thread_block &cta) {
+__global__ void test() {
     // Start
     double *sdata;
+    cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
     const unsigned int tid = cta.thread_rank();
     cooperative_groups::thread_block_tile<32> tile32 = cooperative_groups::tiled_partition<32>(cta);
-    cooperative_groups::reduce(tile32, sdata[tid], cooperative_groups::plus<double>());
+    cooperative_groups::reduce(tile32 /* type group */, sdata[tid] /* type argument */, cooperative_groups::plus<double>() /* type operator */ );
     // End
 }
 
--- clang/examples/DPCT/Runtime/cooperative_groups$$thread_rank.cu
+++ clang/examples/DPCT/Runtime/cooperative_groups$$thread_rank.cu
@@ -1,9 +1,11 @@
+// Option: --use-experimental-features=free-function-queries
 #include "cooperative_groups.h"
 #include <cooperative_groups/reduce.h>
 
 
-__global__ void test(const cooperative_groups::thread_block &cta) {
+__global__ void test() {
     // Start
+    cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
     cta.thread_rank();
     // End
 }
