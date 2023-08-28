880a9d3b4aaa527c10a810ea85b588e775ad8cb9
Wang, Yihan <yihan.wang@intel.com>
2023-08-29 00:15:44 +0800

    fix lit
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/query_api_mapping/CUB/cub_device.cu
+++ clang/test/dpct/query_api_mapping/CUB/cub_device.cu
@@ -1,4 +1,4 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-11.4
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4
 // clang-format off
 // RUN: dpct --cuda-include-path="%cuda-path/include" --stop-on-parse-err --query-api-mapping=cub::DeviceReduce::ArgMax | FileCheck %s -check-prefix=CHECK_REDUCE_ARG_MAX
