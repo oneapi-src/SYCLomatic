202da6e73dc163054bbaa6ad5eaf6dc828c5a295
Chen, Sheng S <sheng.s.chen@intel.com>
2023-08-29 17:32:27 +0800

    Only support on Linux

--- clang/test/dpct/query_api_mapping/Runtime/test_cooperative_groups.cu
+++ clang/test/dpct/query_api_mapping/Runtime/test_cooperative_groups.cu
@@ -1,3 +1,4 @@
+// UNSUPPORTED: system-windows
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0
 // RUN: dpct --cuda-include-path="%cuda-path/include" -stop-on-parse-err --query-api-mapping=cooperative_groups::tiled_partition | FileCheck %s -check-prefix=CG_TILED_PARTITION
