3f5850f2d4a212950769beb32ca488b75b583f3c
Chen, Sheng S <sheng.s.chen@intel.com>
2023-08-29 09:09:44 +0800

    Add 2 new
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

---
+++ clang/examples/DPCT/Runtime/cooperative_groups$$this_thread_block.cu
@@ -0,0 +1,12 @@
+// Option: --use-experimental-features=free-function-queries
+#include "cooperative_groups.h"
+#include <cooperative_groups/reduce.h>
+
+
+__global__ void test() {
+    // Start
+    cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
+    // End
+}
+
+
