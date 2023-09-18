a1463770308345593bb9e7116195eb0addf2e642
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-09-18 12:53:15 +0800

    Fix lit
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/cusparse-10.cu
+++ clang/test/dpct/cusparse-10.cu
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-12.0, cuda-12.1, cuda-12.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v12.0, v12.1, v12.2
-// RUN: dpct --format-range=none --out-root %T/cusparse-type10 %s --cuda-include-path="%cuda-path/include"
-// RUN: FileCheck --input-file %T/cusparse-type10/cusparse-type10.dp.cpp --match-full-lines %s
+// RUN: dpct --format-range=none --out-root %T/cusparse-10 %s --cuda-include-path="%cuda-path/include"
+// RUN: FileCheck --input-file %T/cusparse-10/cusparse-10.dp.cpp --match-full-lines %s
 
 #include <cusparse.h>
 
