7e6782b63bf2d48f3062ca3d0fad5d9f85286e59
Tang, Jiajun <jiajun.tang@intel.com>
2023-09-28 15:44:29 +0800

    [SYCLomatic] Fix comment.
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- clang/lib/DPCT/Utility.h
+++ clang/lib/DPCT/Utility.h
@@ -201,9 +201,6 @@ inline bool isChildOrSamePath(const std::string &RootAbs,
   if (Child.empty()) {
     return false;
   }
-  if (RootAbs == Child) {
-    return true;
-  }
 
   auto Key = RootAbs + ":" + Child;
   auto Iter = ChildOrSameCache.find(Key);
