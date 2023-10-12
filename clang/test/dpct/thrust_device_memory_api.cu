f1ccdcfc1277fcd89b1ffda0d8b4de4a2dfb8d21
chenwei.sun <chenwei.sun@intel.com>
2023-10-12 17:30:45 +0800

    skip cuda-9.2 lit tests
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/thrust_device_memory_api.cu
+++ clang/test/dpct/thrust_device_memory_api.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0
-// UNSUPPORTED: v8.0
+// UNSUPPORTED: cuda-8.0, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.1, 
+// UNSUPPORTED: v8.0, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1
 // RUN: dpct -out-root %T/thrust_device_memory_api %s --cuda-include-path="%cuda-path/include" --usm-level=none
 // RUN: FileCheck --input-file %T/thrust_device_memory_api/thrust_device_memory_api.dp.cpp --match-full-lines %s
 
