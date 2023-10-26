e5c84b5ff3f51c1ff49a1e45a0ace4987916ebb9
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-10-26 13:04:35 +0800

    Add unsupported tags
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/compat_nvcc_2.cu
+++ clang/test/dpct/compat_nvcc_2.cu
@@ -1,3 +1,4 @@
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1
 // RUN: dpct --format-range=none -out-root %T/compat_nvcc_2 %s --cuda-include-path="%cuda-path/include" --stop-on-parse-err --extra-arg="-std=c++14"
 // RUN: FileCheck %s --match-full-lines --input-file %T/compat_nvcc_2/compat_nvcc_2.dp.cpp
 
