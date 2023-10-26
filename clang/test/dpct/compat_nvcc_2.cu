73006cab084047f33b9e221bcbb3825d666aed8a
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-10-26 10:13:33 +0800

    [SYCLomatic] Add "#include <algorithm>" in __clang_cuda_runtime_wrapper.h
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

---
+++ clang/test/dpct/compat_nvcc_2.cu
@@ -0,0 +1,15 @@
+// RUN: dpct --format-range=none -out-root %T/compat_nvcc_2 %s --cuda-include-path="%cuda-path/include" --stop-on-parse-err --extra-arg="-std=c++14"
+// RUN: FileCheck %s --match-full-lines --input-file %T/compat_nvcc_2/compat_nvcc_2.dp.cpp
+
+#include <cuda_runtime.h>
+
+const float FOUR_EIGHT_ZERO = 480.0f;
+
+// CHECK: void foo() {
+// CHECK-NEXT:   float max = -10000.f;
+// CHECK-NEXT:   float res = std::max(max / FOUR_EIGHT_ZERO, 1.0f / 32.f);
+// CHECK-NEXT: }
+__global__ void foo() {
+  float max = -10000.f;
+  float res = std::max(max / FOUR_EIGHT_ZERO, 1.0f / 32.f);
+}
