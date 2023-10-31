789f686db2aa4301f5f4c90b9ac78ab1004510f2
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-10-31 15:54:45 +0800

    Update test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/compat_with_clang.cu
+++ clang/test/dpct/compat_with_clang.cu
@@ -5,8 +5,8 @@
 #include "cuda_fp16.h"
 
 // CHECK: inline void foo1(sycl::half2 *array, sycl::half a) {
-// CHECK-NEXT:   array[10] = {a, a};
+// CHECK-NEXT:   array[dpct::reverse_bits<unsigned int>(123)] = {a, sycl::vec<float, 1>{2.3f}.convert<sycl::half, sycl::rounding_mode::automatic>()[0]};
 // CHECK-NEXT: }
 __device__ inline void foo1(__half2 *array, __half a) {
-  array[10] = {a, a};
+  array[__brev(123)] = {a, __float2half(2.3f)};
 }
