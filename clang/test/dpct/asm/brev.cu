2f7195904f81d626eef9d707825ceb363026f05a
Wang, Yihan <yihan.wang@intel.com>
2023-11-17 16:14:59 +0800

    [SYCLomatic] rebase
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/asm/brev.cu
+++ clang/test/dpct/asm/brev.cu
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
-// RUN: dpct --format-range=none -out-root %T/add %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
-// RUN: FileCheck %s --match-full-lines --input-file %T/add/add.dp.cpp
+// RUN: dpct --format-range=none -out-root %T/brev %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck %s --match-full-lines --input-file %T/brev/brev.dp.cpp
 
 // clang-format off
 #include <cuda_runtime.h>
