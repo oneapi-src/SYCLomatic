805d30b8acccd59db6f727ddd3645b9e960dd0db
Zhiwei Jiang <zhiwei.jiang@intel.com>
2023-11-17 14:41:58 +0800

    Print migrated code
    Signed-off-by: Zhiwei Jiang <zhiwei.jiang@intel.com>

--- clang/test/dpct/file_type_test_win/main.cu
+++ clang/test/dpct/file_type_test_win/main.cu
@@ -22,14 +22,35 @@
 // RUN: sed -i  '13,15s/\\/\//g'  %T/compile_commands.json 
 
 // RUN: dpct --format-range=none --cuda-include-path="%cuda-path/include" -in-root=%S -out-root=%T -p=%T %s %S/bar/util.gpu %S/bar/util_bar.cc --sycl-named-lambda -extra-arg="-I%S/bar" -extra-arg="--std=c++14"
+// RUN: echo "=====main.dp.cpp begin====="
+// RUN: cat %T/main.dp.cpp
+// RUN: echo "=====main.dp.cpp end====="
 // RUN: FileCheck %s --match-full-lines --input-file %T/main.dp.cpp
+// RUN: echo "=====util.gpu.dp.cpp begin====="
+// RUN: cat %T/bar/util.gpu.dp.cpp
+// RUN: echo "=====util.gpu.dp.cpp end====="
 // RUN: FileCheck %S/bar/util.gpu --match-full-lines --input-file %T/bar/util.gpu.dp.cpp
+// RUN: echo "=====util.gpuhead begin====="
+// RUN: cat %T/bar/util.gpuhead
+// RUN: echo "=====util.gpuhead end====="
 // RUN: FileCheck %S/bar/util.gpuhead --match-full-lines --input-file %T/bar/util.gpuhead
+// RUN: echo "=====util_bar.hh begin====="
+// RUN: cat%T/bar/util_bar.hh
+// RUN: echo "=====util_bar.hh end====="
 // RUN: FileCheck %S/bar/util_bar.hh --match-full-lines --input-file %T/bar/util_bar.hh
+// RUN: echo "=====macro_def.hh begin====="
+// RUN: cat %T/bar/macro_def.hh
+// RUN: echo "=====macro_def.hh end====="
 // RUN: FileCheck %S/bar/macro_def.hh --match-full-lines --input-file %T/bar/macro_def.hh
+// RUN: echo "=====util_bar.cc.dp.cpp begin====="
+// RUN: cat %T/bar/util_bar.cc.dp.cpp
+// RUN: echo "=====util_bar.cc.dp.cpp end====="
 // RUN: FileCheck %S/bar/util_bar.cc --match-full-lines --input-file %T/bar/util_bar.cc.dp.cpp
 
 // RUN: dpct --format-range=none --cuda-include-path="%cuda-path/include" -in-root=%S -out-root=%T -p=%T  %S/main.gpu   --sycl-named-lambda 
+// RUN: echo "=====main.gpu.dp.cpp begin====="
+// RUN: cat %T/main.gpu.dp.cpp
+// RUN: echo "=====main.gpu.dp.cpp end====="
 // RUN: FileCheck %S/main.gpu --match-full-lines --input-file %T/main.gpu.dp.cpp
 
 #include <stdio.h>
