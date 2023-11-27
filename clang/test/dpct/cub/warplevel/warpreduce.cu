74d2a41b91a6f733c3ad0cb5b43f467a18713563
abhilash1910 <abhilash.majumder@intel.com>
2023-11-27 02:05:22 -0800

    fix format

--- clang/test/dpct/cub/warplevel/warpreduce.cu
+++ clang/test/dpct/cub/warplevel/warpreduce.cu
@@ -69,6 +69,8 @@ __global__ void ReduceKernel(int* data) {
 }
 
 //CHECK: void ReduceKernel_Max(int* data, const sycl::nd_item<3> &item_ct1) {
+//CHECK-EMPTY:
+//CHECK-EMPTY:
 //CHECK-NEXT:  int threadid = item_ct1.get_local_id(2);
 //CHECK-NEXT:  int input = data[threadid];
 //CHECK-NEXT:  int output = 0;
@@ -86,6 +88,8 @@ __global__ void ReduceKernel_Max(int* data) {
 }
 
 //CHECK: void ReduceKernel_Min(int* data, const sycl::nd_item<3> &item_ct1) {
+//CHECK-EMPTY:
+//CHECK-EMPTY:
 //CHECK-NEXT:  int threadid = item_ct1.get_local_id(2);
 //CHECK-NEXT:  int input = data[threadid];
 //CHECK-NEXT:  int output = 0;
