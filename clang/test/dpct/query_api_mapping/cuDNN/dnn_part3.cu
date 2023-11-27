21e01064df9452b34a0230a81689a59680ee8be7
Wang, Hao3 <hao3.wang@intel.com>
2023-11-27 21:16:13 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- clang/test/dpct/query_api_mapping/cuDNN/dnn_part3.cu
+++ clang/test/dpct/query_api_mapping/cuDNN/dnn_part3.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.2
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.2
 
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=cudnnGetRNNTempSpaceSizes | FileCheck %s -check-prefix=cudnnGetRNNTempSpaceSizes
 // cudnnGetRNNTempSpaceSizes: CUDA API:
