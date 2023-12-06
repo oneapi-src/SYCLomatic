df64a4fca9ee659c323f0d20d2a94a3fe678ddad
Wang, Hao3 <hao3.wang@intel.com>
2023-12-06 19:57:31 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- clang/test/dpct/query_api_mapping/Runtime/test_warp.cu
+++ clang/test/dpct/query_api_mapping/Runtime/test_warp.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0
-// UNSUPPORTED: v8.0
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0
 
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=__activemask | FileCheck %s -check-prefix=__activemask
 // __activemask: CUDA API:
