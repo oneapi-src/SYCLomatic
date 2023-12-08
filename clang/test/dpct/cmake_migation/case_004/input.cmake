277e6a614eddbbf98fa8d86a41568a9f2d3f76c7
Ahmed, Daiyaan <daiyaan.ahmed@intel.com>
2023-12-08 15:58:11 +0800

    Update LIT tests
    Signed-off-by: Ahmed, Daiyaan <daiyaan.ahmed@intel.com>

--- clang/test/dpct/cmake_migation/case_004/input.cmake
+++ clang/test/dpct/cmake_migation/case_004/input.cmake
@@ -1,19 +1,18 @@
-cmake_minimum_required(VERSION 3.10)
-target_link_libraries(main PRIVATE cxxopts fmt::fmt cublas)
+target_link_libraries(main PRIVATE cxxopts fmt::fmt -lcublas)
 
 target_link_libraries(tiny-cuda-nn PUBLIC ${CUDA_LIBRARIES} ${TCNN_LIBRARIES} fmt)
 
-target_link_libraries(transformer_engine PUBLIC
-                     CUDA::cublas
-                     CUDA::cuda_driver
-                     CUDA::cudart
-                     CUDA::nvrtc
-                     CUDA::nvToolsExt
-                     CUDNN::cudnn)
+target_link_libraries(transformer_engine PUBLIC 
+   CUDA::cublas
+   CUDA::cuda_driver
+   CUDA::cudart
+   CUDA::nvrtc
+   CUDA::nvToolsExt
+   CUDNN::cudnn)
                      
 target_link_libraries(${target} PRIVATE
-         -static-libstdc++
          -static-libgcc
+         -static-libstdc++
          )
                      
 target_link_libraries(${PROJECT_NAME}
