7031f9f2ed4343031e7b98cf6c8a6c8eaaa7ad8f
Teja Alaghari <teja.alaghari@intel.com>
2023-12-11 11:43:47 +0800

    Lit test for find_package CMake macro

---
+++ clang/test/dpct/cmake_migration/case_008/input.cmake
@@ -0,0 +1,18 @@
+find_package(CUDA)
+find_package(CUDA REQUIRED)
+find_package(CUDA ${REQUIRED_CUDA_VERSION} REQUIRED)
+#Current YAML rule comments find_package(MPI) as it changes CMAKE_CXX_COMPILER
+find_package(MPI)
+find_package(MPI REQUIRED)
+#Current YAML rule comments find_package(OpenMP) as it changes CMAKE_CXX_COMPILER
+FIND_PACKAGE(OpenMP)
+FIND_PACKAGE(OpenMP REQUIRED)
+find_package(CUDAToolkit)
+find_package(CUDAToolkit 11.4 REQUIRED)
+find_package(CUDAToolkit REQUIRED cublas cufft nvToolsExt)
+find_package(CUDAToolkit REQUIRED cusparse curand solver)
+find_package(CUB)
+find_package(CUB REQUIRED)
+#Current YAML rule comments find_package(NVJPEG) as no SYCL equivalent lib exists
+find_package(NVJPEG)
+find_package(NVJPEG 9.0 REQUIRED)
