913dac81315a56b90de9a8f1f85b9f639b7674ef
chenwei.sun <chenwei.sun@intel.com>
2023-12-14 08:39:49 +0800

    Add cmake helper function file
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

---
+++ clang/tools/dpct/cmake/sycl_helper.cmake
@@ -0,0 +1,23 @@
+find_package(IntelSYCL REQUIRED)
+
+macro(FIND_DPCT_PATH dpct_path)
+    # Use find_program to locate the executable
+    find_program(_dpct_path
+        NAMES dpct
+        PATHS)
+
+    if(_dpct_path)
+        message("Found foo executable at: ${_dpct_path}")
+    else()
+        message(FATAL_ERROR "Could not find foo executable.")
+    endif()
+    
+    get_filename_component( dir_path ${_dpct_path} DIRECTORY)
+    message("dir_path: ${dir_path}")
+    set(cmake_yaml_file_path "${dir_path}/../extensions/opt_rules/cmake_rules/cmake_script_migration_rule.yaml")
+    message("dir_path: ${cmake_yaml_file_path}")
+     
+    set( ${dpct_path} ${_dpct_path})
+endmacro()
+
+
