088de300882310949cdc59bc2c125b8bd5edd172
chenwei.sun <chenwei.sun@intel.com>
2023-12-14 11:52:44 +0800

    Add content to sycl_helper.cmake
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/tools/dpct/cmake/sycl_helper.cmake
+++ clang/tools/dpct/cmake/sycl_helper.cmake
@@ -1,23 +1,64 @@
 find_package(IntelSYCL REQUIRED)
 
-macro(FIND_DPCT_PATH dpct_path)
-    # Use find_program to locate the executable
-    find_program(_dpct_path
-        NAMES dpct
-        PATHS)
-
-    if(_dpct_path)
-        message("Found foo executable at: ${_dpct_path}")
-    else()
-        message(FATAL_ERROR "Could not find dpct executable.")
-    endif()
-    
-    get_filename_component(dir_path ${_dpct_path} DIRECTORY)
-    message("dir_path: ${dir_path}")
-    set(cmake_yaml_file_path "${dir_path}/../extensions/opt_rules/cmake_rules/cmake_script_migration_rule.yaml")
-    message("dir_path: ${cmake_yaml_file_path}")
-     
-    set( ${dpct_path} ${_dpct_path})
+macro(GET_SOURCES _sources)
+  set( ${_sources} )
+  foreach(arg ${ARGN})
+    # Assume arg is a source file
+    list(APPEND ${_sources} ${arg})
+  endforeach()
 endmacro()
 
+macro(INNER_SRS_WRAPPER sycl_target generated_files)
+  set(_argn_list "${ARGN}")
+  set(_generated_files "")
+  set(generated_extension ${CMAKE_CXX_OUTPUT_EXTENSION})
 
+  set(_counter 1) # use to unique the generated obj file name
+  set(_sycl_target "${sycl_target}")
+  # Iterate each macro arguments and create custom command for each cpp file
+  foreach(file ${_argn_list})
+    if(${file} MATCHES "\\.cpp")
+        get_filename_component( basename ${file} NAME )
+    
+        set(generated_file_basename "${sycl_target}_${_counter}_generated_${basename}${generated_extension}")
+        set(generated_file "${CMAKE_CURRENT_BINARY_DIR}/${generated_file_basename}")
+
+        get_filename_component(file_path "${file}" PATH)
+        if(IS_ABSOLUTE "${file_path}")
+            set(source_file "${file}")
+        else()
+            set(source_file "${CMAKE_CURRENT_SOURCE_DIR}/${file}")
+        endif()
+
+        add_custom_command(
+          OUTPUT ${generated_file}
+          COMMAND icpx -fsycl  -o ${generated_file} -c ${source_file}
+          DEPENDS ${file}
+        )
+
+        list(APPEND _generated_files ${generated_file})
+    else()
+        message(FATAL_ERROR "Only support cpp file.")
+    endif()
+
+    math(EXPR _counter "${_counter} + 1")
+  endforeach()
+
+  set(${generated_files} ${_generated_files})
+endmacro()
+
+macro(SYCL_COMPILE_BASE sycl_target generated_files)
+
+  set(_sycl_target "${sycl_target}")
+  GET_SOURCES(_sources ${ARGN})
+
+  
+  # Create custom command for each cpp source file
+  INNER_SRS_WRAPPER( ${_sycl_target} _generated_files ${_sources})
+
+  set( ${generated_files} ${_generated_files})
+endmacro()
+
+macro(SYCL_COMPILE_DEVICE generated_files)
+  SYCL_COMPILE_BASE(sycl_device ${generated_files} ${ARGN})
+endmacro()
