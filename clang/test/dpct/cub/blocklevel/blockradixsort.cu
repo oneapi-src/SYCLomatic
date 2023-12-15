8a05dbae0babf01be9c48253bb206344291fcc9f
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2023-12-15 11:56:31 +0530

    Add test

--- clang/test/dpct/cub/blocklevel/blockradixsort.cu
+++ clang/test/dpct/cub/blocklevel/blockradixsort.cu
@@ -31,10 +31,9 @@ void print_data(int* data, int num) {
 }
 
 //CHECK: void BlockRadixSortKernel(int* d_data,  const sycl::nd_item<3> &item_ct1) {
-//CHECK:  dpct::group::radix_sort radixsort_obj;
 //CHECK:  int threadid = item_ct1.get_local_id(2);
 //CHECK:  d_data[threadid*ItemsPerThread] = (threadid*2)%32;
-//CHECK:  radixsort_obj.sort(item_ct1.get_group(), d_data);
+//CHECK:  dpct::group::radix_sort().sort(item_ct1.get_group(), d_data);
 //CHECK:}
 
 __global__ void BlockRadixSortKernel(int *d_data)
