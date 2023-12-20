394f609c94007d21df740aa591314bb76282263c
tomflinda <chenwei.sun@intel.com>
2023-12-20 15:41:33 +0800

    [SYCLomatic]Add migration rules and enable lit tests for 4 cmake syntax migration (#1541)
    This PR coveres 4 cmake syntaxes as below:
    1. user defined CUDA specific variables migration
    2. target_compile_options
    3. add_compile_options
    4. CUDA_HAS_FP16
    
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/tools/dpct/cmake/dpct.cmake
+++ clang/tools/dpct/cmake/dpct.cmake
@@ -74,3 +74,7 @@ endmacro()
 macro(DPCT_COMPILE_SYCL_CODE generated_files)
   DPCT_COMPILE_SYCL_CODE_IMP(sycl_device ${generated_files} ${ARGN})
 endmacro()
+
+# Always set SYCL_HAS_FP16 to true to assume SYCL device to support float16
+message("dpct.cmake: SYCL_HAS_FP16 is set true by default.")
+set(SYCL_HAS_FP16 TRUE)
