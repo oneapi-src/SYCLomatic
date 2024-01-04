fb964748275e40819004cb014ecbf7a7bc5932eb
Yihan Wang <yihan.wang@intel.com>
2024-01-04 11:39:44 +0800

    [SYCLomatic] Fix the migration of mov instruction when one of the input/output is a vector expression (#1569)
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/asm/mov.cu
+++ clang/test/dpct/asm/mov.cu
@@ -30,4 +30,83 @@ __global__ void mov() {
   return;
 }
 
+inline __device__ float half_to_float(uint16_t h) {
+  float f = 0;
+  return f;
+}
+
+inline __device__ float2 half2_to_float2(uint32_t v) {
+  uint16_t lo, hi;
+  // CHECK: lo = sycl::vec<uint32_t, 1>(v).template as<sycl::vec<uint16_t, 2>>()[0];
+  // CHECK: hi = sycl::vec<uint32_t, 1>(v).template as<sycl::vec<uint16_t, 2>>()[1];
+  asm volatile("mov.b32 {%0, %1}, %2;\n" : "=h"(lo), "=h"(hi) : "r"(v));
+  return make_float2(half_to_float(lo), half_to_float(hi));
+}
+
+__global__ void test() {
+  {
+    uint32_t v;
+    uint16_t lo, hi;
+    // CHECK: lo = sycl::vec<uint32_t, 1>(v).template as<sycl::vec<uint16_t, 2>>()[0];
+    // CHECK: hi = sycl::vec<uint32_t, 1>(v).template as<sycl::vec<uint16_t, 2>>()[1];
+    asm volatile("mov.b32 {%0, %1}, %2;\n" : "=h"(lo), "=h"(hi) : "h"(v));
+  }
+  {
+    uint64_t v;
+    uint32_t lo, hi;
+    // CHECK: lo = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint32_t, 2>>()[0];
+    // CHECK: hi = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint32_t, 2>>()[1];
+    asm volatile("mov.b64 {%0, %1}, %2;\n" : "=r"(lo), "=r"(hi) : "l"(v));
+  }
+  {
+    uint64_t v;
+    uint16_t a, b, c, d;
+    // CHECK: a = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[0];
+    // CHECK: b = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[1];
+    // CHECK: c = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[2];
+    // CHECK: d = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[3];
+    asm volatile("mov.b64 {%0, %1, %2, %3}, %4;\n" : "=h"(a), "=h"(b), "=h"(c), "=h"(d) : "l"(v));
+  }
+  {
+    uint32_t v;
+    uint16_t lo, hi;
+    // CHECK: v = sycl::vec<uint16_t, 2>({lo, hi}).template as<sycl::vec<uint32_t, 1>>()[0];
+    asm volatile("mov.b32 %0, {%1, %2};\n" : "=r"(v) : "h"(lo), "h"(hi));
+  }
+  {
+    uint64_t v;
+    uint32_t lo, hi;
+    // CHECK: v = sycl::vec<uint32_t, 2>({lo, hi}).template as<sycl::vec<uint64_t, 1>>()[0];
+    asm volatile("mov.b64 %0, {%1, %2};\n" : "=l"(v) : "r"(lo), "r"(hi));
+  }
+  {
+    uint64_t v;
+    uint16_t a, b, c, d;
+    // CHECK: v = sycl::vec<uint16_t, 4>({a, b, c, d}).template as<sycl::vec<uint64_t, 1>>()[0];
+    asm volatile("mov.b64 %0, {%1, %2, %3, %4};\n" : "=l"(v) : "h"(a), "h"(b), "h"(c), "h"(d));
+  }
+  
+  {
+    uint32_t v;
+    uint16_t lo;
+    // CHECK: lo = sycl::vec<uint32_t, 1>(v).template as<sycl::vec<uint16_t, 2>>()[0];
+    asm volatile("mov.b32 {%0, _}, %1;\n" : "=h"(lo) : "h"(v));
+  }
+  {
+    uint64_t v;
+    uint32_t hi;
+    // CHECK: hi = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint32_t, 2>>()[1];
+    asm volatile("mov.b64 {_, %0}, %1;\n" : "=r"(hi) : "l"(v));
+  }
+  {
+    uint64_t v;
+    uint16_t a, b, d;
+    // CHECK: a = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[0];
+    // CHECK: b = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[1];
+    // CHECK: d = sycl::vec<uint64_t, 1>(v).template as<sycl::vec<uint16_t, 4>>()[3];
+    asm volatile("mov.b64 {%0, %1, _, %2}, %3;\n" : "=h"(a), "=h"(b), "=h"(d) : "l"(v));
+  }
+}
+
+
 // clang-format on
