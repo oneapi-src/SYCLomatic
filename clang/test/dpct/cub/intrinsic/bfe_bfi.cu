3bf2b978a296058cce9e2d91279c0ae1087056af
Wang, Yihan <yihan.wang@intel.com>
2024-01-05 10:41:35 +0800

    [SYCLomatic] Fix the migration of cub::BFI
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/intrinsic/bfe_bfi.cu
+++ clang/test/dpct/cub/intrinsic/bfe_bfi.cu
@@ -22,10 +22,46 @@ __global__ void kernel() {
   // CHECK-NEXT: dpct::bfe_safe(u32, bit_start, num_bits);
   // CHECK-NEXT: dpct::bfe_safe(i64, bit_start, num_bits);
   // CHECK-NEXT: dpct::bfe_safe(u64, bit_start, num_bits);
-  // CHECK-NEXT: res = dpct::bfi_safe<unsigned>(res, u32, u32, bit_start);
+  // CHECK-NEXT: res = dpct::bfi_safe<unsigned>(u32, u32, bit_start, num_bits);
   cub::BFE(i32, bit_start, num_bits);
   cub::BFE(u32, bit_start, num_bits);
   cub::BFE(i64, bit_start, num_bits);
   cub::BFE(u64, bit_start, num_bits);
   cub::BFI(res, u32, u32, bit_start, num_bits);
+}
+
+__global__ void bfe_kernel(int *res) {
+  if (cub::BFE((uint8_t)0xF0, 4, 8) != 15) {
+    *res = 1;
+    return;
+  }
+  if (cub::BFE((uint16_t)0x0FF0u, 4, 12) != 255) {
+    *res = 2;
+    return;
+  }
+  if (cub::BFE(0x00FFFF00u, 8, 16) != 65535u) {
+    *res = 3;
+    return;
+  }
+  if (cub::BFE(0x000000FFull, 0, 9) != 255) {
+    *res = 4;
+    return;
+  }
+  *res = 0;
+}
+
+__global__ void bfi_kernel(int *res) {
+  unsigned d = 0;
+  cub::BFI(d, 0x00FF0000u, 0x0000FFFFu, 0, 16);
+  if (d != 0x00FFFFFFu) {
+    *res = 1;
+    return;
+  }
+
+  cub::BFI(d, 0x00FF0000u, 0x000000FFu, 0, 8);
+  if (d != 0x00FF00FFu) {
+    *res = 2;
+    return;
+  }
+  *res = 0;
 }
