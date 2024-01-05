5a96f801db3d4501030e4ca66080b14b45e96b16
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-01-05 11:49:58 +0530

    update loop and remove repeat code

--- clang/runtime/dpct-rt/include/dpct/dpl_extras/dpcpp_extensions.h
+++ clang/runtime/dpct-rt/include/dpct/dpl_extras/dpcpp_extensions.h
@@ -428,10 +428,11 @@ public:
 
   radix_sort(uint8_t *local_memory) : _local_memory(local_memory) {}
 
+  
   template <typename Item>
   __dpct_inline__ void
-  sort(const Item &item, T (&keys)[VALUES_PER_THREAD], int begin_bit = 0,
-       int end_bit = 8 * sizeof(T)) {
+  helper_sort(const Item &item, T (&keys)[VALUES_PER_THREAD], int begin_bit = 0,
+       int end_bit = 8 * sizeof(T), bool is_striped = false) {
 
     uint32_t(&unsigned_keys)[VALUES_PER_THREAD] =
         reinterpret_cast<uint32_t(&)[VALUES_PER_THREAD]>(keys);
@@ -441,22 +442,29 @@ public:
       unsigned_keys[i] = detail::traits<T>::twiddle_in(unsigned_keys[i]);
     }
 
-    while (true) {
+    for(int i = begin_bit ; i < end_bit ; i += RADIX_BITS){
       int pass_bits = sycl::min(RADIX_BITS, end_bit - begin_bit);
 
       int ranks[VALUES_PER_THREAD];
       detail::radix_rank<RADIX_BITS, DESCENDING>(_local_memory)
-          .template rank_keys(item, unsigned_keys, ranks, begin_bit, pass_bits);
-      begin_bit += RADIX_BITS;
-
+          .template rank_keys(item, unsigned_keys, ranks, i, pass_bits);
+      
       item.barrier(sycl::access::fence_space::local_space);
 
+      if(is_striped && i >= end_bit){
+    
+        exchange<T, VALUES_PER_THREAD>(_local_memory)
+            .scatter_to_striped(item, keys, ranks);
+
+        break;
+        
+      }
       exchange<T, VALUES_PER_THREAD>(_local_memory)
           .scatter_to_blocked(item, keys, ranks);
 
       item.barrier(sycl::access::fence_space::local_space);
 
-      if (begin_bit >= end_bit)
+      if (!is_striped && i >= end_bit)
         break;
     }
 
@@ -466,47 +474,16 @@ public:
     }
   }
 
-  template <typename Item>
-  __dpct_inline__ void
-  sort_blocked_to_striped(const Item &item, T (&keys)[VALUES_PER_THREAD],
-                       int begin_bit = 0, int end_bit = 8 * sizeof(T)) {
+  template<typename Item>
+  __dpct_inline__ void sort_blocked(const Item &item, T (&keys)[VALUES_PER_THREAD], int begin_bit = 0,
+       int end_bit = 8 * sizeof(T)){
+    helper_sort(item, keys, begin_bit, end_bit, false);
+  }
 
-    uint32_t(&unsigned_keys)[VALUES_PER_THREAD] =
-        reinterpret_cast<uint32_t(&)[VALUES_PER_THREAD]>(keys);
-
-#pragma unroll
-    for (int i = 0; i < VALUES_PER_THREAD; ++i) {
-      unsigned_keys[i] = detail::traits<T>::twiddle_in(unsigned_keys[i]);
-    }
-
-    while (true) {
-      int pass_bits = sycl::min(RADIX_BITS, end_bit - begin_bit);
-
-      int ranks[VALUES_PER_THREAD];
-      detail::radix_rank<RADIX_BITS, DESCENDING>(_local_memory)
-          .template rank_keys(item, unsigned_keys, ranks, begin_bit, pass_bits);
-      begin_bit += RADIX_BITS;
-
-      item.barrier(sycl::access::fence_space::local_space);
-
-      if (begin_bit >= end_bit) {
-
-        exchange<T, VALUES_PER_THREAD>(_local_memory)
-            .scatter_to_striped(item, keys, ranks);
-
-        break;
-      }
-
-      exchange<T, VALUES_PER_THREAD>(_local_memory)
-          .scatter_to_blocked(item, keys, ranks);
-
-      item.barrier(sycl::access::fence_space::local_space);
-    }
-
-#pragma unroll
-    for (int i = 0; i < VALUES_PER_THREAD; ++i) {
-      unsigned_keys[i] = detail::traits<T>::twiddle_out(unsigned_keys[i]);
-    }
+  template<typename Item>
+  __dpct_inline__ void sort_blocked_to_striped(const Item &item, T (&keys)[VALUES_PER_THREAD], int begin_bit = 0,
+       int end_bit = 8 * sizeof(T)){
+    helper_sort(item, keys, begin_bit, end_bit, true);
   }
 
 private:
