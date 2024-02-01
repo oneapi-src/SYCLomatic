3df8eb734b6b75095984f49e139a99dc0cec9ba1
Deepak Raj H R <deepak.raj.h.r@intel.com>
2024-02-01 13:44:24 +0800

    Use getNL

--- clang/lib/DPCT/SaveNewFiles.cpp
+++ clang/lib/DPCT/SaveNewFiles.cpp
@@ -210,7 +210,7 @@ static bool checkOverwriteAndWarn(StringRef OutFilePath, StringRef InFilePath) {
                  << SrcFilePath->second << "' have same name '" << OutFilePath
                  << "'. To avoid overwrite, the migration of '" << InFilePath
                  << "' is skipped. Please change the output file extension "
-                    "with option '--sycl-file-extension'.\n";
+                    "with option '--sycl-file-extension'." << getNL();
     Overwrites = true;
   }
   return Overwrites;
