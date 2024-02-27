dcf47c2c19e49940f48ab041f3148dde68e6474f
chenwei.sun <chenwei.sun@intel.com>
2024-02-27 09:29:45 +0800

    [SYCLomatic] Support migration of thrust::reverse_iterator
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/types006.cu
+++ clang/test/dpct/types006.cu
@@ -2,7 +2,14 @@
 // RUN: FileCheck %s --match-full-lines --input-file %T/types006/types006.dp.cpp
 // RUN: %if build_lit %{icpx -c -fsycl %T/types006/types006.dp.cpp -o %T/types006/types006.dp.o %}
 
+// CHECK: #include <oneapi/dpl/execution>
+// CHECK-NEXT:#include <oneapi/dpl/algorithm>
+// CHECK-NEXT:#include <sycl/sycl.hpp>
+// CHECK-NEXT:#include <dpct/dpct.hpp>
+// CHECK-NEXT:#include <dpct/rng_utils.hpp>
+// CHECK-NEXT:#include <dpct/dpl_utils.hpp>
 #include <curand_kernel.h>
+#include <cub/cub.cuh>
 
 void foo() {
 // CHECK: dpct::rng::device::rng_generator<oneapi::mkl::rng::device::philox4x32x10<1>> bar;
@@ -13,4 +20,11 @@ void foo() {
   auto lambda = [&bar] __device__ () {
     return curand_uniform(&bar);
   };
+}
+
+// CHECK:void foo(){
+// CHECK-NEXT:  oneapi::dpl::reverse_iterator<int *> d_tmp(0);
+// CHECK-NEXT:}
+void foo(){
+  thrust::reverse_iterator<int *> d_tmp(0);
 }
