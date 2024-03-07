d9842070eb2b61815a265004e75de63c2d27d643
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-03-07 11:08:33 +0800

    Update
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/launch-kernel-usm.cu
+++ clang/test/dpct/launch-kernel-usm.cu
@@ -79,7 +79,7 @@ int main() {
 
   void *kernel_func = (void *)&kernel;
   // CHECK: /*
-  // CHECK-NEXT: DPCT1123:{{[0-9]+}}: The kernel function pointer cannot be deduced correctly. You need cast "kernel_func" to a function pointer. According to the kernel function definition, adjusting the kernel function arguments and the dimension of the sycl::nd_item may be also required.
+  // CHECK-NEXT: DPCT1123:{{[0-9]+}}: The kernel function pointer cannot be used in the device code. You need call the kernel function with correct argunemt(s) directly. According to the kernel function definition, adjusting the dimension of the sycl::nd_item may be also required.
   // CHECK-NEXT: */
   // CHECK-NEXT: q_ct1.parallel_for(
   // CHECK-NEXT:   sycl::nd_range<3>(sycl::range<3>(1, 1, 16) * sycl::range<3>(1, 1, 16), sycl::range<3>(1, 1, 16)), 
@@ -91,7 +91,7 @@ int main() {
   void *kernel_array[100];
   kernel_array[10] = (void *)&kernel;
   // CHECK: /*
-  // CHECK-NEXT: DPCT1123:{{[0-9]+}}: The kernel function pointer cannot be deduced correctly. You need cast "(kernel_array[10])" to a function pointer. According to the kernel function definition, adjusting the kernel function arguments and the dimension of the sycl::nd_item may be also required.
+  // CHECK-NEXT: DPCT1123:{{[0-9]+}}: The kernel function pointer cannot be used in the device code. You need call the kernel function with correct argunemt(s) directly. According to the kernel function definition, adjusting the dimension of the sycl::nd_item may be also required.
   // CHECK-NEXT: */
   // CHECK-NEXT: q_ct1.parallel_for(
   // CHECK-NEXT:   sycl::nd_range<3>(sycl::range<3>(1, 1, 16) * sycl::range<3>(1, 1, 16), sycl::range<3>(1, 1, 16)), 
