7780b12cfdc1e3c13fc84dbd8a3adb01bf5db262
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-03-08 16:47:13 +0800

    [SYCLomatic] Emit warning if kernel function pointer cannot be deduced (#1740)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/launch-kernel-cooperative.cu
+++ clang/test/dpct/launch-kernel-cooperative.cu
@@ -82,9 +82,13 @@ int main() {
   void *kernel_func = (void *)&kernel;
 
   // CHECK: /*
-  // CHECK-NEXT: DPCT1007:{{[0-9]+}}: Migration of cudaLaunchCooperativeKernel is not supported.
+  // CHECK-NEXT: DPCT1123:{{[0-9]+}}: The kernel function pointer cannot be used in the device code. You need call the kernel function with correct argunemt(s) directly. According to the kernel function definition, adjusting the dimension of the sycl::nd_item may be also required.
   // CHECK-NEXT: */
-  // CHECK-NEXT: cudaLaunchCooperativeKernel(kernel_func, sycl::range<3>(1, 1, 16), sycl::range<3>(1, 1, 16), args, 0, &q_ct1);
+  // CHECK-NEXT: q_ct1.parallel_for(
+  // CHECK-NEXT:   sycl::nd_range<3>(sycl::range<3>(1, 1, 16) * sycl::range<3>(1, 1, 16), sycl::range<3>(1, 1, 16)), 
+  // CHECK-NEXT:   [=](sycl::nd_item<3> item_ct1) {
+  // CHECK-NEXT:     kernel_func();
+  // CHECK-NEXT:   });
   cudaLaunchCooperativeKernel(kernel_func, dim3(16), dim3(16), args, 0, 0);
 
   cudaStreamDestroy(stream);
