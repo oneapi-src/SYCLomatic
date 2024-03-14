79456bd1064211ef551047e01ce8e46a1aca241c
Chen, Sheng S <sheng.s.chen@intel.com>
2024-03-14 17:05:12 +0800

    update

--- clang/test/dpct/codepin/test.cpp
+++ clang/test/dpct/codepin/test.cpp
@@ -1,5 +1,3 @@
 // UNSUPPORTED: system-windows
 // RUN: auto-compare --cuda-log %S/cuda.json --sycl %S/sycl.json &> %T/compare_log
 // FileCheck --input-file %T/compare_log --match-full-lines %s
-
-// CHECK: Comparison succeeded, no differences found.
