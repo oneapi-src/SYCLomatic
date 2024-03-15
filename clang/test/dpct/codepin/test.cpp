03fa33cefdad8572109e3e3706802556df8b0a9f
Chen, Sheng S <sheng.s.chen@intel.com>
2024-03-15 14:06:46 +0800

    update
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/codepin/test.cpp
+++ clang/test/dpct/codepin/test.cpp
@@ -1,3 +1,2 @@
 // UNSUPPORTED: system-windows
-// RUN: auto-compare --cuda-log %S/cuda.json --sycl %S/sycl.json &> %T/compare_log
-// FileCheck --input-file %T/compare_log --match-full-lines %s
+// RUN: auto-compare --cuda-log %S/cuda.json --sycl %S/sycl.json
