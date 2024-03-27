9138355ab8d8cb42171c4e3387fadd674065045f
intwanghao <hao3.wang@intel.com>
2024-03-27 14:32:24 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -9121,18 +9121,6 @@ void ConstantMemVarMigrationRule::registerMatcher(MatchFinder &MF) {
 void ConstantMemVarMigrationRule::runRule(
     const MatchFinder::MatchResult &Result) {
   std::string CanonicalType;
-  auto IsVarUsedByRuntimeSymbolAPI = [&](const VarDecl *VarD) {
-    auto &VarUsedByRuntimeSymbolAPISet =
-        DpctGlobalInfo::getVarUsedByRuntimeSymbolAPISet();
-    auto LocInfo = DpctGlobalInfo::getLocInfo(VarD->getBeginLoc());
-    std::string Key = LocInfo.first.getCanonicalPath().str() +
-                      std::to_string(LocInfo.second) + VarD->getNameAsString();
-    if (VarUsedByRuntimeSymbolAPISet.find(Key) ==
-        VarUsedByRuntimeSymbolAPISet.end()) {
-      return false;
-    }
-    return true;
-  };
   if (auto MemVar = getAssistNodeAsType<VarDecl>(Result, "var")) {
     if (isCubVar(MemVar)) {
       return;
@@ -9151,7 +9139,7 @@ void ConstantMemVarMigrationRule::runRule(
     if (!Info)
       return;
     if (DpctGlobalInfo::isOptimizeMigration()) {
-      if (!IsVarUsedByRuntimeSymbolAPI(MemVar)) {
+      if (!DpctGlobalInfo::IsVarUsedByRuntimeSymbolAPI(Info)) {
         Info->setUseHelperFuncFlag(false);
       }
     }
@@ -9595,7 +9583,7 @@ void MemVarAnalysisRule::processTypeDeclaredLocal(
 void MemVarAnalysisRule::runRule(const MatchFinder::MatchResult &Result) {
   std::string CanonicalType;
   if (auto MemVar = getAssistNodeAsType<VarDecl>(Result, "var")) {
-    if (isCubVar(MemVar)) {
+    if (isCubVar(MemVar) || MemVar->hasAttr<CUDAConstantAttr>()) {
       return;
     }
     CanonicalType = MemVar->getType().getCanonicalType().getAsString();
