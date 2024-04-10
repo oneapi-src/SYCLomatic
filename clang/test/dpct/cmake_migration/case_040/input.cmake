7b3bdc24b4d2b6a0261d5ea940195cf5e61e1bfe
Deepak Raj H R <deepak.raj.h.r@intel.com>
2024-04-10 18:38:18 +0800

    Add cmake migration rule for set_source_files_prop

--- clang/test/dpct/cmake_migration/case_040/input.cmake
+++ clang/test/dpct/cmake_migration/case_040/input.cmake
@@ -1,3 +1,8 @@
 # simplest case
 set_source_files_properties(${src_files} PROPERTIES LANGUAGE CUDA)
 
+# with more properties after LANGUAGE
+set_source_files_properties(${src_files} PROPERTIES LANGUAGE CUDA COMPILE_DEFINITIONS SOME_DEFINE)
+
+set_source_files_properties(${src_files} PROPERTIES COMPILE_FLAGS "--some-flag" LANGUAGE CUDA COMPILE_DEFINITIONS)
+
