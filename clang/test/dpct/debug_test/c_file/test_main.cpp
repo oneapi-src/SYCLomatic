3f6f20d73e822a14a6721beb8fa8b37763ed4dc9
Shengchen <sheng.s.chen@intel.com>
2024-04-26 10:09:39 +0800

    [SYCLomatic] For --enable-codepin ,  generated instrumented code in <--out-root>_codepin_cuda and <--out-root>_codepin_sycl folder  (#1898)
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/debug_test/c_file/test_main.cpp
+++ clang/test/dpct/debug_test/c_file/test_main.cpp
@@ -1,13 +1,13 @@
 // RUN: dpct --enable-codepin --out-root %T/debug_test/c_file %S/test_main.cpp %S/test_cuda.cu --cuda-include-path="%cuda-path/include"
-// RUN: FileCheck %S/test_main.cpp --match-full-lines --input-file %T/debug_test/c_file/test_main.cpp -check-prefix=SYCL
-// RUN: FileCheck %S/test_cuda.cu --match-full-lines --input-file %T/debug_test/c_file/test_cuda.dp.cpp -check-prefix=SYCL
-// RUN: FileCheck %S/test.h --match-full-lines --input-file %T/debug_test/c_file/test.h -check-prefix=SYCL
-// RUN: FileCheck %S/test_main.cpp --match-full-lines --input-file %T/debug_test/c_file_debug/test_main.cpp -check-prefix=CUDA
-// RUN: FileCheck %S/test_cuda.cu --match-full-lines --input-file %T/debug_test/c_file_debug/test_cuda.cu -check-prefix=CUDA
-// RUN: FileCheck %S/test.h --match-full-lines --input-file %T/debug_test/c_file_debug/test.h -check-prefix=CUDA
-// RUN: %if build_lit %{icpx -fsycl %T/debug_test/c_file/test_main.cpp %T/debug_test/c_file/test_cuda.dp.cpp -o %T/debug_test/c_file/c_file.run %}
-// RUN: rm -rf %T/debug_test/c_file
-// RUN: rm -rf %T/debug_test/c_file_debug
+// RUN: FileCheck %S/test_main.cpp --match-full-lines --input-file %T/debug_test/c_file_codepin_sycl/test_main.cpp -check-prefix=SYCL
+// RUN: FileCheck %S/test_cuda.cu --match-full-lines --input-file %T/debug_test/c_file_codepin_sycl/test_cuda.dp.cpp -check-prefix=SYCL
+// RUN: FileCheck %S/test.h --match-full-lines --input-file %T/debug_test/c_file_codepin_sycl/test.h -check-prefix=SYCL
+// RUN: FileCheck %S/test_main.cpp --match-full-lines --input-file %T/debug_test/c_file_codepin_cuda/test_main.cpp -check-prefix=CUDA
+// RUN: FileCheck %S/test_cuda.cu --match-full-lines --input-file %T/debug_test/c_file_codepin_cuda/test_cuda.cu -check-prefix=CUDA
+// RUN: FileCheck %S/test.h --match-full-lines --input-file %T/debug_test/c_file_codepin_cuda/test.h -check-prefix=CUDA
+// RUN: %if build_lit %{icpx -fsycl %T/debug_test/c_file_codepin_sycl/test_main.cpp %T/debug_test/c_file_codepin_sycl/test_cuda.dp.cpp -o %T/debug_test/c_file_codepin_sycl/c_file.run %}
+// RUN: rm -rf %T/debug_test/c_file_codepin_sycl
+// RUN: rm -rf %T/debug_test/c_file_debug_cuda
 
 // SYCL: #include "test.h"
 // SYCL-NEXT: int main() {
