944213ec743701a46451b4652aca7f55d36cb5d9
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-04-30 16:56:06 +0800

    Add examples

---
+++ clang/examples/DPCT/cuSPARSE/cusparseDcsrsv_analysis.cu
@@ -0,0 +1,11 @@
+#include "cusparse.h"
+
+void test(cusparseHandle_t handle, cusparseOperation_t trans, int m, int nnz,
+          const void *alpha, const cusparseMatDescr_t desc, const double *value,
+          const int *row_ptr, const int *col_idx,
+          cusparseSolveAnalysisInfo_t info) {
+  // Start
+  cusparseDcsrsv_analysis(handle, trans, m, nnz, desc, value, row_ptr, col_idx,
+                          info);
+  // End
+}
---
+++ clang/examples/DPCT/cuSPARSE/cusparseZcsrsv_analysis.cu
@@ -0,0 +1,11 @@
+#include "cusparse.h"
+
+void test(cusparseHandle_t handle, cusparseOperation_t trans, int m, int nnz,
+          const void *alpha, const cusparseMatDescr_t desc,
+          const cuDoubleComplex *value, const int *row_ptr, const int *col_idx,
+          cusparseSolveAnalysisInfo_t info) {
+  // Start
+  cusparseZcsrsv_analysis(handle, trans, m, nnz, desc, value, row_ptr, col_idx,
+                          info);
+  // End
+}
