c2e41e6ca94408c0615549e8a417a8b7897b558c
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-04-30 09:01:36 +0800

    Add examples
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

---
+++ clang/examples/DPCT/cuBLAS/cublasGetMatrix_64.cu
@@ -0,0 +1,10 @@
+#include "cublas_v2.h"
+
+void test(int64_t rows, int64_t cols, int64_t elementsize, const void *a,
+          int64_t lda, void *b, int64_t ldb) {
+  // Start
+  cublasGetMatrix_64(rows /*int64_t*/, cols /*int64_t*/,
+                     elementsize /*int64_t*/, a /*const void **/,
+                     lda /*int64_t*/, b /*void **/, ldb /*int64_t*/);
+  // End
+}
