57e78cb6ed790ac3eec37c054c5437ea9e8df5b0
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-05-07 13:47:41 +0800

    Add cusolver migration examples
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

---
+++ clang/examples/DPCT/cuSolver/cusolverDnCreateGesvdjInfo.cu
@@ -0,0 +1,8 @@
+#include "cusolverDn.h"
+
+void test() {
+  // Start
+  gesvdjInfo_t info;
+  cusolverDnCreateGesvdjInfo(&info /*gesvdjInfo_t **/);
+  // End
+}
---
+++ clang/examples/DPCT/cuSolver/cusolverDnCreateSyevjInfo.cu
@@ -0,0 +1,8 @@
+#include "cusolverDn.h"
+
+void test() {
+  // Start
+  syevjInfo_t info;
+  cusolverDnCreateSyevjInfo(&info /*syevjInfo_t **/);
+  // End
+}
