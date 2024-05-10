1272320a1880ca224dceef70e2748b7ddfac57e9
chenwei.sun <chenwei.sun@intel.com>
2024-05-11 05:07:11 +0800

    Fix CI test failure
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/cmake_migration/case_051/case_051_help_msg_hint.cpp
+++ clang/test/dpct/cmake_migration/case_051/case_051_help_msg_hint.cpp
@@ -4,7 +4,11 @@
 // RUN: cat %S/ref.txt > ./check.txt
 // RUN: cat migration1.log >> ./check.txt
 // RUN: FileCheck --match-full-lines --input-file check.txt check.txt
-// RUN: not dpct -in-root ./ -out-root out  --migrate-build-script=CMake > migration2.log 2>&1
+// RUN: cat %S/foo.cu > ./foo.cu
+// RUN: cat %S/compile_commands.json > ./compile_commands.json
+// RUN: dpct -in-root ./ -out-root out --cuda-include-path="%cuda-path/include" -p ./ --migrate-build-script=CMake > migration2.log 2>&1
 // RUN: cat %S/ref.txt > ./check.txt
 // RUN: cat migration2.log >> ./check.txt
 // RUN: FileCheck --match-full-lines --input-file check.txt check.txt
+
+void foo() {}
