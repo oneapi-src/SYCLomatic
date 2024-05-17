89f0b7393427680c179ac1153e86bf117e3590d1
Tang Jiajun <jiajun.tang@intel.com>
2024-05-17 16:21:27 +0800

    [SYCLomatic] Support query mapping for some API with older version (v8.0~v10.0) header on windows. (#1975)
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- clang/test/dpct/query_api_mapping/Runtime/test.cu
+++ clang/test/dpct/query_api_mapping/Runtime/test.cu
@@ -1,5 +1,3 @@
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0
-
 /// Device Management
 
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=cudaDeviceGetAttribute | FileCheck %s -check-prefix=CUDADEVICEGETATTRIBUTE
