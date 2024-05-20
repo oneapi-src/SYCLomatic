0663a4309b006ee192267dc6a5389c64c40c1314
abhilash1910 <abhilash.majumder@intel.com>
2024-05-19 20:38:20 -0700

    add cmake

--- clang/lib/DPCT/GenHelperFunction.cpp
+++ clang/lib/DPCT/GenHelperFunction.cpp
@@ -73,6 +73,9 @@ const std::string FftUtilsAllContentStr =
 const std::string LapackUtilsAllContentStr =
 #include "clang/DPCT/lapack_utils.hpp.inc"
     ;
+const std::string GroupUtilsAllContentStr =
+#include "clang/DPCT/group_utils.hpp.inc"
+    ;
 const std::string DplExtrasAlgorithmAllContentStr =
 #include "clang/DPCT/dpl_extras/algorithm.h.inc"
     ;
@@ -162,6 +165,7 @@ void genHelperFunction(const clang::tooling::UnifiedPath &OutRoot) {
   GENERATE_ALL_FILE_CONTENT(SparseUtils, ".", sparse_utils.hpp)
   GENERATE_ALL_FILE_CONTENT(FftUtils, ".", fft_utils.hpp)
   GENERATE_ALL_FILE_CONTENT(LapackUtils, ".", lapack_utils.hpp)
+  GENERATE_ALL_FILE_CONTENT(GroupUtils, ".", group_utils.hpp)
   GENERATE_ALL_FILE_CONTENT(CodePin, "codepin", codepin.hpp)
   GENERATE_ALL_FILE_CONTENT(CodePinSerializationBasic, "codepin/serialization",
                             basic.hpp)
