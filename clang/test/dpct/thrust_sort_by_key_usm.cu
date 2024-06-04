da40f7788d68e96dec09ea49d2666138220d0238
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-06-04 07:57:42 -0400

    further DEBUGGING
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- clang/test/dpct/thrust_sort_by_key_usm.cu
+++ clang/test/dpct/thrust_sort_by_key_usm.cu
@@ -43,6 +43,16 @@ int main(void) {
   thrust::sort_by_key(                h_ptr,      h_ptr+4,  BH.begin());
   thrust::sort_by_key(                d_ptr,      d_ptr+4,  BD.begin());
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHECK:dpct::sort(oneapi::dpl::execution::seq, AH.begin(), AH.end(), BH.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::sort(oneapi::dpl::execution::make_device_policy(q_ct1), AD.begin(), AD.end(), BD.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::sort(oneapi::dpl::execution::seq, h_ptr, h_ptr + 4, BH.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::sort(oneapi::dpl::execution::seq, d_ptr, d_ptr + 4, BD.begin(), std::greater<int>());
+  // VERSION                          first       last      result      comparator
+  thrust::sort_by_key(                AH.begin(), AH.end(), BH.begin(), thrust::greater<int>());
+  thrust::sort_by_key(                AD.begin(), AD.end(), BD.begin(), thrust::greater<int>());
+  thrust::sort_by_key(                h_ptr,      h_ptr+4,  BH.begin(), thrust::greater<int>());  
+  thrust::sort_by_key(                d_ptr,      d_ptr+4,  BD.begin(), thrust::greater<int>());
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // CHECK:dpct::sort(oneapi::dpl::execution::seq, AH.begin(), AH.end(), BH.begin());
@@ -55,6 +65,14 @@ int main(void) {
   thrust::sort_by_key(thrust::host,   h_ptr,      h_ptr+4,  BH.begin());
   thrust::sort_by_key(thrust::device, d_ptr,      d_ptr+4,  BD.begin());
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHECK:dpct::sort(oneapi::dpl::execution::seq, AH.begin(), AH.end(), BH.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::sort(oneapi::dpl::execution::make_device_policy(q_ct1), AD.begin(), AD.end(), BD.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::sort(oneapi::dpl::execution::seq, h_ptr, h_ptr + 4, BH.begin(), std::greater<int>());
+  // VERSION          exec            first       last      result      comparator
+  thrust::sort_by_key(thrust::host,   AH.begin(), AH.end(), BH.begin(), thrust::greater<int>());
+  thrust::sort_by_key(thrust::device, AD.begin(), AD.end(), BD.begin(), thrust::greater<int>());
+  thrust::sort_by_key(thrust::host,   h_ptr,      h_ptr+4,  BH.begin(), thrust::greater<int>());
 
   return 0;
 }
--- clang/test/dpct/thrust_stable_sort_by_key_usm.cu
+++ clang/test/dpct/thrust_stable_sort_by_key_usm.cu
@@ -54,5 +54,25 @@ int main(void) {
   thrust::stable_sort_by_key(                h_ptr,      h_ptr+4,  BH.begin(), thrust::greater<int>());  
   thrust::stable_sort_by_key(                d_ptr,      d_ptr+4,  BD.begin(), thrust::greater<int>());
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHECK:dpct::stable_sort(oneapi::dpl::execution::seq, AH.begin(), AH.end(), BH.begin());
+// CHECK-NEXT:dpct::stable_sort(oneapi::dpl::execution::make_device_policy(q_ct1), AD.begin(), AD.end(), BD.begin());
+// CHECK-NEXT:dpct::stable_sort(oneapi::dpl::execution::seq, h_ptr, h_ptr + 4, BH.begin());
+// CHECK-NEXT:dpct::stable_sort(oneapi::dpl::execution::make_device_policy(q_ct1), d_ptr, d_ptr + 4, BD.begin());
+  // VERSION                 exec            first       last      result
+  thrust::stable_sort_by_key(thrust::host,   AH.begin(), AH.end(), BH.begin());
+  thrust::stable_sort_by_key(thrust::device, AD.begin(), AD.end(), BD.begin());
+  thrust::stable_sort_by_key(thrust::host,   h_ptr,      h_ptr+4,  BH.begin());
+  thrust::stable_sort_by_key(thrust::device, d_ptr,      d_ptr+4,  BD.begin());
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHECK:dpct::stable_sort(oneapi::dpl::execution::seq, AH.begin(), AH.end(), BH.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::stable_sort(oneapi::dpl::execution::make_device_policy(q_ct1), AD.begin(), AD.end(), BD.begin(), std::greater<int>());
+// CHECK-NEXT:dpct::stable_sort(oneapi::dpl::execution::seq, h_ptr, h_ptr + 4, BH.begin(), std::greater<int>());
+  // VERSION                 exec            first       last      result      comparator
+  thrust::stable_sort_by_key(thrust::host,   AH.begin(), AH.end(), BH.begin(), thrust::greater<int>());
+  thrust::stable_sort_by_key(thrust::device, AD.begin(), AD.end(), BD.begin(), thrust::greater<int>());
+  thrust::stable_sort_by_key(thrust::host,   h_ptr,      h_ptr+4,  BH.begin(), thrust::greater<int>());
+
   return 0;
 }
