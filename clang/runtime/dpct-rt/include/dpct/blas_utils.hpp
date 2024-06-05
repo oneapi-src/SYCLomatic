8f0db8c6ec1444f03369da699ce558f27ec5803a
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-06-05 15:25:17 +0800

    Add convert to col32_2r_4r4
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/runtime/dpct-rt/include/dpct/blas_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/blas_utils.hpp
@@ -3081,6 +3081,31 @@ void matrix_transform(queue_ptr q_ptr, size_t rows, size_t cols,
       });
     });
   } else if (a_order == order_t::col && c_order == order_t::col32_2r_4r4) {
+    q_ptr->submit([&](sycl::handler &cgh) {
+      cgh.parallel_for(sycl::range<2>(a_ld, cols), [=](sycl::id<2> index) {
+        size_t row_idx = index.get(0);
+        size_t col_idx = index.get(1);
+        if (row_idx < rows) {
+          size_t from_linear_idx = a_ld * col_idx + row_idx;
+
+          size_t from_row_in_row32_col32 = row_idx % 32;
+          size_t from_col_in_row32_col32 = col_idx % 32;
+
+          size_t to_row_in_row32_col32 =
+              8 * ((from_row_in_row32_col32 % 8) / 2) +
+              (from_row_in_row32_col32 / 8) * 2 + from_row_in_row32_col32 % 2;
+          size_t to_col_in_row32_col32 = from_col_in_row32_col32;
+          size_t to_linear_idx_in_row32_col32 =
+              to_row_in_row32_col32 * 32 + to_col_in_row32_col32;
+
+          size_t to_linear_idx = c_ld * (col_idx / 32) +
+                                 (row_idx / 32) * (32 * 32) +
+                                 to_linear_idx_in_row32_col32;
+
+          c[to_linear_idx] = a[from_linear_idx];
+        }
+      });
+    });
   } else if (a_order == order_t::col32_2r_4r4 && c_order == order_t::col) {
   } else {
     throw std::runtime_error("Unsupported order combination.");
