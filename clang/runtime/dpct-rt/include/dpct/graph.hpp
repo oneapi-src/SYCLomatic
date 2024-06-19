203e6ab9fed7227b951e060edc987802510a3340
Ahmed, Daiyaan <daiyaan.ahmed@intel.com>
2024-06-19 17:05:49 +0800

    [SYCLomatic] Migration of cudaStreamBeginCapture, cudaStreamEndCapture
    Signed-off-by: Ahmed, Daiyaan <daiyaan.ahmed@intel.com>

--- clang/runtime/dpct-rt/include/dpct/graph.hpp
+++ clang/runtime/dpct-rt/include/dpct/graph.hpp
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include "sycl/queue.hpp"
 #include <sycl/ext/oneapi/experimental/graph.hpp>
 #include <sycl/sycl.hpp>
 
@@ -21,6 +22,43 @@ typedef sycl::ext::oneapi::experimental::command_graph<
 typedef sycl::ext::oneapi::experimental::command_graph<
     sycl::ext::oneapi::experimental::graph_state::executable>
     *command_graph_exec_ptr;
+
+static inline std::unordered_map<
+    sycl::queue *,
+    sycl::ext::oneapi::experimental::command_graph<
+        sycl::ext::oneapi::experimental::graph_state::modifiable> *>
+    queue_graph_map;
+
+static bool command_graph_begin_recording(sycl::queue *queue_ptr) {
+  sycl::ext::oneapi::experimental::command_graph<
+      sycl::ext::oneapi::experimental::graph_state::modifiable>
+      graph(queue_ptr->get_context(), queue_ptr->get_device());
+  auto result = queue_graph_map.insert({queue_ptr, &graph});
+  if (!result.second) {
+    return false;
+  }
+  return result.first->second->begin_recording(*queue_ptr);
+}
+
+static bool
+command_graph_end_recording(sycl::queue *queue_ptr,
+                            dpct::experimental::command_graph_ptr graph) {
+  auto it = queue_graph_map.find(queue_ptr);
+  if (it == queue_graph_map.end()) {
+    return false;
+  }
+  graph = std::move(it->second);
+  return graph->end_recording();
+}
+
+// static bool
+// command_graph_end_recording(sycl::queue *queue_ptr,
+//                             dpct::experimental::command_graph_ptr &graph) {
+
+//   // bool status = queue_graph_map.at(queue_ptr).end_recording(*queue_ptr);
+//   // graph = &(queue_graph_map.at(queue_ptr));
+//   // return status;
+// }
 
 } // namespace experimental
 } // namespace dpct
