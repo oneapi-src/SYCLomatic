f15d2f12d6ed29fdcff159ab1564e63fe697aecd
chenwei.sun <chenwei.sun@intel.com>
2024-07-04 06:22:15 +0800

    Manual fix conflict code pieces
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -3137,31 +3137,16 @@ void Driver::BuildInputs(const ToolChain &TC, DerivedArgList &Args,
       Diag(clang::diag::note_drv_t_option_is_global);
   }
 
-  // Warn -x after last input file has no effect
-<<<<<<< HEAD
-  auto LastXArg = Args.getLastArgValue(options::OPT_x);
-  const llvm::StringSet<> ValidXArgs = {"cuda", "hip", "cui", "hipi"};
-  if (!IsCLMode() || ValidXArgs.contains(LastXArg)) {
 #ifndef SYCLomatic_CUSTOMIZATION
-=======
+  // Warn -x after last input file has no effect
   {
->>>>>>> origin/sycl
     Arg *LastXArg = Args.getLastArgNoClaim(options::OPT_x);
     Arg *LastInputArg = Args.getLastArgNoClaim(options::OPT_INPUT);
     if (LastXArg && LastInputArg &&
         LastInputArg->getIndex() < LastXArg->getIndex())
       Diag(clang::diag::warn_drv_unused_x) << LastXArg->getValue();
-<<<<<<< HEAD
-#endif // SYCLomatic_CUSTOMIZATION
-  } else {
-    // In CL mode suggest /TC or /TP since -x doesn't make sense if passed via
-    // /clang:.
-    if (auto *A = Args.getLastArg(options::OPT_x))
-      Diag(diag::err_drv_unsupported_opt_with_suggestion)
-          << A->getAsString(Args) << "/TC' or '/TP";
-=======
->>>>>>> origin/sycl
   }
+#endif // SYCLomatic_CUSTOMIZATION
 
   for (Arg *A : Args) {
     if (A->getOption().getKind() == Option::InputClass) {
