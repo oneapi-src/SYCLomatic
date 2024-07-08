41cab0d6972c334a931355b0e31909a5b20d086e
tomflinda <chenwei.sun@intel.com>
2024-07-08 16:04:39 +0800

    [SYCLomatic] Fix incorrect migration for option "-Xcompiler" and three cuda specific options (#2134)
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/cmake_migration/case_055/input.cmake
+++ clang/test/dpct/cmake_migration/case_055/input.cmake
@@ -8,3 +8,9 @@ set(CUDA_HOST_FLAGS "--extra-warnings -Wdeprecated")
 set (CUDA_HOST_FLAGS "--extra-warnings -Wdeprecated")
 
 set(CUDA_HOST_FLAGS "${host_flags}")
+
+list(APPEND CMAKE_CUDA_FLAGS "-Xcompiler=-Wno-float-conversion")
+list(APPEND CMAKE_CUDA_FLAGS "-Xcompiler=-fno-strict-aliasing")
+list(APPEND CMAKE_CUDA_FLAGS "-Xcudafe=--diag_suppress=unrecognized_gcc_pragma")
+list(APPEND CMAKE_CUDA_FLAGS "--extended-lambda")
+list(APPEND CMAKE_CUDA_FLAGS "--expt-relaxed-constexpr")
