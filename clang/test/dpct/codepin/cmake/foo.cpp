30c0815d24b9899e62c797c8f3c7690d4628c0f7
Chen, Sheng S <sheng.s.chen@intel.com>
2024-08-02 10:47:02 +0800

    up
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/codepin/cmake/foo.cpp
+++ clang/test/dpct/codepin/cmake/foo.cpp
@@ -1,7 +1,10 @@
 // UNSUPPORTED: system-windows
 // RUN: dpct --in-root %S -out-root %T/foo  --cuda-include-path="%cuda-path/include" --enable-codepin --migrate-build-script=CMake -- -std=c++14  -x cuda --cuda-host-only || true
 // RUN: cp %s %T/foo_codepin_sycl
-// RUN: cd %T/foo_codepin_sycl && mkdir build && cd build && cmake ..
+// RUN: cd %T/foo_codepin_sycl
+// RUN: ls  > default.log
+// RUN: FileCheck --input-file default.log --match-full-lines %T/foo_codepin_sycl/foo.cpp -check-prefix=DEFAULT
+// DEFAULT: CMakeLists.txt
 
 #include <iostream>
 
