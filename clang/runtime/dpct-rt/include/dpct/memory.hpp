f62bc25ea8e10d7d4ea41f326465baa1efea6c05
Chen, Sheng S <sheng.s.chen@intel.com>
2024-08-06 11:27:38 +0800

    up
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/runtime/dpct-rt/include/dpct/memory.hpp
+++ clang/runtime/dpct-rt/include/dpct/memory.hpp
@@ -32,6 +32,10 @@
 #error "Only support Windows and Linux."
 #endif
 
+#ifndef __DPCT_DEPRECATED
+#define __DPCT_DEPRECATED(message) [[deprecated(message)]]
+#endif
+
 namespace dpct {
 
 enum memcpy_direction {
