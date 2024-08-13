458c0871e7adba31fb7c6b201bea1694fb8e8a8b
Huang, Andy <andy.huang@intel.com>
2024-08-13 09:38:32 +0800

    [SYCLomatic] implement cuCtxCreate_v3 and vcuCtxCreate_v4
    Signed-off-by: Huang, Andy <andy.huang@intel.com>

--- clang/test/dpct/driver_context.cu
+++ clang/test/dpct/driver_context.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.1, cuda-11.2, cuda-11.3, cuda-11.4, cuda-11.5, cuda-11.6, cuda-11.7, cuda-12.0, cuda-12.1, cuda-12.2, cuda-12.3, cuda-12.4
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1, v11.2, v11.3, v11.4, v11.5, v11.6, v11.7, v12.0, v12.1, v12.2, v12.3, v12.4
 // RUN: dpct --format-range=none -out-root %T/driver_context %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck %s --match-full-lines --input-file %T/driver_context/driver_context.dp.cpp
 // RUN: %if build_lit %{icpx -c -fsycl %T/driver_context/driver_context.dp.cpp -o %T/driver_context/driver_context.dp.o %}
@@ -34,6 +34,16 @@ int main(){
 
   // CHECK: ctx = dpct::push_device_for_curr_thread(device);
   cuCtxCreate(&ctx, CU_CTX_LMEM_RESIZE_TO_MAX, device);
+
+
+  CUexecAffinityParam* paramsArray;
+  // CHECK: ctx = dpct::select_device(device);
+  cuCtxCreate_v3(&ctx, paramsArray, 1, flags, device);
+
+  CUctxCreateParams* ctxCreateParams;
+  // CHECK: ctx = dpct::select_device(device);
+  cuCtxCreate_v4(&ctx, ctxCreateParams, flags, device);
+
 
   // CHECK: ctx = dpct::select_device(device);
   cuDevicePrimaryCtxRetain(&ctx, device);
