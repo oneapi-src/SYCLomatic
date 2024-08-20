f8dec90f35a9bb519173e161e3c7f92140a2e5c6
Wang, Yihan <yihan.wang@intel.com>
2024-08-20 10:38:40 +0800

    [SYCLomatic] Fix build lit
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/blocklevel/blockradixsort.cu
+++ clang/test/dpct/cub/blocklevel/blockradixsort.cu
@@ -2,7 +2,7 @@
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct -format-range=none -in-root %S -out-root %T/blocklevel/blockradixsort %S/blockradixsort.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/blocklevel/blockradixsort/blockradixsort.dp.cpp --match-full-lines %s
-// RUN: %if build_lit %{icpx -c -fsycl %T/blocklevel/blockradixsort/blockradixsort.dp.cpp -o %T/blocklevel/blockradixsort/blockradixsort.dp.o %}
+// RUN: %if build_lit %{icpx -c -fsycl -DBUILD_TEST %T/blocklevel/blockradixsort/blockradixsort.dp.cpp -o %T/blocklevel/blockradixsort/blockradixsort.dp.o %}
 
 #include <cstdio>
 #include <cub/cub.cuh>
