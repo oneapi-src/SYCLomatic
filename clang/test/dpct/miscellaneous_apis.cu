10530940b5dbaafe1c8ebb464c7a7924ea7176bb
Deepak Raj H R <deepak.raj.h.r@intel.com>
2024-08-21 15:22:48 +0530

    Apply suggestions from code review

--- clang/test/dpct/miscellaneous_apis.cu
+++ clang/test/dpct/miscellaneous_apis.cu
@@ -4,7 +4,7 @@
 
 void foo(const void **table, CUuuid *tableId) {
   // CHECK: /*
-  // CHECK-NEXT: DPCT1026:{{[0-9]+}}: The call to cuGetExportTable was removed because the data of the retrieved table is insufficent to populate the function pointer with the necessary API.
+  // CHECK-NEXT: DPCT1026:{{[0-9]+}}: The call to cuGetExportTable was removed because the data of the retrieved table is insufficent to populate the function pointer with the necessary API. Check and implement the functionality correspounding to the API.
   // CHECK-NEXT: */
   cuGetExportTable(table, tableId);
 }
