42fb6b3f9d806dc9185436353b3a8b5bd00d3b1c
Teja Alaghari <teja.alaghari@intel.com>
2024-08-27 17:04:03 +0800

    Addressed comments

--- clang/test/dpct/cmake_migration/case_063/input.cmake
+++ clang/test/dpct/cmake_migration/case_063/input.cmake
@@ -1,5 +1,7 @@
 find_program(CUDAToolkit_NVCC_EXECUTABLE nvcc ${CUDAToolkit_BIN_DIR})
 
+find_program(CUDAToolkit_NVCC_other_EXE nvcc_other)
+
 find_program(CUDAToolkit_NVCC_EXECUTABLE
     NAMES nvcc nvcc.exe
     PATHS ${CUDAToolkit_BIN_DIR}
