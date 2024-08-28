de78f8f2229ea25884606f1fc73fb82e97dbb263
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-08-28 08:33:02 +0800

    Fix has_capability_or_fail usage
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/AnalysisInfo.cpp
+++ clang/lib/DPCT/AnalysisInfo.cpp
@@ -5983,8 +5983,10 @@ void KernelCallExpr::addDevCapCheckStmt() {
     requestFeature(HelperFeatureEnum::device_ext);
     std::string Str;
     llvm::raw_string_ostream OS(Str);
+    OS << MapNames::getDpctNamespace() << "get_device(";
+    OS << MapNames::getDpctNamespace() << "get_device_id(";
     printStreamBase(OS);
-    OS << "get_device().has_capability_or_fail({" << AspectList.front();
+    OS << "get_device())).has_capability_or_fail({" << AspectList.front();
     for (size_t i = 1; i < AspectList.size(); ++i) {
       OS << ", " << AspectList[i];
     }
