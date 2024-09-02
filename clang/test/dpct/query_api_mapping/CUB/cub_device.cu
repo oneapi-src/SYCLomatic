d7f03ad3f9813b1a6bea38cde7311d27e3beab5b
Wang, Yihan <yihan.wang@intel.com>
2024-09-02 14:47:04 +0800

    [SYCLomatic] Enable CUB query-api-mapping lit test on windows
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/query_api_mapping/CUB/cub_device.cu
+++ clang/test/dpct/query_api_mapping/CUB/cub_device.cu
@@ -1,6 +1,5 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4
-// UNSUPPORTED: system-windows
 // clang-format off
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=cub::DeviceReduce::ArgMax | FileCheck %s -check-prefix=CHECK_REDUCE_ARG_MAX
 // CHECK_REDUCE_ARG_MAX: CUDA API:
