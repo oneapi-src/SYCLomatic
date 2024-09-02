5351da04e48a310a33622fc598f869e8def6cd74
Deepak Raj H R <deepak.raj.h.r@intel.com>
2024-09-02 15:33:12 +0800

    Update lit

--- clang/test/dpct/kernel-function-typecast.cu
+++ clang/test/dpct/kernel-function-typecast.cu
@@ -6,8 +6,13 @@
 #include <cuda.h>
 
 typedef uint64_t u64;
+
+// CHECK: u64 foo(dpct::kernel_function cuFunc, dpct::kernel_library cuMod) {
 u64 foo(CUfunction cuFunc, CUmodule cuMod) {
+  // CHECK: cuFunc = dpct::get_kernel_function(cuMod, "kfoo");
   cuModuleGetFunction(&cuFunc, cuMod, "kfoo");
   u64 function = (u64)cuFunc;
+  u64 functionTwo = cuFunc;
+
   return function;
 }
