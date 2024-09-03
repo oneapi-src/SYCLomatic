4303c51760fcdd7d02aaeddc50eb113a50cd789b
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-09-03 15:49:04 +0800

    Fix test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/uppercase_filename/test.cu
+++ clang/test/dpct/uppercase_filename/test.cu
@@ -1,12 +1,12 @@
 // RUN: dpct --out-root %T/uppercase_filename %s --cuda-include-path="%cuda-path/include"
 // RUN: FileCheck %s --match-full-lines --input-file %T/uppercase_filename/test.dp.cpp
-// RUN: FileCheck %S/Header.h --match-full-lines --input-file %T/uppercase_filename/Header.h
+// RUN: FileCheck %S/Header.cuh --match-full-lines --input-file %T/uppercase_filename/Header.dp.hpp
 // RUN: %if build_lit %{icpx -c -fsycl %T/uppercase_filename/test.dp.cpp -o %T/uppercase_filename/test.dp.o %}
 
 // CHECK: #include <sycl/sycl.hpp>
 // CHECK-NEXT: #include <dpct/dpct.hpp>
-// CHECK-NEXT: #include "Header.h"
-#include "Header.h"
+// CHECK-NEXT: #include "Header.dp.hpp"
+#include "Header.cuh"
 
 int main() {
   f2.x = 123.f;
