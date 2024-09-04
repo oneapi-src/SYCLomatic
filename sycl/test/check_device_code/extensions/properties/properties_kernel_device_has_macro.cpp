0b1f1770dbbc48da478fed5bab3b778b763da101
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-09-04 14:42:54 +0530

    kernel macro update

--- sycl/test/check_device_code/extensions/properties/properties_kernel_device_has_macro.cpp
+++ sycl/test/check_device_code/extensions/properties/properties_kernel_device_has_macro.cpp
@@ -11,16 +11,16 @@ static constexpr auto device_has_all = device_has<
     aspect::ext_oneapi_cuda_async_barrier, aspect::custom, aspect::fp16,
     aspect::fp64, aspect::image, aspect::online_compiler, aspect::online_linker,
     aspect::queue_profiling, aspect::usm_device_allocations,
-    aspect::usm_system_allocations, aspect::ext_intel_pci_address, aspect::cpu,
-    aspect::gpu, aspect::accelerator, aspect::ext_intel_gpu_eu_count,
-    aspect::ext_intel_gpu_subslices_per_slice,
-    aspect::ext_intel_gpu_eu_count_per_subslice,
-    aspect::ext_intel_max_mem_bandwidth, aspect::ext_intel_mem_channel,
+    aspect::usm_system_allocations, aspect::pci_address, aspect::cpu,
+    aspect::gpu, aspect::accelerator, aspect::gpu_eu_count,
+    aspect::gpu_subslices_per_slice,
+    aspect::gpu_eu_count_per_subslice,
+    aspect::max_mem_bandwidth, aspect::ext_intel_mem_channel,
     aspect::usm_atomic_host_allocations, aspect::usm_atomic_shared_allocations,
-    aspect::atomic64, aspect::ext_intel_device_info_uuid,
-    aspect::ext_oneapi_srgb, aspect::ext_intel_gpu_eu_simd_width,
-    aspect::ext_intel_gpu_slices, aspect::ext_oneapi_native_assert,
-    aspect::host_debuggable, aspect::ext_intel_gpu_hw_threads_per_eu,
+    aspect::atomic64, aspect::uuid,
+    aspect::ext_oneapi_srgb, aspect::gpu_eu_simd_width,
+    aspect::gpu_slices, aspect::ext_oneapi_native_assert,
+    aspect::host_debuggable, aspect::gpu_hw_threads_per_eu,
     aspect::usm_host_allocations, aspect::usm_shared_allocations,
     aspect::ext_intel_free_memory, aspect::ext_intel_device_id,
     aspect::ext_intel_memory_clock_rate, aspect::ext_intel_memory_bus_width>;
@@ -45,25 +45,25 @@ SYCL_EXTERNAL SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"queue_profiling", i32 [[queue_profiling_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_device_allocations", i32 [[usm_device_allocations_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_system_allocations", i32 [[usm_system_allocations_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_pci_address", i32 [[ext_intel_pci_address_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"pci_address", i32 [[pci_address_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"cpu", i32 [[cpu_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu", i32 [[gpu_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"accelerator", i32 [[accelerator_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_eu_count", i32 [[ext_intel_gpu_eu_count_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_subslices_per_slice", i32 [[ext_intel_gpu_subslices_per_slice_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_eu_count_per_subslice", i32 [[ext_intel_gpu_eu_count_per_subslice_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_max_mem_bandwidth", i32 [[ext_intel_max_mem_bandwidth_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_eu_count", i32 [[gpu_eu_count_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_subslices_per_slice", i32 [[gpu_subslices_per_slice_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_eu_count_per_subslice", i32 [[gpu_eu_count_per_subslice_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"max_mem_bandwidth", i32 [[max_mem_bandwidth_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_mem_channel", i32 [[ext_intel_mem_channel_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_atomic_host_allocations", i32 [[usm_atomic_host_allocations_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_atomic_shared_allocations", i32 [[usm_atomic_shared_allocations_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"atomic64", i32 [[atomic64_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_device_info_uuid", i32 [[ext_intel_device_info_uuid_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"uuid", i32 [[uuid_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_oneapi_srgb", i32 [[ext_oneapi_srgb_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_eu_simd_width", i32 [[ext_intel_gpu_eu_simd_width_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_slices", i32 [[ext_intel_gpu_slices_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_eu_simd_width", i32 [[gpu_eu_simd_width_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_slices", i32 [[gpu_slices_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_oneapi_native_assert", i32 [[ext_oneapi_native_assert_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"host_debuggable", i32 [[host_debuggable_ASPECT_MD:[0-9]+]]}
-// CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_gpu_hw_threads_per_eu", i32 [[ext_intel_gpu_hw_threads_per_eu_ASPECT_MD:[0-9]+]]}
+// CHECK-IR-DAG: !{{[0-9]+}} = !{!"gpu_hw_threads_per_eu", i32 [[gpu_hw_threads_per_eu_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_host_allocations", i32 [[usm_host_allocations_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"usm_shared_allocations", i32 [[usm_shared_allocations_ASPECT_MD:[0-9]+]]}
 // CHECK-IR-DAG: !{{[0-9]+}} = !{!"ext_intel_free_memory", i32 [[ext_intel_free_memory_ASPECT_MD:[0-9]+]]}
