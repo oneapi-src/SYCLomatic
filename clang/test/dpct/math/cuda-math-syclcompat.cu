ea319feb85043150cfc6173b3a4133b791a82c6c
Ziran Zhang <ziran.zhang@intel.com>
2024-09-06 17:14:54 +0800

    Disable the lit running due to syclcompat headers issue.

--- clang/test/dpct/math/cuda-math-syclcompat.cu
+++ clang/test/dpct/math/cuda-math-syclcompat.cu
@@ -7,6 +7,7 @@
 #include "cuda_bf16.h"
 #include "cuda_fp16.h"
 
+#ifndef BUILD_TEST
 __global__ void kernelFuncBfloat162Arithmetic() {
   __nv_bfloat162 bf162, bf162_1, bf162_2, bf162_3;
   // CHECK: bf162 = syclcompat::cmul_add(bf162_1, bf162_2, bf162_3);
@@ -16,3 +17,4 @@ __global__ void kernelFuncBfloat162Arithmetic() {
   // CHECK: h2_2 = syclcompat::cmul_add(h2, h2_1, h2_2);
   h2_2 = __hcmadd(h2, h2_1, h2_2);
 }
+#endif
