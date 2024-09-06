fd16b4eca48bb6bc65f056afc7dbfa2ade19b803
Ziran Zhang <ziran.zhang@intel.com>
2024-09-06 15:15:10 +0800

    [NFC][SYCLomatic] Fix a lit test fail
    Signed-off-by: Ziran Zhang <ziran.zhang@intel.com>

--- clang/test/dpct/math/cuda-math-syclcompat.cu
+++ clang/test/dpct/math/cuda-math-syclcompat.cu
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none -out-root %T/math/cuda-math-syclcompat %s -use-syclcompat --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only --std=c++14
 // RUN: FileCheck --input-file %T/math/cuda-math-syclcompat/cuda-math-syclcompat.dp.cpp --match-full-lines %s
 // RUN: %if build_lit %{icpx -c -fsycl -DBUILD_TEST %T/math/cuda-math-syclcompat/cuda-math-syclcompat.dp.cpp -o %T/math/cuda-math-syclcompat/cuda-math-syclcompat.dp.o %}
