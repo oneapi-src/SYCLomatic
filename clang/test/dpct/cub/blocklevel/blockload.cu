72ba72d9fe7db5bf7c95330c03064e6aa0e396da
Wang, Yihan <yihan.wang@intel.com>
2024-09-24 18:06:57 +0800

    Disable test on windows
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/blocklevel/blockload.cu
+++ clang/test/dpct/cub/blocklevel/blockload.cu
@@ -1,5 +1,6 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
+// UNSUPPORTED: system-windows
 // RUN: dpct -in-root %S -out-root %T/blocklevel/blockload %S/blockload.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/blocklevel/blockload/blockload.dp.cpp --match-full-lines %s
 // RUN: %if build_lit %{icpx -c -fsycl %T/blocklevel/blockload/blockload.dp.cpp -o %T/blocklevel/blockload/blockload.dp.o %}
