b28a20ebdea9aed355e3cd452014f00ff78751c5
Wang, Yihan <yihan.wang@intel.com>
2024-09-24 14:07:15 +0800

    Fix build lit
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/test/dpct/cub/using_namespace.cu
+++ clang/test/dpct/cub/using_namespace.cu
@@ -2,7 +2,7 @@
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct -in-root %S -out-root %T/using_namespace %S/using_namespace.cu --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/using_namespace/using_namespace.dp.cpp --match-full-lines %s
-// RUN: %if build_lit %{icpx -c -fsycl -DBUILD_TEST  %T/type/using_namespace/using_namespace.dp.cpp -o %T/type/using_namespace/using_namespace.dp.o %}
+// RUN: %if build_lit %{icpx -c -fsycl -DBUILD_TEST  %T/using_namespace/using_namespace.dp.cpp -o %T/using_namespace/using_namespace.dp.o %}
 
 #include <cub/cub.cuh>
 #include <cuda_runtime.h>
