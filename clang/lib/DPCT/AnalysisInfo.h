eb207ff9d8b09fae3c7b3974c188c8168ad5f59d
Wang, Yihan <yihan.wang@intel.com>
2024-10-09 17:31:33 +0800

    [SYCLomatic] Add braces around kernel call that use root-group
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- clang/lib/DPCT/AnalysisInfo.h
+++ clang/lib/DPCT/AnalysisInfo.h
@@ -2836,6 +2836,7 @@ private:
 
   void removeExtraIndent();
   void addDevCapCheckStmt();
+  void addPropertiesStmt();
   void addAccessorDecl(MemVarInfo::VarScope Scope);
   void addAccessorDecl(std::shared_ptr<MemVarInfo> VI);
   void addStreamDecl();
