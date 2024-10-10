03939739bd5c1cfe9d734f5a0694618cc91d7a2a
intwanghao <hao3.wang@intel.com>
2024-10-10 13:20:12 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/test/dpct/virtual_memory.cu
+++ clang/test/dpct/virtual_memory.cu
@@ -1,3 +1,5 @@
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none --use-experimental-features=virtual_memory -out-root %T/virtual_memory %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
 // RUN: FileCheck %s --match-full-lines --input-file %T/virtual_memory/virtual_memory.dp.cpp
 #include <cuda.h>
