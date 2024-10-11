04885f2284416529888b8a68a99904002d16cd3c
AndyCHHuang <andy.huang@intel.com>
2024-10-11 09:29:47 +0800

    [SYCLomatic] Fix get range in range for stringfy macro (#2400)
    Signed-off-by: Huang, Andy <andy.huang@intel.com>

--- clang/test/dpct/macro_test.cu
+++ clang/test/dpct/macro_test.cu
@@ -1384,4 +1384,26 @@ int foo39() {
   CALL(0, 1)
   return 0;
 }
+
+#define STRINGIFY_(...) #__VA_ARGS__
+#define STRINGIFY(...) STRINGIFY_(__VA_ARGS__)
+
+//CHECK: void foo40_dev_func(const char *file_name, const char *other,
+//CHECK-NEXT:                     const sycl::stream &stream_ct1) {
+//CHECK-NEXT:   stream_ct1 << "sss\n";
+//CHECK-NEXT: }
+//CHECK-NEXT: #define FOO40_MACRO                                                            \
+//CHECK-NEXT:   foo40_dev_func(__FILE__, STRINGIFY(__OTHER_MACRO__), stream_ct1)
+__device__ void foo40_dev_func(const char *file_name, const char *other) {
+  printf("sss\n");
+}
+#define FOO40_MACRO foo40_dev_func(__FILE__, STRINGIFY(__OTHER_MACRO__))
+
+__global__ void foo40_kernel() {
+  FOO40_MACRO;
+}
+void foo40() {
+  foo40_kernel<<<1, 1>>>();
+}
+
 #endif
