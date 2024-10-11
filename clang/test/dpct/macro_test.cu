da8eaaa07344e43e9d56ba53b643076d87dea2aa
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-10-11 16:07:23 +0800

    [SYCLomatic] Fix extra arg inserting location (in macro) (#2379)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/macro_test.cu
+++ clang/test/dpct/macro_test.cu
@@ -1385,25 +1385,13 @@ int foo39() {
   return 0;
 }
 
-#define STRINGIFY_(...) #__VA_ARGS__
-#define STRINGIFY(...) STRINGIFY_(__VA_ARGS__)
-
-//CHECK: void foo40_dev_func(const char *file_name, const char *other,
-//CHECK-NEXT:                     const sycl::stream &stream_ct1) {
-//CHECK-NEXT:   stream_ct1 << "sss\n";
+//CHECK: void foo40_kernel(const sycl::stream &stream_ct1) {
+//CHECK-NEXT:   FOO40_MACRO;
 //CHECK-NEXT: }
-//CHECK-NEXT: #define FOO40_MACRO                                                            \
-//CHECK-NEXT:   foo40_dev_func(__FILE__, STRINGIFY(__OTHER_MACRO__), stream_ct1)
-__device__ void foo40_dev_func(const char *file_name, const char *other) {
-  printf("sss\n");
-}
-#define FOO40_MACRO foo40_dev_func(__FILE__, STRINGIFY(__OTHER_MACRO__))
-
 __global__ void foo40_kernel() {
   FOO40_MACRO;
 }
 void foo40() {
   foo40_kernel<<<1, 1>>>();
 }
-
 #endif
