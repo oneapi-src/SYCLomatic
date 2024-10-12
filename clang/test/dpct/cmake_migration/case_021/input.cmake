27185c9d55d0fc3843219714c3eb59e324f8ffc8
Teja Alaghari <teja.alaghari@intel.com>
2024-10-12 11:26:20 +0800

    Removed duplicate case

--- clang/test/dpct/cmake_migration/case_021/input.cmake
+++ clang/test/dpct/cmake_migration/case_021/input.cmake
@@ -11,7 +11,3 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
 add_link_options("--add-arch=sm_70")
 list(APPEND PMEMD_NVCC_FLAGS --std c++11)
-
-string(APPEND CMAKE_C_FLAGS " -prec_div")
-string(APPEND CMAKE_CXX_FLAGS " -parallel")
-string(APPEND CMAKE_CXX_FLAGS " -wd188,186,144,913,556,858,597,177,1292,167,279,592,94,2722,3199")
