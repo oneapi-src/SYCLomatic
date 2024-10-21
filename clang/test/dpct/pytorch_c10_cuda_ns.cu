d527efee898e02a240fb5e9ea7ab5edb757e0f73
Teja Alaghari <teja.alaghari@intel.com>
2024-10-21 11:29:43 +0800

    Fixed lit config bug

--- clang/test/dpct/pytorch_c10_cuda_ns.cu
+++ clang/test/dpct/pytorch_c10_cuda_ns.cu
@@ -1,13 +1,13 @@
 // RUN: rm -rf %T/src
 // RUN: mkdir %T/src
-// RUN: cat %s > %T/src/pytorch_c10_cuda_ns.cu
-// RUN: cat %S/user_defined_rule_pytorch.yaml > %T/user_defined_rule_pytorch.yaml
+// RUN: cp %S/pytorch_c10_cuda_ns.cu %T/src/
+// RUN: cp %S/user_defined_rule_pytorch.yaml %T/
 // RUN: cp -r %S/pytorch_cuda_inc %T/
 // RUN: cd %T
 // RUN: rm -rf %T/pytorch_c10_cuda_ns_output
 // RUN: mkdir %T/pytorch_c10_cuda_ns_output
 // RUN: dpct -out-root %T/pytorch_c10_cuda_ns_output src/pytorch_c10_cuda_ns.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/pytorch_c10_cuda_ns_output/pytorch_c10_cuda_ns.dp.cpp --match-full-lines pytorch_c10_cuda_ns.cu
+// RUN: FileCheck --input-file %T/pytorch_c10_cuda_ns_output/pytorch_c10_cuda_ns.dp.cpp --match-full-lines %T/src/pytorch_c10_cuda_ns.cu
 // RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST  %T/pytorch_c10_cuda_ns_output/pytorch_c10_cuda_ns.dp.cpp -o %T/pytorch_c10_cuda_ns_output/pytorch_c10_cuda_ns.dp.o %}
 
 #ifndef NO_BUILD_TEST
