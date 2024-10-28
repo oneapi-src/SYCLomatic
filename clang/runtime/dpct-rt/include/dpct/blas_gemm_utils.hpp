13c1e778b2374a6e041eca1eddb8f5276b1d9afe
Abhilash Majumder <abhilash.majumder@intel.com>
2024-10-28 05:58:41 -0700

    review comments

--- clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
@@ -134,7 +134,7 @@ public:
   };
 
   matmul_desc_t(compute_type compute_type, library_data_t scale_type)
-      : _compute_type(compute_type), _scale_type(scale_type), _bias_type(bias_type) {}
+      : _compute_type(compute_type), _scale_type(scale_type) {}
 
   void set_attribute(attribute attr, const void *mem) {
     if (attr != attribute::unsupport)
@@ -176,7 +176,7 @@ private:
 
   compute_type _compute_type;
   library_data_t _scale_type;
-  library_data_t _bias_type;
+  library_data_t _bias_type = library_data_t::real_float;
   pointer_mode_t _pointer_mode = pointer_mode_t::host;
   oneapi::mkl::transpose _trans_a = oneapi::mkl::transpose::nontrans;
   oneapi::mkl::transpose _trans_b = oneapi::mkl::transpose::nontrans;
