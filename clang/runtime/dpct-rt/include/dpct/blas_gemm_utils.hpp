a084d4180a5084e1bb75affd9bfcc7170f25ecb4
Abhilash Majumder <abhilash.majumder@intel.com>
2024-10-29 23:12:14 -0700

    fix var name

--- clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
@@ -102,8 +102,8 @@ private:
   std::uint64_t _rows;
   std::uint64_t _cols;
   std::int64_t _ld;
-  std::uint64_t batch_count = 1;
-  std::uint64_t strided_batch_offset = 0;
+  std::uint64_t _batch_count = 1;
+  std::uint64_t _strided_batch_offset = 0;
 
   friend sycl::event matmul(descriptor_ptr handle, matmul_desc_ptr computeDesc,
                             const void *alpha, const void *a,
