a4f3c14c3ef953ae9309f6fa065b64e6f2430d4d
intwanghao <hao3.wang@intel.com>
2024-11-04 17:55:12 +0800

    fixsomebug
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/lib/Lex/Preprocessor.cpp
+++ clang/lib/Lex/Preprocessor.cpp
@@ -818,7 +818,7 @@ bool Preprocessor::HandleIdentifier(Token &Identifier) {
 #ifdef SYCLomatic_CUSTOMIZATION
   // The "__noinline__" macro is re-defined and it is used in "__attribute__()",
   // do not handle it.
-  if (Identifier.is(tok::TokenKind::kw___noinline__) &&
+  if (LangOpts.CUDA && Identifier.is(tok::TokenKind::kw___noinline__) &&
       IsInAnalysisScopeFunc(Identifier.getLocation()) && IsInAttr) {
     return true;
   }
