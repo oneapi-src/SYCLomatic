76bb4a7d777178e62d000e8b9a9cd2535d7cd313
intwanghao <hao3.wang@intel.com>
2024-11-04 21:11:20 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/test/dpct/cppnoinline.cpp
+++ clang/test/dpct/cppnoinline.cpp
@@ -2,6 +2,8 @@
 // RUN: FileCheck %s --match-full-lines --input-file %T/cppnoinline/cppnoinline.cpp.dp.cpp
 // RUN: %if build_lit %{icpx -c -fsycl %T/cppnoinline/cppnoinline.cpp.dp.cpp -o %T/cppnoinline/cppnoinline.cpp.dp.o %}
 
+#include <cuda_runtime.h>
+
 // CHECK: #define NOINLINE __attribute__((__noinline__))
 #define NOINLINE __attribute__((__noinline__))
 // CHECK: NOINLINE void macro_in_macro_with_attr() {}
