be83487fd01099f7016a9ecdc1d1693ff087ff9f
intwanghao <hao3.wang@intel.com>
2024-11-05 09:15:19 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -281,7 +281,10 @@ void IncludesCallbacks::MacroDefined(const Token &MacroNameTok,
 
     // The "__noinline__" macro is re-defined and it is used in
     // "__attribute__()", do not migrate it.
-    if (!II->hasMacroDefinition() && (II->getName() == "__noinline__")) {
+    if ((GetSourceFileType(
+             DpctGlobalInfo::getInstance().getMainFile()->getFilePath()) ==
+         SPT_CppSource) &&
+        (II->getName() == "__noinline__")) {
       continue;
     }
 
