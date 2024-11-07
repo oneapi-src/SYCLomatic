870001f51b14bf39516a68665596cdea53080441
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-11-07 16:35:27 +0800

    [SYCLomatic] Fix build
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/RulesInclude/InclusionHeaders.h
+++ clang/lib/DPCT/RulesInclude/InclusionHeaders.h
@@ -17,7 +17,7 @@ namespace dpct {
 
 enum HeaderType {
 #define HEADER(Name, Spelling) HT_##Name,
-#include "RulesInclude/HeaderTypes.inc"
+#include "HeaderTypes.inc"
   NUM_HEADERS,
   HT_NULL = -1
 };
