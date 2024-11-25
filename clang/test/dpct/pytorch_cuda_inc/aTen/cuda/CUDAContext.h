e79b69260d9dd36a7c4cf8ef39f3008e803fdba6
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-24 23:41:58 -0800

    add cuda aten headers

---
+++ clang/test/dpct/pytorch_cuda_inc/aTen/cuda/CUDAContext.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <ATen/cuda/CUDAContextLight.h>
+
+#include <ATen/Context.h>
+#include <c10/cuda/CUDAStream.h>
+#include <c10/util/Logging.h>
+#include <ATen/cuda/Exceptions.h>
