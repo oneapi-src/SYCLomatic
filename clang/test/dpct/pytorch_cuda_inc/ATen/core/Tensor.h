df5d5922942361bc2ef7f83e41fc27d08a984605
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-24 23:56:15 -0800

    clang format

--- clang/test/dpct/pytorch_cuda_inc/ATen/core/Tensor.h
+++ clang/test/dpct/pytorch_cuda_inc/ATen/core/Tensor.h
@@ -6,23 +6,23 @@
 namespace at {
 // NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
 class TORCH_API OptionalTensorRef {
- public:
+public:
   OptionalTensorRef() = default;
 
   ~OptionalTensorRef() {
     ref_.unsafeReleaseTensorImpl();
   }
 
-  OptionalTensorRef(const TensorBase& src)
+  OptionalTensorRef(const TensorBase &src)
       : ref_(Tensor::unsafe_borrow_t{}, src) {
     TORCH_INTERNAL_ASSERT_DEBUG_ONLY(src.defined());
   }
 
-  OptionalTensorRef(const OptionalTensorRef& rhs)
+  OptionalTensorRef(const OptionalTensorRef &rhs)
       : ref_(Tensor::unsafe_borrow_t{}, rhs.ref_) {}
 
-  OptionalTensorRef(OptionalTensorRef&& rhs) = default;
-  OptionalTensorRef& operator=(OptionalTensorRef rhs) {
+  OptionalTensorRef(OptionalTensorRef &&rhs) = default;
+  OptionalTensorRef &operator=(OptionalTensorRef rhs) {
     std::swap(ref_, rhs.ref_);
     return *this;
   }
@@ -31,15 +31,15 @@ class TORCH_API OptionalTensorRef {
     return ref_.defined();
   }
 
-  const Tensor& getTensorRef() const & {
+  const Tensor &getTensorRef() const & {
     return ref_;
   }
 
-  const Tensor& operator*() const & {
+  const Tensor &operator*() const & {
     return ref_;
   }
 
-  const Tensor* operator->() const & {
+  const Tensor *operator->() const & {
     return &ref_;
   }
 
@@ -47,40 +47,41 @@ class TORCH_API OptionalTensorRef {
     return ref_.defined();
   }
 
- private:
+private:
   Tensor ref_;
 };
 
 // Use to convert a TensorBase (that may be undefined) to an at::Tensor
 // without bumping refcount.
 class TORCH_API TensorRef {
- public:
+public:
   ~TensorRef() {
     ref_.unsafeReleaseTensorImpl();
   }
 
-  TensorRef(const TensorBase& src)
+  TensorRef(const TensorBase &src)
       : ref_(Tensor::unsafe_borrow_t{}, src) {}
-  TensorRef(TensorRef&& other) = default;
-  TensorRef(const TensorRef&) = default;
-  TensorRef& operator=(const TensorRef&) = default;
-  TensorRef& operator=(TensorRef&&) = default;
+  TensorRef(TensorRef &&other) = default;
+  TensorRef(const TensorRef &) = default;
+  TensorRef &operator=(const TensorRef &) = default;
+  TensorRef &operator=(TensorRef &&) = default;
 
-  const Tensor& operator*() const & {
+  const Tensor &operator*() const & {
     return ref_;
   }
- private:
+
+private:
   Tensor ref_;
 };
 
 template <typename T>
 // NOLINTNEXTLINE(cppcoreguidelines-missing-std-forward)
-auto Tensor::register_hook(T&& hook) const -> Tensor::hook_return_void_t<T> {
+auto Tensor::register_hook(T &&hook) const -> Tensor::hook_return_void_t<T> {
   // Return the grad argument in case of a hook with void return type to have an
   // std::function with Tensor return type
   static_assert(std::is_same_v<decltype(hook(Tensor())), void>,
                 "Expected hook to return void");
-  return _register_hook([fn=std::forward<T>(hook)](const TensorBase& grad_base) {
+  return _register_hook([fn = std::forward<T>(hook)](const TensorBase &grad_base) {
     TensorRef grad(grad_base);
     fn(*grad);
     return Tensor();
@@ -89,8 +90,8 @@ auto Tensor::register_hook(T&& hook) const -> Tensor::hook_return_void_t<T> {
 
 template <typename T>
 // NOLINTNEXTLINE(cppcoreguidelines-missing-std-forward)
-auto Tensor::register_hook(T&& hook) const -> Tensor::hook_return_var_t<T> {
-  return _register_hook([fn=std::forward<T>(hook)](const TensorBase& grad_base) {
+auto Tensor::register_hook(T &&hook) const -> Tensor::hook_return_var_t<T> {
+  return _register_hook([fn = std::forward<T>(hook)](const TensorBase &grad_base) {
     TensorRef grad(grad_base);
     Tensor ret = fn(*grad);
     return TensorBase(std::move(ret));
--- clang/test/dpct/pytorch_cuda_inc/ATen/cuda/CUDAContext.h
+++ clang/test/dpct/pytorch_cuda_inc/ATen/cuda/CUDAContext.h
@@ -3,6 +3,6 @@
 #include <ATen/cuda/CUDAContextLight.h>
 
 #include <ATen/Context.h>
+#include <ATen/cuda/Exceptions.h>
 #include <c10/cuda/CUDAStream.h>
 #include <c10/util/Logging.h>
-#include <ATen/cuda/Exceptions.h>
