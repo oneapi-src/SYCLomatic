c1a89662090e04b71633a0432eec4613ecc5bdd5
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-25 23:13:22 -0800

    fix

--- clang/test/dpct/pytorch_ATen_cuda_ns.cu
+++ clang/test/dpct/pytorch_ATen_cuda_ns.cu
@@ -6,7 +6,7 @@
 // RUN: cd %T
 // RUN: rm -rf %T/pytorch_ATen_cuda_ns_output
 // RUN: mkdir %T/pytorch_ATen_cuda_ns_output
-// RUN: dpct -out-root %T/pytorch_ATen_cuda_ns_output src/pytorch_ATen_cuda_ns.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
+// RUN: dpct -out-root %T/pytorch_ATen_cuda_ns_output %T/src/pytorch_ATen_cuda_ns.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=%T/user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
 // RUN: FileCheck --input-file %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.cpp --match-full-lines %T/src/pytorch_ATen_cuda_ns.cu
 // RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST  %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.cpp -o %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.o %}
 
