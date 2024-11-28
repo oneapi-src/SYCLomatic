ea7518ebdf685c11df58ba6c9d842aac46962ce7
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-27 21:26:41 -0800

    replace folder name

--- clang/test/dpct/pytorch_ATen_cuda_ns.cu
+++ clang/test/dpct/pytorch_ATen_cuda_ns.cu
@@ -1,13 +1,13 @@
-// RUN: rm -rf %T/src
-// RUN: mkdir %T/src
-// RUN: cp %S/pytorch_ATen_cuda_ns.cu %T/src/
+// RUN: rm -rf %T/pytorch_ATen_cuda_ns
+// RUN: mkdir %T/pytorch_ATen_cuda_ns
+// RUN: cp %S/pytorch_ATen_cuda_ns.cu %T/pytorch_ATen_cuda_ns/
 // RUN: cp %S/user_defined_rule_pytorch.yaml %T/
 // RUN: cp -r %S/pytorch_cuda_inc %T/
 // RUN: cd %T
 // RUN: rm -rf %T/pytorch_ATen_cuda_ns_output
 // RUN: mkdir %T/pytorch_ATen_cuda_ns_output
-// RUN: dpct -out-root %T/pytorch_ATen_cuda_ns_output %T/src/pytorch_ATen_cuda_ns.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=%T/user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.cpp --match-full-lines %T/src/pytorch_ATen_cuda_ns.cu
+// RUN: dpct -out-root %T/pytorch_ATen_cuda_ns_output %T/pytorch_ATen_cuda_ns/pytorch_ATen_cuda_ns.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=%T/user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.cpp --match-full-lines %T/pytorch_ATen_cuda_ns/pytorch_ATen_cuda_ns.cu
 // RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST  %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.cpp -o %T/pytorch_ATen_cuda_ns_output/pytorch_ATen_cuda_ns.dp.o %}
 
 #ifndef NO_BUILD_TEST
