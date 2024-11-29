9f92c6111aefb407377fe85de163a6bb62ddf077
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-29 00:58:57 -0800

    fix

--- clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
@@ -1038,7 +1038,7 @@ inline sycl::event matmul(descriptor_ptr handle, matmul_desc_ptr compute_desc,
       matmul_ops.append_eltwise(::dnnl::algorithm::eltwise_gelu_erf, 0.f, 0.f);
       matmul_ops.append_binary(::dnnl::algorithm::binary_add, bias_md);
       dpct::blas::matrix_mem_copy(compute_desc->_epilogue_aux_pointer, new_c,
-                                  matmul_desc_t::attribute::epilogue_aux_ld, new_ldc, m, n,
+                                  compute_desc->_epilogue_aux_ld, new_ldc, m, n,
                                   sizeof(size_t) , q_ptr);
     }
     matmul_attr.set_post_ops(matmul_ops);
