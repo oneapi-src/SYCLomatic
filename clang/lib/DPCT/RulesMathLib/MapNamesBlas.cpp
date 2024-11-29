d52bc6bbdebe236118e913dd85129daa495aa446
Abhilash Majumder <abhilash.majumder@intel.com>
2024-11-29 02:57:42 -0800

    fix

--- clang/lib/DPCT/RulesMathLib/MapNamesBlas.cpp
+++ clang/lib/DPCT/RulesMathLib/MapNamesBlas.cpp
@@ -1624,10 +1624,10 @@ void MapNamesBlas::setExplicitNamespaceMap(
       {"CUBLASLT_EPILOGUE_RELU",
        MapNames::getLibraryHelperNamespace() +
            "blas_gemm::experimental::epilogue_t::relu"},
-      {"CUBLASLT_EPILOGUE_RELU",
-       MapNames::getLibraryHelperNamespace() +
-           "blas_gemm::experimental::epilogue_t::gelu"}
       {"CUBLASLT_EPILOGUE_GELU",
+       MapNames::getLibraryHelperNamespace() +
+           "blas_gemm::experimental::epilogue_t::gelu"},
+      {"CUBLASLT_EPILOGUE_BIAS",
        MapNames::getLibraryHelperNamespace() +
            "blas_gemm::experimental::epilogue_t::bias"},
       {"CUBLASLT_EPILOGUE_GELU_AUX_BIAS",
