500b1572aed6224fd4bfa24b7fbbb91f3017df08
Abhilash Majumder <abhilash.majumder@intel.com>
2024-12-05 01:01:25 -0800

    fix

--- clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/blas_gemm_utils.hpp
@@ -1050,13 +1050,13 @@ inline sycl::event matmul(descriptor_ptr handle, matmul_desc_ptr compute_desc,
       matmul_ops.append_binary(::dnnl::algorithm::binary_add, bias_md);
     } else if (compute_desc->_epilogue == epilogue_t::gelu_aux_bias) {
       matmul_ops.append_eltwise(::dnnl::algorithm::eltwise_gelu_tanh, 0.f, 0.f);
-      matmul_ops.append_binary(::dnnl::algorithm::binary_add, epilogue_bias_md);
+      matmul_ops.append_binary(::dnnl::algorithm::binary_add, bias_md);
       dpct::blas::matrix_mem_copy(compute_desc->_epilogue_aux_pointer, new_c,
                                   compute_desc->_epilogue_aux_ld, new_ldc, m, n,
                                   sizeof(size_t) , dpct::device_to_device, queue);
     }
   matmul_attr.set_post_ops(matmul_ops);
-  
+  }
   auto matmul_pd = ::dnnl::matmul::primitive_desc(
       handle->get_engine(), src_md, weights_md, dst_md, matmul_attr);
   auto matmul_prim = ::dnnl::matmul(matmul_pd);
