63c17e4776983892c8e4c24bac37379373d2d27d
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-12-06 16:27:33 +0800

    [SYCLomatic] Support gelu_aux and gelu_aux_bias epilogue for cublasLtMatmul
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/cublaslt.cu
+++ clang/test/dpct/cublaslt.cu
@@ -234,9 +234,13 @@ void foo3() {
   // CHECK: dpct::blas_gemm::experimental::epilogue_t e;
   // CHECK-NEXT: e = dpct::blas_gemm::experimental::epilogue_t::nop;
   // CHECK-NEXT: e = dpct::blas_gemm::experimental::epilogue_t::relu;
+  // CHECK-NEXT: e = dpct::blas_gemm::experimental::epilogue_t::gelu_aux;
+  // CHECK-NEXT: e = dpct::blas_gemm::experimental::epilogue_t::gelu_aux_bias;
   cublasLtEpilogue_t e;
   e = CUBLASLT_EPILOGUE_DEFAULT;
   e = CUBLASLT_EPILOGUE_RELU;
+  e = CUBLASLT_EPILOGUE_GELU_AUX;
+  e = CUBLASLT_EPILOGUE_GELU_AUX_BIAS;
 }
 
 void foo4() {
