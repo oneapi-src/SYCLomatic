f7534e40cf8a92d8b8f41ce7c554368708b647d9
Teja Alaghari <teja.alaghari@intel.com>
2024-12-11 09:50:58 +0800

    stream() base method bug fix

--- clang/test/dpct/pytorch/c10.cu
+++ clang/test/dpct/pytorch/c10.cu
@@ -5,9 +5,9 @@
 // RUN: cp -r %S/pytorch_cuda_inc %T/pytorch/c10/
 // RUN: cd %T/pytorch/c10
 // RUN: mkdir dpct_out
-// RUN: dpct -out-root dpct_out src/c10.cu --extra-arg="-I./pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
-// RUN: FileCheck --input-file dpct_out/c10.dp.cpp --match-full-lines src/c10.cu
-// RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST  dpct_out/c10.dp.cpp -o dpct_out/c10.dp.o %}
+// RUN: dpct -out-root dpct_out %T/pytorch/c10/src/c10.cu --extra-arg="-I%T/pytorch/c10/pytorch_cuda_inc" --cuda-include-path="%cuda-path/include" --rule-file=%T/pytorch/c10/user_defined_rule_pytorch.yaml  -- -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/pytorch/c10/dpct_out/c10.dp.cpp --match-full-lines %T/pytorch/c10/src/c10.cu
+// RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST  %T/pytorch/c10/dpct_out/c10.dp.cpp -o %T/pytorch/c10/dpct_out/c10.dp.o %}
 
 #ifndef NO_BUILD_TEST
 #include <iostream>
@@ -30,8 +30,10 @@ int main() {
   // CHECK: auto currentStream = c10::xpu::getCurrentXPUStream();
   auto currentStream = c10::cuda::getCurrentCUDAStream();
 
-  // CHECK: std::cout << "Current Stream (Default Device): " << currentStream.queue() << std::endl;
-  std::cout << "Current Stream (Default Device): " << currentStream.stream() << std::endl;
+  // CHECK: dpct::queue_ptr curr_cuda_st = &(currentStream.queue());
+  // CHECK-NEXT: curr_cuda_st = &(c10::xpu::getCurrentXPUStream().queue());
+  cudaStream_t curr_cuda_st = currentStream.stream();
+  curr_cuda_st = c10::cuda::getCurrentCUDAStream().stream();
 
   // CHECK: auto deviceStream = c10::xpu::getCurrentXPUStream(0);
   auto deviceStream = c10::cuda::getCurrentCUDAStream(0);
