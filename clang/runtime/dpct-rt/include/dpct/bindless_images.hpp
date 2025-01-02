d3b81cfa9857ec02b53a34c7f253c6d6fb03aa77
TejaX-Alaghari <teja.alaghari@intel.com>
2025-01-02 12:07:14 -0500

    Bug fix for interop

--- clang/runtime/dpct-rt/include/dpct/bindless_images.hpp
+++ clang/runtime/dpct-rt/include/dpct/bindless_images.hpp
@@ -122,7 +122,13 @@ public:
 
   /// size setter
   /// \param [in] img_dims The dimensions of the imported image resource
-  void set_size(sycl::range<3> img_dims) { size = img_dims; }
+  void set_size(sycl::range<3> img_dims) {
+    for (int i = 0; i < 3; i++) {
+      if (img_dims[i] > 1) {
+        size[i] = img_dims[i];
+      }
+    }
+  }
 
   /// image_channel setter
   /// \param [in] img_ch The channel info of the imported image resource
@@ -130,7 +136,13 @@ public:
 
   /// num_levels setter
   /// \param [in] numLevels The no. of levels in the imported image resource
-  void set_num_levels(unsigned int numLevels) { num_levels = numLevels; }
+  void set_num_levels(unsigned int numLevels) {
+    num_levels = numLevels;
+
+    if (num_levels > 1) {
+      set_image_type(sycl::ext::oneapi::experimental::image_type::mipmap);
+    }
+  }
 
   /// type setter
   /// \param [in] img_type The type of the imported image resource
@@ -303,6 +315,11 @@ public:
   /// Get the image mip level of the bindless image memory.
   /// \returns The image mip level of the bindless image memory.
   image_mem_wrapper *get_mip_level(unsigned int level) {
+    if (_desc.type == sycl::ext::oneapi::experimental::image_type::standard) {
+      assert(level == 0);
+      return this;
+    }
+
     assert(_desc.type == sycl::ext::oneapi::experimental::image_type::mipmap);
     return _sub_wrappers + level;
   }
