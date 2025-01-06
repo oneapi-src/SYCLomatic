5cfa6395fb9894dcb44342e44bf29a475914ee92
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2025-01-06 14:09:46 +0800

    Update comment
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/DPCT/Utility.cpp
+++ clang/lib/DPCT/Utility.cpp
@@ -2264,8 +2264,8 @@ getRangeInRange(SourceRange Range, SourceLocation SearchRangeBegin,
     // the behavior of immediateExpansion is different:
     // 1. string_literal created with "#" does not include the last token
     // 2. greatergreatergreater of template does include the last token
+    // 3. raw_identifier created with "##" is similar as (1)
     // We need to process the last token according to the token kind.
-    // 3. raw_identifier created with "##" is similar as (1).
     if (IncludeLastToken &&
         (!SM.isWrittenInScratchSpace(SM.getSpellingLoc(Range.getEnd())) ||
          Tok.getKind() == tok::TokenKind::string_literal ||
