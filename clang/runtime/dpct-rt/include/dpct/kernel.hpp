fc8049d4eb9419f34d8af323f0fdc914dbe3004e
intwanghao <hao3.wang@intel.com>
2025-03-20 10:23:32 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/runtime/dpct-rt/include/dpct/kernel.hpp
+++ clang/runtime/dpct-rt/include/dpct/kernel.hpp
@@ -29,6 +29,7 @@
 #include "image.hpp"
 #include <fstream>
 #include <random>
+#include <mutex>
 
 namespace dpct {
 
@@ -493,6 +494,7 @@ class kernel_launcher {
         static_cast<sycl::range<3>>(local_range));
     _local_mem_size = local_mem_size;
   };
+  static inline std::mutex kernel_function_ptr_map_mutex;
 
 public:
   /// Variables for storing execution configuration.
@@ -513,6 +515,7 @@ public:
       const void *func,
       std::function<void(dim3, dim3, void **, unsigned int, queue_ptr)>
           launcher) {
+    std::lock_guard<std::mutex> lock(kernel_function_ptr_map_mutex);
     kernel_function_ptr_map[func] = std::move(launcher);
   }
   /// Launches a kernel function with arguments provided directly through
@@ -543,6 +546,7 @@ public:
   /// \param [in] que SYCL queue used to execute kernel.
   static void launch(const void *func, dim3 group_range, dim3 local_range,
                      void **args, unsigned int local_mem_size, queue_ptr que) {
+    std::lock_guard<std::mutex> lock(kernel_function_ptr_map_mutex);
     kernel_function_ptr_map[func](group_range, local_range, args,
                                   local_mem_size, que);
   }
