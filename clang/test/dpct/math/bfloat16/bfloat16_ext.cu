b66357fa1f2d5f18c9ed6e67173206da007aaa6c
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2025-03-21 08:20:35 +0800

    Add test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/test/dpct/math/bfloat16/bfloat16_ext.cu
+++ clang/test/dpct/math/bfloat16/bfloat16_ext.cu
@@ -2,7 +2,7 @@
 // UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2
 // RUN: dpct --format-range=none --use-dpcpp-extensions=intel_device_math -out-root %T/math/bfloat16/bfloat16_ext %s --cuda-include-path="%cuda-path/include" -- -x cuda --cuda-host-only
 // RUN: FileCheck %s --match-full-lines --input-file %T/math/bfloat16/bfloat16_ext/bfloat16_ext.dp.cpp
-// RUN: %if build_lit %{icpx -c -fsycl %T/math/bfloat16/bfloat16_ext/bfloat16_ext.dp.cpp -o %T/math/bfloat16/bfloat16_ext/bfloat16_ext.dp.o %}
+// RUN: %if build_lit %{icpx -c -DNO_BUILD_TEST -fsycl %T/math/bfloat16/bfloat16_ext/bfloat16_ext.dp.cpp -o %T/math/bfloat16/bfloat16_ext/bfloat16_ext.dp.o %}
 
 #include "cuda_bf16.h"
 
@@ -11,6 +11,10 @@ __global__ void kernelFuncBfloat162Arithmetic() {
   __nv_bfloat162 bf162, bf162_1, bf162_2;
   // CHECK: bf162 = bf162_1 / bf162_2;
   bf162 = __h2div(bf162_1, bf162_2);
+  // CHECK: bf162 = bf162_1 * bf162_2;
+  bf162 = __hmul2(bf162_1, bf162_2);
+  // CHECK: bf162 = bf162_1 - bf162_2;
+  bf162 = __hsub2(bf162_1, bf162_2);
 }
 
 __global__ void kernelFuncBfloat16Comparison() {
