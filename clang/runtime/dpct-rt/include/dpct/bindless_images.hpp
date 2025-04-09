f39f6a649da81bf7ae06ad51ef7b02b0fee492fe
Chen, Sheng S <sheng.s.chen@intel.com>
2025-04-09 01:03:20 -0700

    update
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/runtime/dpct-rt/include/dpct/bindless_images.hpp
+++ clang/runtime/dpct-rt/include/dpct/bindless_images.hpp
@@ -1736,6 +1736,7 @@ public:
 
   inline image_mem_wrapper *&get_img_mem() {
     return detail::get_img_mem_map(_img);
+  }
   /// Set mipmap sample filtering mode for bindless image handle
   /// \param [in] filtering_mode The mipmap filtering mode
   inline void set_mip_filtering_mode(sycl::filtering_mode mode) {
