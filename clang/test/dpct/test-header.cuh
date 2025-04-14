f9240d5ea309d21a1b4c59c5efa0a647627af72e
Chen, Sheng S <sheng.s.chen@intel.com>
2025-04-14 14:55:41 +0800

    Init
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/test-header.cuh
+++ clang/test/dpct/test-header.cuh
@@ -1,7 +1,8 @@
 // RUN: echo "empty command"
 
-
+// CHECK: void foofunc() {
 __global__ void foofunc() {
-  // CHECK: size_t tix = item_ct1.get_local_id(2);
+
+  // CHECK: size_t tix = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_local_id(2);
   size_t tix = threadIdx.x;
 }
