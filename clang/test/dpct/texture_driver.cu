1be67ad585930df692ed116822c0a189fe9a33b4
Chen, Sheng S <sheng.s.chen@intel.com>
2025-04-16 16:06:30 +0800

    Up
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/texture_driver.cu
+++ clang/test/dpct/texture_driver.cu
@@ -1,6 +1,6 @@
 // RUN: dpct --format-range=none --usm-level=none -out-root %T/texture_driver %s --cuda-include-path="%cuda-path/include" --sycl-named-lambda -- -x cuda --cuda-host-only -std=c++14
 // RUN: FileCheck --input-file %T/texture_driver/texture_driver.dp.cpp --match-full-lines %s
-// RUN: %if build_lit %{icpx -c -fsycl %T/texture_driver/texture_driver.dp.cpp -o %T//texture_driver/texture_driver.dp.o %}
+// RUN: %if build_lit %{icpx -c -fsycl -DNO_BUILD_TEST %T/texture_driver/texture_driver.dp.cpp -o %T//texture_driver/texture_driver.dp.o %}
 
 #include "cuda.h"
 #include <stdio.h>
