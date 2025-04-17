9da956031f4a1a69630a5d684ab8b7f636cb4dd8
Teja Alaghari <teja.alaghari@intel.com>
2025-04-17 23:14:30 +0800

    Added init rules infra

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -27,6 +27,7 @@
 #include "RulesSHMEM/NVSHMEMAPIMigration.h"
 #include "RulesSecurity/Homoglyph.h"
 #include "RulesSecurity/MisleadingBidirectional.h"
+#include "RulesTensor/CUTENSORAPIMigration.h"
 #include "TextModification.h"
 #include "Utility.h"
 
@@ -196,6 +197,8 @@ REGISTER_RULE(CuDNNTypeRule, PassKind::PK_Migration, RuleGroupKind::RK_DNN)
 REGISTER_RULE(CuDNNAPIRule, PassKind::PK_Migration, RuleGroupKind::RK_DNN)
 
 REGISTER_RULE(NVSHMEMRule, PassKind::PK_Migration, RuleGroupKind::RK_NVSHMEM)
+
+REGISTER_RULE(CUTENSORRule, PassKind::PK_Migration, RuleGroupKind::RK_CUTENSOR)
 
 } // namespace dpct
 } // namespace clang
--- clang/lib/DPCT/RuleInfra/CallExprRewriter.h
+++ clang/lib/DPCT/RuleInfra/CallExprRewriter.h
@@ -70,6 +70,7 @@ private:
   static void initRewriterMapMisc();
   static void initRewriterMapNccl();
   static void initRewriterMapNvshmem();
+  static void initRewriterMapCUTENSOR();
   static void initRewriterMapStream();
   static void initRewriterMapTexture();
   static void initRewriterMapThrust();
