7feb175c36a11892d619acd609aa6f155c4ad089
Chen, Sheng S <sheng.s.chen@intel.com>
2025-04-28 19:55:31 -0700

    up
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/runtime/dpct-rt/include/dpct/ze_utils.hpp
+++ clang/runtime/dpct-rt/include/dpct/ze_utils.hpp
@@ -18,6 +18,11 @@
 namespace dpct {
 namespace experimental {
 
+struct ipc_mem_handle_ext_t {
+  pid_t pid;
+  ze_ipc_mem_handle_t handle;
+};
+
 namespace detail {
 
 #ifndef _SYS_pidfd_open
@@ -28,10 +33,7 @@ namespace detail {
 #define _SYS_pidfd_getfd 438 // syscall number for pidfd_getfd
 #endif
 
-/// Obtain a duplicate of another process's file descriptor.
-/// \param [in] ext_handle IPC memory handle extension
-/// \returns obtained file descriptor
-template <class T> int get_fd_of_peer_process(T ext_handle) {
+inline int get_fd_of_peer_process(ipc_mem_handle_ext_t ext_handle) {
   int pidfd = syscall(_SYS_pidfd_open, ext_handle.pid,
                       0); // obtain a file descriptor that refers to a
                           // process(requires kernel 5.6+).
@@ -44,41 +46,34 @@ template <class T> int get_fd_of_peer_process(T ext_handle) {
 
 } // namespace detail
 
-struct ipc_mem_handle_ext_t {
-  pid_t pid;
-  ze_ipc_mem_handle_t handle;
-};
-
 /// Creates an IPC memory handle for the specified allocation.
 /// \param [in] ptr Pointer to the device memory allocation
 /// \param [out] ext_handle_ptr IPC memory handle extension
-inline void get_mem_ipc_handle(const void *ptr,
-                               ipc_mem_handle_ext_t *ext_handle_ptr) {
+inline ze_result_t get_mem_ipc_handle(const void *ptr,
+                                      ipc_mem_handle_ext_t *ext_handle_ptr) {
   ext_handle_ptr->pid = getpid();
-  auto ret =
-      zeMemGetIpcHandle(sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
-                            dpct::get_current_device().get_context()),
-                        ptr, &ext_handle_ptr->handle);
-  if (ret != ZE_RESULT_SUCCESS)
-    throw std::runtime_error("The zeMemGetIpcHandle execution failed.");
+  return zeMemGetIpcHandle(
+      sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
+          dpct::get_current_device().get_context()),
+      ptr, &ext_handle_ptr->handle);
 }
 
 /// Opens an IPC memory handle to retrieve a device pointer.
 /// \param [in] ext_handle IPC memory handle extension
 /// \param [out] pptr Pointer to device allocation in this process
-inline void open_mem_ipc_handle(ipc_mem_handle_ext_t ext_handle, void **pptr) {
+inline ze_result_t open_mem_ipc_handle(ipc_mem_handle_ext_t ext_handle,
+                                       void **pptr) {
   int fd = detail::get_fd_of_peer_process(ext_handle);
   if (fd < 0)
     throw std::runtime_error("Cannot get file descriptor of peer process.");
   *((int *)ext_handle.handle.data) = fd;
-  auto ret =
-      zeMemOpenIpcHandle(sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
-                             dpct::get_current_device().get_context()),
-                         sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
-                             (sycl::device)dpct::get_current_device()),
-                         ext_handle.handle, 0u, pptr);
-  if (ret != ZE_RESULT_SUCCESS)
-    throw std::runtime_error("The zeMemOpenIpcHandle execution failed.");
+
+  return zeMemOpenIpcHandle(
+      sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
+          dpct::get_current_device().get_context()),
+      sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
+          (sycl::device)dpct::get_current_device()),
+      ext_handle.handle, 0u, pptr);
 }
 
 } // namespace experimental
