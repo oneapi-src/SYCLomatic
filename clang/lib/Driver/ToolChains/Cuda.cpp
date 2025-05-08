5768f50c068eb861c0279e8caeb480057c44ca2c
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2025-05-08 15:41:29 +0800

    Update
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/lib/Driver/ToolChains/Cuda.cpp
+++ clang/lib/Driver/ToolChains/Cuda.cpp
@@ -203,7 +203,6 @@ bool CudaInstallationDetector::ParseCudaVersionFile(const std::string &FilePath)
     Version = CudaVersion::CUDA_129;
   }
 
-
   if (Version != CudaVersion::UNKNOWN) {
     IsVersionSupported = true;
     return true;
@@ -352,32 +351,10 @@ CudaInstallationDetector::CudaInstallationDetector(
 
   // In decreasing order so we prefer newer versions to older versions.
 #ifdef SYCLomatic_CUSTOMIZATION
-  std::initializer_list<const char *> Versions = {"12.9",
-                                                  "12.8",
-                                                  "12.6",
-                                                  "12.5",
-                                                  "12.4",
-                                                  "12.3",
-                                                  "12.2",
-                                                  "12.1",
-                                                  "12.0",
-                                                  "11.8",
-                                                  "11.7",
-                                                  "11.6",
-                                                  "11.5",
-                                                  "11.4",
-                                                  "11.3",
-                                                  "11.2",
-                                                  "11.1",
-                                                  "10.2",
-                                                  "10.1",
-                                                  "10.0",
-                                                  "9.2",
-                                                  "9.1",
-                                                  "9.0",
-                                                  "8.0",
-                                                  "7.5",
-                                                  "7.0"};
+  std::initializer_list<const char *> Versions = {
+      "12.9", "12.8", "12.6", "12.5", "12.4", "12.3", "12.2", "12.1", "12.0",
+      "11.8", "11.7", "11.6", "11.5", "11.4", "11.3", "11.2", "11.1", "10.2",
+      "10.1", "10.0", "9.2",  "9.1",  "9.0",  "8.0",  "7.5",  "7.0"};
 #else
   std::initializer_list<const char *> Versions = {
       "11.4", "11.3", "11.2", "11.1", "10.2", "10.1", "10.0",
