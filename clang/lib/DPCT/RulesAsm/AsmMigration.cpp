be3dc4f04deaef6f85f2a0f861b167583c3ce1bb
TejaX-Alaghari <teja.alaghari@intel.com>
2025-05-15 21:17:31 +0800

    Corrected format

--- clang/lib/DPCT/RulesAsm/AsmMigration.cpp
+++ clang/lib/DPCT/RulesAsm/AsmMigration.cpp
@@ -600,10 +600,10 @@ bool SYCLGenBase::emitVariableDeclaration(const InlineAsmVarDecl *D) {
 
 bool SYCLGenBase::emitAddressExpr(const InlineAsmAddressExpr *Dst) {
   // Address expression only support ld/st/red & atom instructions.
-  if (!CurrInst || !CurrInst->is(asmtok::op_st, asmtok::op_ld, asmtok::op_atom,
-                                 asmtok::op_prefetch, asmtok::op_red,
-                                 asmtok::op_cp, asmtok::op_ldmatrix,
-                                 asmtok::op_stmatrix)) {
+  if (!CurrInst ||
+      !CurrInst->is(asmtok::op_st, asmtok::op_ld, asmtok::op_atom,
+                    asmtok::op_prefetch, asmtok::op_red, asmtok::op_cp,
+                    asmtok::op_ldmatrix, asmtok::op_stmatrix)) {
     return SYCLGenError();
   }
   std::string Type;
@@ -1386,16 +1386,6 @@ protected:
 
     if (!Type || Type->getKind() != InlineAsmBuiltinType::b16)
       return SYCLGenError();
-<<<<<<< HEAD
-=======
-
-    llvm::SaveAndRestore<const InlineAsmInstruction *> Store(CurrInst);
-    CurrInst = Inst;
-    const auto *Dst =
-        dyn_cast_or_null<InlineAsmAddressExpr>(Inst->getOutputOperand());
-    if (!Dst)
-      return false;
->>>>>>> db0271c2b2a8 (Added b16 limitation and comments)
 
     const InlineAsmVectorExpr *VE;
     if (VE = dyn_cast<InlineAsmVectorExpr>(Inst->getInputOperand(0))) {
