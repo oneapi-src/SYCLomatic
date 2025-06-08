d8e36c9060862ff12c645980f8ddf68fb4e5ba7d
Teja Alaghari <teja.alaghari@intel.com>
2025-06-08 12:15:07 +0800

    Added unsupported CUDA versions to QAM test cases

--- clang/test/dpct/query_api_mapping/nvSHMEM/test_rma_nbi.cu
+++ clang/test/dpct/query_api_mapping/nvSHMEM/test_rma_nbi.cu
@@ -1,4 +1,5 @@
 // REQUIRES: system-linux
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1
 
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=nvshmem_putmem_nbi | FileCheck %s -check-prefix=NVSHMEM_PUTMEM_NBI
 // NVSHMEM_PUTMEM_NBI: CUDA API:
--- clang/test/dpct/query_api_mapping/nvSHMEM/test_team_mgmt.cu
+++ clang/test/dpct/query_api_mapping/nvSHMEM/test_team_mgmt.cu
@@ -1,4 +1,5 @@
 // REQUIRES: system-linux
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1
 
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=nvshmem_team_destroy | FileCheck %s -check-prefix=NVSHMEM_TEAM_DESTROY
 // NVSHMEM_TEAM_DESTROY: CUDA API:
