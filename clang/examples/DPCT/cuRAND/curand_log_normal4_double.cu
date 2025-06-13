ba1c5f871d8456c1c30a01c3bbe1b840191c83ab
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2025-06-13 10:43:03 +0800

    Fix
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- clang/examples/DPCT/cuRAND/curand_log_normal4_double.cu
+++ clang/examples/DPCT/cuRAND/curand_log_normal4_double.cu
@@ -1,9 +1,9 @@
 #include "curand_kernel.h"
 
 __global__ void test() {
-  curandStatePhilox4_32_10_t *ps;
   double mean, stddev;
   // Start
+  curandStatePhilox4_32_10_t *ps;
   curand_log_normal4_double(ps /*curandStatePhilox4_32_10_t **/,
                             mean /*double*/, stddev /*double*/);
   // End
--- clang/examples/DPCT/cuRAND/curand_uniform4_double.cu
+++ clang/examples/DPCT/cuRAND/curand_uniform4_double.cu
@@ -1,8 +1,8 @@
 #include "curand_kernel.h"
 
 __global__ void test() {
-  curandStatePhilox4_32_10_t *ps;
   // Start
+  curandStatePhilox4_32_10_t *ps;
   curand_uniform4_double(ps /*curandStatePhilox4_32_10_t **/);
   // End
 }
