fe8d9ed040d90e8135ca05a36bad39862ba879eb
Chen, Sheng S <sheng.s.chen@intel.com>
2025-06-19 11:38:00 +0800

    update
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- clang/test/dpct/query_api_mapping/Runtime/test_cooperative_groups.cu
+++ clang/test/dpct/query_api_mapping/Runtime/test_cooperative_groups.cu
@@ -1,6 +1,6 @@
 // UNSUPPORTED: system-windows
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.1, cuda-11.2, cuda-11.3, cuda-11.4, cuda-11.5
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.1, v11.2, v11.3, v11.4, v11.5
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=cooperative_groups::tiled_partition | FileCheck %s -check-prefix=CG_TILED_PARTITION
 // CG_TILED_PARTITION: CUDA API:
 // CG_TILED_PARTITION-NEXT:    cooperative_groups::thread_block cta =
