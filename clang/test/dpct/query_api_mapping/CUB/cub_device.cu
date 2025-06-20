c4b149c1c752fef51511bcd73814b67ec0a0c8e1
intwanghao <hao3.wang@intel.com>
2025-06-20 12:52:52 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- clang/test/dpct/query_api_mapping/CUB/cub_device.cu
+++ clang/test/dpct/query_api_mapping/CUB/cub_device.cu
@@ -1,5 +1,5 @@
-// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4
+// UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1, cuda-10.2, cuda-11.0, cuda-11.2, cuda-11.4, cuda-11.5, cuda-11.6, cuda-11.7, cuda-11.8
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1, v10.2, v11.0, v11.2, v11.4, v11.5, v11.6, v11.7, v11.8
 // UNSUPPORTED: system-windows
 // clang-format off
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=cub::DeviceReduce::ArgMax | FileCheck %s -check-prefix=CHECK_REDUCE_ARG_MAX
