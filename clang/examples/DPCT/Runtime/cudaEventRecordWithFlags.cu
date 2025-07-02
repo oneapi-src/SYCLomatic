16c0a179e8e702d0a0a86f2fddf5b24b98ad02a2
Ziran Zhang <ziran.zhang@intel.com>
2025-07-02 16:06:59 +0800

    Remove cudaEventRecordWithFlags

--- clang/examples/DPCT/Runtime/cudaEventRecordWithFlags.cu
+++
@@ -1,5 +0,0 @@
-void test(cudaEvent_t event, cudaStream_t stream, unsigned int flags) {
-  // Start
-  cudaEventRecordWithFlags(event/*cudaEvent_t*/, stream/*cudaStream_t*/, flags/*unsigned int*/);
-  // End
-}
